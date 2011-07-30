55d6cdf2056cc627eb0305f145dffe5b2950148e
Ben Phelps <bphelpsen@gmail.com>
2011-07-29 22:48:41 -0500

    Format Cleanup, Hunter Fix
    I cleaned up the two rotation tables and fixed some tab styling
    (everywhere uses single tabs, set your editor to your preference instead
    of using hard space tabs).

--- jpdata.lua
+++ jpdata.lua
@@ -1,16 +1,16 @@
 -- Lookup Tables
 -- Specs
 jps.Specs = {
-	["Death Knight"] = {[1] = "Blood", [2] = "Frost", [3] = "Unholy"},
-	["Druid"] = {[1] = "Balance", [2] = "Feral", [3] = "Restoration"},
-	["Warlock"] = {[1] = "Affliction", [2] = "Demonology", [3] = "Destruction"},
-	["Priest"] = {[1] = "Discipline", [2] = "Holy", [3] = "Shadow"},
-	["Warrior"] = {[1] = "Arms", [2] = "Fury", [3] = "Protection"},
-	["Paladin"] = {[1] = "Holy", [2] = "Protection", [3] = "Retribution"},
-	["Shaman"] = {[1] = "Elemental", [2] = "Enhancement", [3] = "Restoration"},
-	["Rogue"] = {[1] = "Assassination", [2] = "Combat", [3] = "Subtlety"},
-	["Hunter"] = {[1] = "Beast Mastery", [2] = "Marksmanship", [3] = "Survival"},
-	["Mage"] = {[1] = "Arcane", [2] = "Fire", [3] = "Frost"},
+    ["Death Knight"] = { [1] = "Blood",            [2] = "Frost",        [3] = "Unholy"     },
+    ["Druid"]        = { [1] = "Balance",          [2] = "Feral",        [3] = "Restoration"},
+    ["Warlock"]      = { [1] = "Affliction",       [2] = "Demonology",   [3] = "Destruction"},
+    ["Priest"]       = { [1] = "Discipline",       [2] = "Holy",         [3] = "Shadow"     },
+    ["Warrior"]      = { [1] = "Arms",             [2] = "Fury",         [3] = "Protection" },
+    ["Paladin"]      = { [1] = "Holy",             [2] = "Protection",   [3] = "Retribution"},
+    ["Shaman"]       = { [1] = "Elemental",        [2] = "Enhancement",  [3] = "Restoration"},
+    ["Rogue"]        = { [1] = "Assassination",    [2] = "Combat",       [3] = "Subtlety"   },
+    ["Hunter"]       = { [1] = "Beast Mastery",    [2] = "Marksmanship", [3] = "Survival"   },
+    ["Mage"]         = { [1] = "Arcane",           [2] = "Fire",         [3] = "Frost"      },
 }
 
 -- Functions
@@ -21,7 +21,7 @@ function jps.Cast(spell)
 	jps.Target = "target"
 	if jps.IconSpell ~= spell then
 		jps.set_jps_icon(spell)
-		if jps.Debug then	print(spell, jps.Target) end
+		if jps.Debug then print(spell, jps.Target) end
 	end
 end
 
@@ -89,35 +89,33 @@ end
 
 function jps.should_pvpkick(unit)
 	local target_spell, _, _, _, _, endTime, _, _, unInterruptable = UnitCastingInfo(unit)
-  local channelling, _, _, _, _, _, _, notInterruptible = UnitChannelInfo(unit)
+  	local channelling, _, _, _, _, _, _, notInterruptible = UnitChannelInfo(unit)
 	if target_spell == "Release Aberrations" then return false end
-
-  if target_spell and not unInterruptable then
-    endTime = endTime - GetTime()*1000
-	if jps.PVPInterrupt == true then
-	    if endTime < 500+jps.Lag then
-	      return true
-	    end 
-	else 
+	if target_spell and not unInterruptable then
+		endTime = endTime - GetTime()*1000
+		if jps.PVPInterrupt == true then
+			if endTime < 500+jps.Lag then
+				return true
+			end 
+		else 
+			return true
+		end
+	elseif chanelling and not notInterruptible then
 		return true
 	end
-  elseif chanelling and not notInterruptible then
-    return true
-  end 
-
 	return false
 end
 
 function jps.should_kick(unit)
 	local target_spell, _, _, _, _, endTime, _, _, unInterruptable = UnitCastingInfo(unit)
-  local channelling, _, _, _, _, _, _, notInterruptible = UnitChannelInfo(unit)
+	local channelling, _, _, _, _, _, _, notInterruptible = UnitChannelInfo(unit)
 	if target_spell == "Release Aberrations" then return false end
-  if target_spell and not unInterruptable then
+	if target_spell and not unInterruptable then
 		return true
 	end
-  if chanelling and not notInterruptible then
-    return true
-  end 
+	if chanelling and not notInterruptible then
+		return true
+	end 
 	return false
 end
 
@@ -127,56 +125,56 @@ jps.HealValues = {}
 function jps.update_healtable(...)
 	local temparglist = {...}
 	if temparglist[5] == GetUnitName("player") and (temparglist[2] == "SPELL_HEAL" or temparglist[2] == "SPELL_PERIODIC_HEAL") and temparglist[15] == 0 then
-		if jps.HealValues[temparglist[11]]== nil then
-			jps.HealValues[temparglist[11]]= {["healtotal"]= temparglist[13],["healcount"]= 1,["averageheal"]= temparglist[13]}
+		if jps.HealValues[temparglist[11]] == nil then
+			jps.HealValues[temparglist[11]] = {["healtotal"] = temparglist[13],["healcount"] = 1, ["averageheal"] = temparglist[13]}
 		else
-			jps.HealValues[temparglist[11]]["healtotal"]= jps.HealValues[temparglist[11]]["healtotal"]+temparglist[13]
-			jps.HealValues[temparglist[11]]["healcount"]= jps.HealValues[temparglist[11]]["healcount"]+1
-			jps.HealValues[temparglist[11]]["averageheal"]= jps.HealValues[temparglist[11]]["healtotal"]/jps.HealValues[temparglist[11]]["healcount"]
+			jps.HealValues[temparglist[11]]["healtotal"]   = jps.HealValues[temparglist[11]]["healtotal"] + temparglist[13]
+			jps.HealValues[temparglist[11]]["healcount"]   = jps.HealValues[temparglist[11]]["healcount"] + 1
+			jps.HealValues[temparglist[11]]["averageheal"] = jps.HealValues[temparglist[11]]["healtotal"] / jps.HealValues[temparglist[11]]["healcount"]
 		end
 	end
 end
 
 function jps.reset_healtable(self)
-  for k,v in pairs(healtable) do
-    jps.HealValues[k]["healtotal"]= jps.HealValues[k]["averageheal"]
-    jps.HealValues[k]["healcount"]= 1
-  end
+	for k,v in pairs(healtable) do
+		jps.HealValues[k]["healtotal"]= jps.HealValues[k]["averageheal"]
+		jps.HealValues[k]["healcount"]= 1
+	end
 end
 
 function jps.getaverage_heal(spellname)
-  if jps.HealValues[spellname] ~= nil then
-    return jps.HealValues[spellname]["averageheal"]
-  else
-    return 0
-  end
+	if jps.HealValues[spellname] ~= nil then
+		return jps.HealValues[spellname]["averageheal"]
+	else
+		return 0
+	end
 end
 
 function jps.UpdateOutOfSightPlayers(self)
    if #jps.OutOfSightPlayers > 0 then
-      for i = #jps.OutOfSightPlayers, 1, -1 do
-         if GetTime() - jps.OutOfSightPlayers[i][2] > 2 then
-            table.remove(jps.OutOfSightPlayers,i)
-         end
-      end
+	  for i = #jps.OutOfSightPlayers, 1, -1 do
+		 if GetTime() - jps.OutOfSightPlayers[i][2] > 2 then
+			table.remove(jps.OutOfSightPlayers,i)
+		 end
+	  end
    end
 end
 
 function jps.PlayerIsExcluded(playerName)
-   for i = 1, #jps.OutOfSightPlayers do
-      if jps.OutOfSightPlayers[i][1] ==  playerName then
-         return true
-      end
-   end
-   return false
+	for i = 1, #jps.OutOfSightPlayers do
+		if jps.OutOfSightPlayers[i][1] ==	 playerName then
+			return true
+		end
+	end
+	return false
 end
 
 function jps.ExcludePlayer(playername)
-   if playername == nil then
-      playername = "nil"
-   end
-   local playerexclude = {}
-   table.insert(playerexclude, playername)
-   table.insert(playerexclude, GetTime())
-   table.insert(jps.OutOfSightPlayers,playerexclude)
+	if playername == nil then
+		playername = "nil"
+	end
+	local playerexclude = {}
+	table.insert(playerexclude, playername)
+	table.insert(playerexclude, GetTime())
+	table.insert(jps.OutOfSightPlayers,playerexclude)
 end
--- jps.lua
+++ jps.lua
@@ -60,181 +60,212 @@ combatFrame:RegisterEvent("UNIT_HEALTH")
 combatFrame:RegisterEvent("BAG_UPDATE")
 
 function combatEventHandler(self, event, ...)
-	if event == "PLAYER_ALIVE" or event == "PLAYER_LOGIN" then
-		jps.Class = UnitClass("player")
-		jps.Spec = jps.Specs[jps.Class][GetPrimaryTalentTree()]
-		if jps.Spec then print (":::: JPS Online for your",jps.Spec,jps.Class,"::::") end
-		if not jps.Enabled then IconFrame:Hide() end
-	elseif event == "PLAYER_REGEN_DISABLED" then
-		jps.Combat = true
-		if jps.Enabled then combat() end
-	elseif event == "PLAYER_REGEN_ENABLED" then
-		jps.Combat = false
-		jps.Opening = true
-		jps.RaidStatus = {}
-		collectgarbage("collect")
-	-- Casting - Credit (and thanks!) to walkistalki for the channeling and pet stuff.
-	elseif event == "UNIT_SPELLCAST_START" or event == "UNIT_SPELLCAST_CHANNEL_START" or event == "UNIT_SPELLCAST_SENT" then
-		if ... == "player" then
-			jps.Casting = true
-		end
+    if event == "PLAYER_ALIVE" or event == "PLAYER_LOGIN" then
+        jps.Class = UnitClass("player")
+        jps.Spec = jps.Specs[jps.Class][GetPrimaryTalentTree()]
+        if jps.Spec then print (":::: JPS Online for your",jps.Spec,jps.Class,"::::") end
+        if not jps.Enabled then IconFrame:Hide() end
+    elseif event == "PLAYER_REGEN_DISABLED" then
+        jps.Combat = true
+        if jps.Enabled then combat() end
+    elseif event == "PLAYER_REGEN_ENABLED" then
+        jps.Combat = false
+        jps.Opening = true
+        jps.RaidStatus = {}
+        collectgarbage("collect")
+    -- Casting - Credit (and thanks!) to walkistalki for the channeling and pet stuff.
+    elseif event == "UNIT_SPELLCAST_START" or event == "UNIT_SPELLCAST_CHANNEL_START" or event == "UNIT_SPELLCAST_SENT" then
+        if ... == "player" then
+            jps.Casting = true
+        end
   elseif (event == "UNIT_SPELLCAST_SUCCEEDED" and (UnitChannelInfo("player") == nil)) or event == "UNIT_SPELLCAST_CHANNEL_STOP" then
-		if ... == "player" then
-			jps.Casting = false
-		end
+        if ... == "player" then
+            jps.Casting = false
+        end
   elseif event == "UNIT_SPELLCAST_FAILED" or event == "UNIT_SPELLCAST_INTERRUPTED" or (event == "UNIT_SPELLCAST_CHANNEL_UPDATE" and (UnitChannelInfo("player")==nil))then
-		if ... == "player" then
-			jps.Casting = false
-		end
-	-- Fishes
-	elseif event == "BAG_UPDATE" and jps.Fishing then
-		RunMacro("MG")
-		RunMacro("MGG")
-	-- UI Error checking - for LoS and Shred-fails.
-	elseif event == "UI_ERROR_MESSAGE" and jps.Enabled then
-		jps.Error = ...
-		if jps.Error == "You must be behind your target." and jps.ThisCast == "shred" then
-			jps.Cast("mangle(cat form)")
-		elseif jps.Error == "You must be behind your target." and (jps.ThisCast == "backstab" or jps.ThisCast == "garrote") then
-			jps.cast("mutilate")
-		end
-	-- RaidStatus Update
-	elseif event == "UNIT_HEALTH" and jps.Enabled then
-		local unit = ...
-		if UnitIsFriend("player",unit) then
-			jps.RaidStatus[unit] = { ["hp"] = UnitHealth(unit), ["hpmax"] = UnitHealthMax(unit), ["freshness"] = 0 }
-		end
-	end
+        if ... == "player" then
+            jps.Casting = false
+        end
+    -- Fishes
+    elseif event == "BAG_UPDATE" and jps.Fishing then
+        RunMacro("MG")
+        RunMacro("MGG")
+    -- UI Error checking - for LoS and Shred-fails.
+    elseif event == "UI_ERROR_MESSAGE" and jps.Enabled then
+        jps.Error = ...
+        if jps.Error == "You must be behind your target." and jps.ThisCast == "shred" then
+            jps.Cast("mangle(cat form)")
+        elseif jps.Error == "You must be behind your target." and (jps.ThisCast == "backstab" or jps.ThisCast == "garrote") then
+            jps.cast("mutilate")
+        end
+    -- RaidStatus Update
+    elseif event == "UNIT_HEALTH" and jps.Enabled then
+        local unit = ...
+        if UnitIsFriend("player",unit) then
+            jps.RaidStatus[unit] = { ["hp"] = UnitHealth(unit), ["hpmax"] = UnitHealthMax(unit), ["freshness"] = 0 }
+        end
+    end
 end
 
 combatFrame:SetScript("OnEvent", combatEventHandler)
 
 function SlashCmdList.jps(msg, editbox)
-	if msg == "toggle" or msg == "t" then
-		if jps.Enabled == false then msg = "e"
-		else msg = "d" end
-	end
-	if msg== "disable" or msg == "d" then
-		jps.Enabled = false
-		IconFrame:Hide()
-		print "JPS Disabled."
-	elseif msg== "enable" or msg == "e" then
-		jps.Enabled = true
-		jps.NextCast = nil
-		IconFrame:Show()
-		print "JPS Enabled."
-	elseif msg == "panther" then
-		jps.Panther = not jps.Panther
-		print("T11 4pc use set to",jps.Panther)
-	elseif msg == "fishing" then
-		jps.Fishing = not jps.Fishing
-		print("Murglesnout & Grey Deletion now",jps.Fishing)
-	elseif msg == "debug" then
-		jps.Debug = not jps.Debug
-		print("Debug mode set to",jps.Debug)
-	elseif msg == "peth" then
-		jps.PetHeal = not jps.PetHeal
-		print("Pet heal set to",jps.PetHeal)
-	elseif msg == "multi" or msg == "multitarget" then
-		jps.MultiTarget = not jps.MultiTarget
-		print("MultiTarget mode set to",jps.MultiTarget)
-	elseif msg == "lifeblood" or msg == "lb" then
-		jps.Lifeblood = not jps.Lifeblood
-		print("Lifeblood set to",jps.MultiTarget)
-	elseif msg == "cds" then
-		jps.UseCDs = not jps.UseCDs
-		print("Cooldown use set to",jps.UseCDs)
-	elseif msg == "int" or msg == "interrupts" then
-		jps.Interrupts = not jps.Interrupts
-		print("Interrupt use set to",jps.Interrupts)
-	elseif msg == "pint" then
-		jps.PVPInterrupt = not jps.PVPInterrupt
-		print("PVP Interrupt use set to",jps.PVPInterrupt)
-	elseif msg == "spam" or msg == "macrospam" or msg == "macro" then
-		jps.MacroSpam = not jps.MacroSpam
-		print("MacroSpam flag is now set to",jps.MacroSpam)
-	elseif msg == "havoc" then
-		jps.Havoc = not jps.Havoc
-		print("Bane of Havoc flag is now set to",jps.Havoc)
-	elseif msg == "opening" then
-		jps.Opening = not jps.Opening
-		print("Opening flag is now set to",jps.Opening)
-	elseif msg == "help" then
-		print("Slash Commands:")
-		print("/jps - Show enabled status.")
-		print("/jps enable/disable - Enable/Disable the addon.")
-		print("/jps spam - Toggle spamming of a given macro.")
-		if jps.Spec == "Feral" then
-			print("/jps panther - Toggle Feral T11 4pc.")
-		end
-		print("/jps cds - Toggle use of cooldowns.")
-		print("/jps pew - Spammable macro to do your best moves, if for some reason you don't want it fully automated")
-		print("/jps interrupts - Toggle interrupting")
-		print("/jps help - Show this help text.")
-	elseif msg == "pew" then
-		combat()
-	else
-		if jps.Enabled then
-			print("JPS v"..jps.Version.." Enabled - Ready and Waiting.")
-		else 
-			print "JPS Disabled - Waiting on Standby."
-		end
-		print("jps.UseCDs:",jps.UseCDs)
-		print("jps.Opening:",jps.Opening)
-		print("jps.Interrupts:",jps.Interrupts)
-		if jps.Spec == "Feral" then
-			print("jps.Panther:",jps.Panther)
-		end
-		print("jps.MacroSpam:",jps.MacroSpam)
-		print("jps.Fishing:",jps.Fishing)
-	end
+    if msg == "toggle" or msg == "t" then
+        if jps.Enabled == false then msg = "e"
+        else msg = "d" end
+    end
+    if msg== "disable" or msg == "d" then
+        jps.Enabled = false
+        IconFrame:Hide()
+        print "JPS Disabled."
+    elseif msg== "enable" or msg == "e" then
+        jps.Enabled = true
+        jps.NextCast = nil
+        IconFrame:Show()
+        print "JPS Enabled."
+    elseif msg == "panther" then
+        jps.Panther = not jps.Panther
+        print("T11 4pc use set to",jps.Panther)
+    elseif msg == "fishing" then
+        jps.Fishing = not jps.Fishing
+        print("Murglesnout & Grey Deletion now",jps.Fishing)
+    elseif msg == "debug" then
+        jps.Debug = not jps.Debug
+        print("Debug mode set to",jps.Debug)
+    elseif msg == "peth" then
+        jps.PetHeal = not jps.PetHeal
+        print("Pet heal set to",jps.PetHeal)
+    elseif msg == "multi" or msg == "multitarget" then
+        jps.MultiTarget = not jps.MultiTarget
+        print("MultiTarget mode set to",jps.MultiTarget)
+    elseif msg == "lifeblood" or msg == "lb" then
+        jps.Lifeblood = not jps.Lifeblood
+        print("Lifeblood set to",jps.MultiTarget)
+    elseif msg == "cds" then
+        jps.UseCDs = not jps.UseCDs
+        print("Cooldown use set to",jps.UseCDs)
+    elseif msg == "int" or msg == "interrupts" then
+        jps.Interrupts = not jps.Interrupts
+        print("Interrupt use set to",jps.Interrupts)
+    elseif msg == "pint" then
+        jps.PVPInterrupt = not jps.PVPInterrupt
+        print("PVP Interrupt use set to",jps.PVPInterrupt)
+    elseif msg == "spam" or msg == "macrospam" or msg == "macro" then
+        jps.MacroSpam = not jps.MacroSpam
+        print("MacroSpam flag is now set to",jps.MacroSpam)
+    elseif msg == "havoc" then
+        jps.Havoc = not jps.Havoc
+        print("Bane of Havoc flag is now set to",jps.Havoc)
+    elseif msg == "opening" then
+        jps.Opening = not jps.Opening
+        print("Opening flag is now set to",jps.Opening)
+    elseif msg == "help" then
+        print("Slash Commands:")
+        print("/jps - Show enabled status.")
+        print("/jps enable/disable - Enable/Disable the addon.")
+        print("/jps spam - Toggle spamming of a given macro.")
+        if jps.Spec == "Feral" then
+            print("/jps panther - Toggle Feral T11 4pc.")
+        end
+        print("/jps cds - Toggle use of cooldowns.")
+        print("/jps pew - Spammable macro to do your best moves, if for some reason you don't want it fully automated")
+        print("/jps interrupts - Toggle interrupting")
+        print("/jps help - Show this help text.")
+    elseif msg == "pew" then
+        combat()
+    else
+        if jps.Enabled then
+            print("JPS v"..jps.Version.." Enabled - Ready and Waiting.")
+        else 
+            print "JPS Disabled - Waiting on Standby."
+        end
+        print("jps.UseCDs:",jps.UseCDs)
+        print("jps.Opening:",jps.Opening)
+        print("jps.Interrupts:",jps.Interrupts)
+        if jps.Spec == "Feral" then
+            print("jps.Panther:",jps.Panther)
+        end
+        print("jps.MacroSpam:",jps.MacroSpam)
+        print("jps.Fishing:",jps.Fishing)
+    end
 end
 
 function JPS_OnUpdate(self,elapsed)
-	self.TimeSinceLastUpdate = self.TimeSinceLastUpdate + elapsed
-	if (self.TimeSinceLastUpdate > jps.UpdateInterval) then
-		if jps.MacroSpam and not jps.Casting then
-			RunMacro(jps.Macro)
-		elseif jps.Combat and jps.Enabled then
-			combat()
-			self.TimeSinceLastUpdate = 0
-		end
-	end
+    self.TimeSinceLastUpdate = self.TimeSinceLastUpdate + elapsed
+    if (self.TimeSinceLastUpdate > jps.UpdateInterval) then
+        if jps.MacroSpam and not jps.Casting then
+            RunMacro(jps.Macro)
+        elseif jps.Combat and jps.Enabled then
+            combat()
+            self.TimeSinceLastUpdate = 0
+        end
+    end
 end
 
 function combat(self) 
-	-- Rotations
-	jps.Rotations = { 
-		["Druid"] = { ["Feral"] = druid_feral, ["Balance"] = druid_balance, ["Restoration"] = druid_resto },
-		["Death Knight"] = { ["Blood"] = dk_blood, ["Frost"] = dk_frost  },
-		["Shaman"] = { ["Enhancement"] = shaman_enhancement, ["Elemental"] = shaman_elemental },
-		["Paladin"] = { ["Protection"] = paladin_protadin, ["Retribution"] = paladin_ret },
-		["Warlock"] = { ["Affliction"] = warlock_affl, ["Destruction"] = warlock_destro, ["Demonology"] = warlock_demo },
-		["Hunter"] = { ["Beast Mastery"] = hunter_bm, ["Marksmanship"] = hunter_mm, ["Survival"] = hunter_sv },
-		["Mage"] = { ["Fire"] = mage_fire, ["Arcane"] = mage_arcane, ["Frost"] = mage_frost },
-		["Rogue"] = { ["Assassination"] = rogue_assass },
-		["Warrior"] = { ["Fury"] = warrior_fury, ["Protection"] = warrior_prot, ["Arms"] = warrior_arms },
-		["Priest"] = { ["Shadow"] = priest_shadow, ["Holy"] = priest_holy  }
-	}
-	-- Check for the Rotation
-	if not jps.Rotations[jps.Class] or not jps.Rotations[jps.Class][jps.Spec] then
-		print("Sorry! JPS does not yet have a rotation for your",jps.Spec,jps.Class.."...yet.")
-		jps.Enabled = false
-		return
-	end
-	-- Lag
-	_,_,jps.Lag = GetNetStats()
-	jps.Lag = jps.Lag/100
-	-- Movement
-	jps.Moving = GetUnitSpeed("player") > 0
-	-- Get spell from rotation.
-	jps.ThisCast = jps.Rotations[jps.Class][jps.Spec]()
-	-- Check spell usability.
-	if jps.ThisCast then
-		jps.Cast(jps.ThisCast)
-	end
-	-- Hide Error
-	StaticPopup1:Hide()
-	-- Return spellcast.
-	return jps.ThisCast
+    -- Rotations
+	-- I hate ugly tables :p (Phelps)
+    jps.Rotations = { 
+        ["Druid"]        = { ["Feral"]         = druid_feral,
+                             ["Balance"]       = druid_balance,
+                             ["Restoration"]   = druid_resto },
+        
+        ["Death Knight"] = { ["Blood"]         = dk_blood,
+                             ["Frost"]         = dk_frost  },
+            
+        ["Shaman"]       = { ["Enhancement"]   = shaman_enhancement,
+                             ["Elemental"]     = shaman_elemental },
+        
+        ["Paladin"]      = { ["Protection"]    = paladin_protadin,
+                             ["Retribution"]   = paladin_ret },
+        
+        ["Warlock"]      = { ["Affliction"]    = warlock_affl,
+                             ["Destruction"]   = warlock_destro,
+                             ["Demonology"]    = warlock_demo },
+        
+        ["Hunter"]       = { ["Beast Mastery"] = hunter_bm,
+                             ["Marksmanship"]  = hunter_mm,
+                             ["Survival"]      = hunter_sv },
+                    
+        ["Mage"]         = { ["Fire"]          = mage_fire,
+                             ["Arcane"]        = mage_arcane,
+                             ["Frost"]         = mage_frost },
+                            
+        ["Rogue"]        = { ["Assassination"] = rogue_assass },
+        
+        ["Warrior"]      = { ["Fury"]          = warrior_fury,
+                             ["Protection"]    = warrior_prot,
+                             ["Arms"]          = warrior_arms },
+                            
+        ["Priest"]       = { ["Shadow"]        = priest_shadow,
+                             ["Holy"]          = priest_holy }
+    }
+    
+    -- Check for the Rotation
+    if not jps.Rotations[jps.Class] or not jps.Rotations[jps.Class][jps.Spec] then
+        print("Sorry! JPS does not yet have a rotation for your",jps.Spec,jps.Class.."...yet.")
+        jps.Enabled = false
+        return
+    end
+    
+    -- Lag
+    _,_,jps.Lag = GetNetStats()
+    jps.Lag = jps.Lag/100
+    
+    -- Movement
+    jps.Moving = GetUnitSpeed("player") > 0
+    
+    -- Get spell from rotation.
+    jps.ThisCast = jps.Rotations[jps.Class][jps.Spec]()
+    
+    -- Check spell usability.
+    if jps.ThisCast then
+        jps.Cast(jps.ThisCast)
+    end
+    
+    -- Hide Error
+    StaticPopup1:Hide()
+    
+    -- Return spellcast.
+    return jps.ThisCast
 end
