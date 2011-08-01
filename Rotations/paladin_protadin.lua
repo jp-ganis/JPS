b989285db7106646c121e285893b1a56e4e7b0ee
Kyle Arrington <kyle@arringtonlumber.com>
2011-08-01 14:05:46 -0500

    Fixes:
    1. WoG added. (3hp, health less than 80%)
    2. Grand Crusader Procs.
    3. Consecration threshold increased to 80% mana.
    4. Defensive CD thresholds adjusted to better reflect their cooldowns/use.
    5. Hammer of Wrath priority adjusted. (Top priority.)
    6. Race detection added for Arcane Torrent.
    7. Added Holy Wrath to Single target rotation, because it's part of it.
    8. Prioritized Judgment for mana return.  (Essential if raid spec'd)

--- Rotations/paladin_protadin.lua
+++ Rotations/paladin_protadin.lua
@@ -1,18 +1,19 @@
 function paladin_protadin(self)
-		-- Complete re-write for 4.2 by GoCarGo.
-        local myHealthPercent = UnitHealth("player")/UnitHealthMax("player") * 100
-        local targetHealthPercent = UnitHealth("target")/UnitHealthMax("target") * 100
-        local myManaPercent = UnitMana("player")/UnitManaMax("player") * 100
-        local hPower = UnitPower("player","9")
-        local spell = nil
+	-- Complete re-write for 4.2 by GoCarGo.
+	local myHealthPercent = UnitHealth("player")/UnitHealthMax("player") * 100
+	local targetHealthPercent = UnitHealth("target")/UnitHealthMax("target") * 100
+	local myManaPercent = UnitMana("player")/UnitManaMax("player") * 100
+	local hPower = UnitPower("player","9")
+	local race = UnitRace("player")
+	local spell = nil
 
 	-- Interrupt, works equally well with "focus" instead of "target"
 	if jps.Interrupts and jps.should_kick("target") and cd("Rebuke") == 0 and myManaPercent >= 25 then
 		return "Rebuke"
 	end
 
-        --***************COMMENT This out if you are not A Blood Elf**********************
-        if jps.Interrupts and jps.should_kick("target") and cd("Arcane Torrent") == 0 then
+    -- Blood Elf Arcane Torrent
+    if jps.Interrupts and jps.should_kick("target") and cd("Arcane Torrent") == 0 and race == "Blood Elf" then
 		return "Arcane Torrent"
 	end
 
@@ -38,33 +39,45 @@ function paladin_protadin(self)
 		return "Avenger's Shield"
 	end
 
-	-- Defense
-	if cd("Holy Shield") == 0 and myHealthPercent < 75 then 
+	-- Cast WoG if health is below 70% and WoG is off CD
+	if cd("Word of Glory") == 0 and myHealthPercent < 80 and hPower == 3 then
+		return "Word of Glory"
+	end
+
+	-- Defensive Cooldowns
+	if cd("Holy Shield") == 0 and myHealthPercent < 80 then 
 		return "Holy Shield"
-        elseif cd("Divine Protection") == 0 and myHealthPercent < 52 then
+	elseif cd("Divine Protection") == 0 and myHealthPercent < 60 then
 		return "Divine Protection"
-	elseif cd("Guardian of Ancient Kings") == 0 and myHealthPercent < 47 then
+	elseif cd("Guardian of Ancient Kings") == 0 and myHealthPercent < 50 then
 		return "Guardian of Ancient Kings"
-	elseif cd("Ardent Defender") == 0 and myHealthPercent < 33 then
+	elseif cd("Ardent Defender") == 0 and myHealthPercent < 20 then
 		return "Ardent Defender"
-        elseif cd("Lay on Hands") == 0 and myHealthPercent < 23 then
+	elseif cd("Lay on Hands") == 0 and myHealthPercent < 10 then
 		return "Lay on Hands"
+	end
+
+	-- Grand Crusader procs
+	if ub("player","Grand Crusader") then
+		spell = "Avenger's Shield"
 	end
 
 	-- Offense, Single-Target
 	if not jps.MultiTarget then
 		if cd("Avenging Wrath") == 0 and UnitHealthMax("target") > 100000 and not ub("player","Avenging Wrath") then
 			spell = "Avenging Wrath"
-                elseif cd("Shield of the Righteous") == 0 and hPower == 3 then
+		elseif cd("Hammer of Wrath") == 0 and targetHealthPercent < 20 then
+			spell = "Hammer of Wrath"
+		elseif cd("Shield of the Righteous") == 0 and hPower == 3 then
 			spell = "Shield of the Righteous"
 		elseif cd("Crusader Strike") == 0 then
 			spell = "Crusader Strike"
-		elseif cd("Avenger's Shield") == 0 then
-			spell = "Avenger's Shield"
-		elseif cd("Hammer of Wrath") == 0 and targetHealthPercent < 20 then
-			spell = "Hammer of Wrath"
 		elseif cd("Judgement") == 0 then
 			spell = "Judgement"
+        elseif cd("Holy Wrath") == 0 then
+			spell = "Holy Wrath"
+		elseif cd("Avenger's Shield") == 0 then
+			spell = "Avenger's Shield"
 		end
 
 	-- Multi-Target
@@ -73,18 +86,18 @@ function paladin_protadin(self)
 			spell = "Inquisition"
 		elseif cd("Hammer of the Righteous") == 0 then
 			spell = "Hammer of the Righteous"
+        elseif cd("Judgement") == 0 then
+			spell = "Judgement"
 		elseif cd("Avenger's Shield") == 0 then
 			spell = "Avenger's Shield"
-                elseif cd("Consecration") == 0 and myManaPercent > 55 then
-			spell = "Consecration"
-                elseif cd("Judgement") == 0 then
-			spell = "Judgement"                
-                elseif cd("Holy Wrath") == 0 then
+        elseif cd("Consecration") == 0 and myManaPercent > 70 then
+			spell = "Consecration"                
+        elseif cd("Holy Wrath") == 0 then
 			spell = "Holy Wrath"
 		elseif cd("Hammer of Wrath") == 0 and targetHealthPercent < 20 then
 			spell = "Hammer of Wrath"
 		
-                end
+        end
 	end
 
 	-- Return
