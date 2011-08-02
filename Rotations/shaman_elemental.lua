b3eb0945e2dc86fdc905552be5babae9e6df7533
Mike Damm <mike@damm.com>
2011-08-02 15:30:35 -0700

    Interrupts and multitarget for ele
    Use blood fury cooldown, interrupt if its enabled, and honor multitarget option for CL

--- Rotations/shaman_elemental.lua
+++ Rotations/shaman_elemental.lua
@@ -5,7 +5,12 @@ function shaman_elemental(self)
    local lsCount = jps.get_buff_stacks("player","lightning shield")
    local fsDuration = jps.buff_duration("target","flame shock")
    
-   if lsCount < 2 then
+   if jps.Interrupts and cd("wind shear") == 0 and jps.should_kick("target") and mana > .4 then 
+      SpellStopCasting()
+      spell = "wind shear"
+   elseif jps.UseCDs and cd("blood fury") == 0 then
+      spell = "blood fury"
+   elseif lsCount < 2 then
       spell = "lightning shield"
    elseif lsCount == 9 and fsDuration > 6 and cd("earth shock") == 0 then
       spell = "earth shock"
@@ -19,7 +24,7 @@ function shaman_elemental(self)
       spell = "elemental mastery"      
    elseif cd("lava burst") == 0 then
       spell = "lava burst"
-   elseif cd("chain lightning") == 0 then
+   elseif cd("chain lightning") == 0 and jps.MultiTarget then
       spell = "chain lightning"
    else
       spell = "lightning bolt"
