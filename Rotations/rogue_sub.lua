999488b6520e8daa462d4ec86672616139f4ccad
Jp <james.ganis@gmail.com>
2011-08-20 15:47:55 +0100

    Rogue sub updated

--- Rotations/rogue_sub.lua
+++ Rotations/rogue_sub.lua
@@ -4,6 +4,10 @@ function rogue_sub(self)
 
    if jps.Interrupts and jps.should_kick("target") and cd("kick") == 0 then
       spell = "kick"
+   elseif cd("premeditation") == 0 and ub("player","stealth") then
+      spell = "premeditation"
+   elseif cd("premeditation") == 0 and ub("player","shadow dance") then
+      spell = "premeditation"
    elseif ub("player","stealth") and cd("shadowstep") == 0 then
       spell = "shadowstep"
    elseif ub("player","stealth") then
@@ -14,11 +18,9 @@ function rogue_sub(self)
       spell = "ambush"
    elseif not ub("player","stealth") and cd("shadow dance") == 0 and jps.UseCDs then
       spell = "shadow dance"
-   elseif not ub("player","stealth") and cd("vanish") == 0 and jps.UseCDs then
-      spell = "vanish"
-   elseif not ub("player","slice and dice") and cp >= 3 then
+   elseif not ub("player","slice and dice") and cp >= 4 then
       spell = "slice and dice"
-   elseif not ub("player","recuperate") and cp >= 3 then
+   elseif not ub("player","recuperate") and cp >= 4 then
       spell = "recuperate"
    elseif not ud("target","hemorrhage") then
       spell = "hemorrhage"
