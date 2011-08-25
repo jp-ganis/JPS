46b4396d0aad190a169df82183d917f631ec8bac
Jp <james.ganis@gmail.com>
2011-08-25 09:10:19 +0100

    More bear bug fixes. Name homogenization. No more 'get' anything in function names.

--- jpparse.lua
+++ jpparse.lua
@@ -1,21 +1,21 @@
 -- JPS Priority List Stuff
 --jpganis
-function ImReallySureICanCastThisShit(spell,unit)
+function ImReallySureICanCastThisShit( spell, unit )
 	if unit == nil then unit = "target" end
 	local _, spellID = GetSpellBookItemInfo(spell)
 	
 	if not UnitIsVisible(unit) then return false end
 	if not IsSpellKnown(spellID) then return false end
-	if jps.getCooldown(spell) ~= 0	then return false end
+	if jps.cooldown(spell) ~= 0	then return false end
 	if SpellHasRange(spell)==1 and IsSpellInRange(spell,unit)==0 then return false end
 	
 	return true
 end
 
-function parseSpellTable(hydra_table)
-	for index, table in pairs(hydra_table) do
-		spell = table[1]
-		conditions = table[2]
+function parseSpellTable( hydra_table )
+	for _, table in pairs(hydra_table) do
+		local spell = table[1]
+		local conditions = table[2]
 
 		-- Special Cases
 		-- Just refresh debuff/buff when it drops off.
@@ -35,9 +35,13 @@ function parseSpellTable(hydra_table)
 			conditions = true
 		end
 
-		-- Return
-		if conditions and ImReallySureICanCastThisShit(spell) then
-			return spell
+		-- Otherwise:
+		-- Return spell if conditions are true.
+		if conditions then
+			jps.Target = table[3]
+			if ImReallySureICanCastThisShit( spell,jps.Target ) then
+				return spell
+			end
 		end
 	end
 
