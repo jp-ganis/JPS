f1d732faaf033496f17b1dbc53cd1bfd2263787d
Jp <james.ganis@gmail.com>
2011-08-30 18:06:34 +0100

    New stuff for healing

--- jpparse.lua
+++ jpparse.lua
@@ -1,5 +1,7 @@
 -- JPS Priority List Stuff
 --jpganis
+
+-- canCast
 function ImReallySureICanCastThisShit( spell, unit )
 	if unit == nil then unit = "target" end
 	local _, spellID = GetSpellBookItemInfo(spell)
@@ -7,47 +9,89 @@ function ImReallySureICanCastThisShit( spell, unit )
 
 	if nomana then return false end
 	if not usable then return false end
+	if UnitIsDead(unit) then return false end
+	if UnitIsDeadOrGhost(unit) then return false end
 	if not UnitIsVisible(unit) then return false end
-	if not IsSpellKnown(spellID) then return false end
 	if jps.cooldown(spell) ~= 0	then return false end
+	if not IsSpellKnown(spellID) then return false end
 	if SpellHasRange(spell)==1 and IsSpellInRange(spell,unit)==0 then return false end
 	
 	return true
 end
 
-function parseSpellTable( hydraTable )
-	for _, table in pairs(hydraTable) do
-		local spell = table[1]
-		local conditions = table[2]
-		local stopCasting = table[4]
-		if type(table[3]) == "string" then jps.Target = table[3]
-		else jps.Target = nil end
+-- multiUnitTable
+function parseMultiUnitTable( spellTable )
+	local spell = spellTable[1]
+	local unitFunction = spellTable[2]
+	local targets = spellTable[3]
+	local conditions = spellTable[4]
+	local target = nil
+	local sirenTable = {}
 
-		-- Special Cases
+	for _, unit in pairs(targets) do
+		local unitTable = {}
+		table.insert( unitTable, 1, spell )
+		table.insert( unitTable, 2, unitFunction(unit) )
+		table.insert( unitTable, 3, unit )
+		table.insert( unitTable, 4, stopCasting )
+
+		table.insert( sirenTable, unitTable )
+	end
+
+	return parseSpellTable( sirenTable )
+end
+
+-- conditionsMatched
+function conditionsMatched( spell, conditions )
+	-- nil
+	if not spell or not conditions then
+		return false
+
+	-- refresh
+	elseif conditions == "refresh" then
+		if IsHarmfulSpell(spell) then return not jps.debuff( spell )
+		else return not jps.buff( spell ) end
+
+	-- onCD
+	elseif conditions == "onCD" then
+		return true
+
+	-- otherwise
+	else
+		return conditions
+
+	end
+end
+	
+
+-- Pick a spell from a priority table.
+function parseSpellTable( hydraTable )
+	for _, spellTable in pairs(hydraTable) do
+		local spell = spellTable[1]
+		local conditions = spellTable[2]
+		local target = nil
+
 		-- Nested table
 		if spell == "nested" and conditions then
-			spell =	parseSpellTable(table[3])
+			local newTable = spellTable[3]
+			spell,target = parseSpellTable( newTable )
 
-		-- Just refresh debuff/buff when it drops off.
-		elseif conditions == "refresh" then
-			if IsHarmfulSpell(spell) then
-				conditions = not jps.debuff( spell,jps.Target )
-			else
-				conditions = not jps.buff( spell,jps.Target )
-			end
-
-		-- onCD, instead of just "true"
-		elseif conditions == "onCD" then
-			conditions = true
+		-- MultiTarget List
+		elseif type(conditions) == "function" then
+			spell,target = parseMultiUnitTable( spellTable )
 		end
 
-		-- Otherwise:
-		-- Return spell if conditions are true.
-		if conditions and spell then
-			if ImReallySureICanCastThisShit( spell,jps.Target ) then
-				if stopCasting then SpellStopCasting() end
-				return spell
-			end
+		-- If not already assigned, assign target now.
+		if not target then
+			target = spellTable[3] end
+
+		-- Return spell if conditions are true and spell is castable.
+		if conditionsMatched( spell, conditions ) and ImReallySureICanCastThisShit( spell,target ) then
+
+			if stopCasting then SpellStopCasting() end
+			jps.Target = target
+			return spell,target
+
 		end
 	end
 
