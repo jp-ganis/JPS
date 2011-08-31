ffed2dd2eef5b75f07193be556dee6534f0e1c32
Jp <james.ganis@gmail.com>
2011-08-31 02:53:21 +0100

    bear threat changes

--- Rotations/hydraBear.lua
+++ Rotations/hydraBear.lua
@@ -1,50 +1,19 @@
 function hydraBear(self)
 	--Gocargo <3
 	-- Threat-Vars
-	local uTS = UnitThreatSituation
-	local focusShouldBeSaved = false
-	local focusThreatDuration = 0
-	local targetShouldBeTaunted = false
-	local targetThreatDuration = 0
+	local myTargetThreatStatus = UnitThreatSituation("player","target")
+	local myFocusThreatStatus = nil
 	local targetTargetTanking = ub("targettarget","bear form") or ub("targettarget","defensive stance") or ub("targettarget","blood presence") or ub("targettarget","righteous fury")
+
+	if UnitIsFriendly("focus") then
+		myFocusThreatStatus = UnitThreatSituation("focus") end
 
 	-- Other stuff
 	local rage = UnitMana("player")
 	local lacCount = jps.debuffStacks("lacerate")
 	local lacDuration = jps.debuffDuration("lacerate")
 	local hp = UnitHealth("player")/UnitHealthMax("player") * 100
-	local refresh = "refresh"
 	local onCD = "onCD"
-
-    --Taunt if not attacking me and if my focus target pulls agro
-    if UnitExists("focus") and UnitIsFriend("focus","player") then
-        if uTS("focus") ~= nil and uTS("focus") == 3 then
-            if focusThreatDuration == 0 then
-                focusThreatDuration = GetTime()
-            elseif GetTime()-focusThreatDuration > 2 and not focusShouldBeSaved then
-                focusShouldBeSaved = true
-                print("focus will be saved")
-            end 
-        elseif focusShouldBeSaved or focusThreatDuration > 0 then
-            focusShouldBeSaved = false
-            focusThreatDuration = 0 
-        end 
-    end 
-   
-	--Target Checks
-    if UnitExists("target") and UnitCanAttack("target","player") then
-        if uTS("player","target") and uTS("player","target") < 3 and not targetTargetTanking then
-            if targetThreatDuration == 0 then
-                targetThreatDuration = GetTime()
-            elseif GetTime()-targetThreatDuration > 0.5 and not targetShouldBeTaunted then
-                targetShouldBeTaunted = true
-                print("Taunting Target")
-            end 
-        elseif targetShouldBeTaunted or targetThreatDuration > 0 then
-            targetShouldBeTaunted = false
-            targetThreatDuration = 0 
-        end 
-    end 
 
 	-- Moves
 	local spellTable =
@@ -60,8 +29,8 @@ function hydraBear(self)
 		-- Offense
 		{"berserk",					jps.UseCDs },
 		-- Taunts
-		{"growl",					targetShouldBeTaunted },
-		{"challenging roar",		focusShouldBeSaved },
+		{"growl",					myTargetThreatStatus < 2 and not targetTargetTanking },
+		{"challenging roar",		focusThreatStatus > 1 },
 		-- Multi-Target
 		{"mangle(bear form)",		jps.buff("berserk") and jps.MultiTarget },
 		{"swipe(bear form)",		jps.MultiTarget },
