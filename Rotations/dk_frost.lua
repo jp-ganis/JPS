9591d3804fb650724e3a1b85c28f02b508696705
Jp <james.ganis@gmail.com>
2011-09-04 15:01:59 +0100

    dk update

--- Rotations/dk_frost.lua
+++ Rotations/dk_frost.lua
@@ -8,19 +8,19 @@ function dk_frost(self)
 		-- Kicks
 		{ "mind freeze",		jps.shouldKick() },
 		{ "mind freeze",		jps.shouldKick("focus"), "focus" },
-		{ "strangulate",		jps.shouldKick() and jps.UseCDs and jps.Strangulate and IsSpellInRange("strangulate","target")==0 and jps.LastCast ~= "mind freeze" },
-		{ "strangulate",		jps.shouldKick("focus") and jps.UseCDs and jps.Strangulate and IsSpellInRange("strangulate","focus")==0 and jps.LastCast ~= "mind freeze" , "focus" },
+		{ "Strangulate",		jps.shouldKick() and jps.UseCDs and IsSpellInRange("strangulate","target")==0 and jps.LastCast ~= "mind freeze" },
+		{ "Strangulate",		jps.shouldKick("focus") and jps.UseCDs and IsSpellInRange("strangulate","focus")==0 and jps.LastCast ~= "mind freeze" , "focus" },
 
 		-- Cooldowns
-		{ "pillar of frost",	jps.UseCDs and jps["Pillar of Frost"] },
-		{ "empower rune weapon",jps.UseCDs and jps["Empower Rune Weapon"] },
+		{ "Pillar of Frost",	jps.UseCDs and jps["Pillar of Frost"] },
+		{ "Empower Rune Weapon",jps.UseCDs and jps["Empower Rune Weapon"] },
 		{ jps.DPSRacial,		jps.UseCDs and jps["DPS Racial"]},
 
 		-- Defence
 		{ "raise dead",			jps.UseCDs and jps["Raise Dead (DPS)"] },
 		{ "raise dead",			jps.hp() < 0.4 and rp >= 50 and jps.UseCDs and jps["Raise Dead (Sacrifice)"] },
 		{ "death pact",			GetTotemTimeLeft(1) > 0 and jps.hp() < 0.4 and jps["Raise Dead (Sacrifice)"] },
-		{ "icebound fortitude",	jps.hp() < 0.5 and jps.UseCDs and jps["Icebound Fortitude"] },
+		{ "Icebound Fortitude",	jps.hp() < 0.5 and jps.UseCDs },
 
 		-- Horn of Winter
 		{ "horn of winter",		not jps.buff("horn of winter") },
@@ -33,7 +33,7 @@ function dk_frost(self)
 		{ "plague strike",		not jps.debuff("blood plague") },
 		{ "obliterate",			"onCD" },
 		{ "frost strike",		"onCD" },
-		{ "blood tap",			"onCD" and jps["Blood Tap"] },
+		{ "Blood Tap",			"onCD" },
 		
 		-- Refresh it while we're here
 		{ "horn of winter",		true },
