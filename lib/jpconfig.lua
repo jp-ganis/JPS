4ad0e279bb7105040bfea88ad5ad3672e1cd3cd0
Jp <james.ganis@gmail.com>
2011-09-04 02:05:21 +0100

    PvP Icon, more nice changes

---
+++ lib/jpconfig.lua
@@ -0,0 +1,59 @@
+-- Please work.
+function jps_createConfigFrame()
+	jpsConfigFrame = CreateFrame("Frame", "jpsConfigFrame", UIParent)
+	local w = 256
+	local h = 128
+
+	-- More Juked <3
+	jpsConfigFrame.name = "JPS Options Panel"
+	local title = jpsConfigFrame:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+	title:SetPoint("TOPLEFT", 20, -10) 
+	title:SetText("JPS")
+	local subtitle = jpsConfigFrame:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+	subtitle:SetHeight(32)
+	subtitle:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -8)
+	subtitle:SetPoint("RIGHT", jpsConfigFrame, -32, 0)
+	subtitle:SetNonSpaceWrap(true)
+	subtitle:SetJustifyH("LEFT")
+	subtitle:SetJustifyV("TOP")
+	subtitle:SetText("Configuration options for JPS")
+
+	-- <3 to Juked
+	-- Create checkboxes
+	local buttonPositionY = -60;
+	local buttonPositionX = 40;
+
+	local t = {}
+	for _,v in pairs(jps_saveVars) do
+		if type(v[2]) == "boolean" then
+			table.insert(t,v[1])
+		end
+	end
+
+	for i,v in ipairs (t) do
+		local JPS_IconOptions_CheckButton = CreateFrame("CheckButton", "JPS_Button_"..v, jpsConfigFrame, "OptionsCheckButtonTemplate");
+		JPS_IconOptions_CheckButton:SetPoint("TOPLEFT",buttonPositionX,buttonPositionY);
+		getglobal(JPS_IconOptions_CheckButton:GetName().."Text"):SetText(v);
+
+		local function JPS_IconOptions_CheckButton_OnClick()
+			if v == "PvP" then jps.togglePvP()
+			else jps[v] = not jps[v] end
+			jps_SAVE_VARIABLES()
+			jps_LOAD_VARIABLES()
+		end  
+
+		local function JPS_IconOptions_CheckButton_OnShow()
+			jps_SAVE_VARIABLES()
+			JPS_IconOptions_CheckButton:SetChecked(jpsDB[jpsRealm][jpsName][v]);
+		end  
+
+		JPS_IconOptions_CheckButton:RegisterForClicks("AnyUp");
+		JPS_IconOptions_CheckButton:SetScript("OnClick", JPS_IconOptions_CheckButton_OnClick);
+		JPS_IconOptions_CheckButton:SetScript("OnShow", JPS_IconOptions_CheckButton_OnShow);
+		buttonPositionY = buttonPositionY - 30;
+	end
+
+	-- Hide at load
+	InterfaceOptions_AddCategory(jpsConfigFrame)
+	jpsConfigFrame:Hide()
+end
