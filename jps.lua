e29360758c56465f78592830e8ff642177f11a69
Jp <james.ganis@gmail.com>
2011-09-07 19:47:59 +0100

    toc update

--- jps.lua
+++ jps.lua
@@ -1,9 +1,25 @@
--- JPS, PLua DPS Addon by jpganis
+--[[
+	 JPS - WoW Protected Lua DPS AddOn
+    Copyright (C) 2011 Jp Ganis
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+]]--
 -- Huge thanks to everyone who's helped out on this, <3
 -- Universal
 jps = {}
 jps.Version = "1.1.0"
-jps.Revision = "r267"
+jps.Revision = "r268"
 jps.RaidStatus = {}
 jps.UpdateInterval = 0.1
 jps.Combat = false
