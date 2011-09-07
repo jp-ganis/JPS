b5a71f85b1dc182d62d88cb87bae59ffa79ecc8b
Jp <james.ganis@gmail.com>
2011-09-07 19:47:59 +0100

    toc update

--- lib/jpconfig.lua
+++ lib/jpconfig.lua
@@ -1,4 +1,20 @@
--- Please work.
+--[[
+	 JPS - WoW Protected Lua DPS AddOn
+    Copyright (C) 2011 Jp Ganis
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+]]--
 function jps_createConfigFrame()
 	jpsConfigFrame = CreateFrame("Frame", "jpsConfigFrame", UIParent)
 	local w = 256
