c9998b08c5b70bdf1c20c09d8c67a1517f536537
Jp <james.ganis@gmail.com>
2011-11-07 02:00:47 +0000

    rotation updates

---
+++ Rotations/shaman_enhancement_pve.lua
@@ -0,0 +1,20 @@
+function new_shaman_enhancement(self)
+	local maelstromStacks = jps.buffStacks("maelstrom weapon")
+
+	local spellTable =
+	{
+		{ "wind shear", jps.shouldKick() },
+		{ "wind shear", jps.shouldKick("focus"), "focus" },
+		{ "shamanistic rage", jps.mana() < 0.2 },
+		{ "searing totem", GetTotemTimeLeft(1) < 2 },
+		{ "lava lash", "onCD" },
+		{ "flame shock", jps.buff("unleash flame") },
+		{ "lightning bolt", maelstromStacks == 5 },
+		{ "unleash elements", "onCD" },
+		{ "stormstrike", "onCD" },
+		{ "earth shock", "onCD" },
+		{ "feral spirit", "onCD" },
+	}
+
+	return parseSpellTable( spellTable )
+end
