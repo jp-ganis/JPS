b9a80b15613409634f3349a2e9206f7ad78aebc5
Jp <james.ganis@gmail.com>
2011-11-14 17:22:15 +0000

    sub rogue + ele updates

--- Rotations/rogue_sub.lua
+++ Rotations/rogue_sub.lua
@@ -1,34 +1,29 @@
 function rogue_sub(self)
    local cp = GetComboPoints("player")
+   local energy = UnitMana("player")
    local spell = nil
-
-   if jps.Interrupts and jps.shouldKick("target") and cd("kick") == 0 then
-      spell = "kick"
-   elseif cd("premeditation") == 0 and ub("player","stealth") then
-      spell = "premeditation"
-   elseif cd("premeditation") == 0 and ub("player","shadow dance") then
-      spell = "premeditation"
-   elseif ub("player","stealth") and cd("shadowstep") == 0 then
-      spell = "shadowstep"
-   elseif ub("player","stealth") then
-      spell = "garrote"
-   elseif ub("player","shadow dance") and cd("shadowstep") == 0 then
-      spell = "shadowstep"
-   elseif ub("player","shadow dance") then
-      spell = "ambush"
-   elseif not ub("player","stealth") and cd("shadow dance") == 0 and jps.UseCDs then
-      spell = "shadow dance"
-   elseif not ub("player","slice and dice") and cp >= 4 then
-      spell = "slice and dice"
-   elseif not ub("player","recuperate") and cp >= 4 then
-      spell = "recuperate"
-   elseif not ud("target","hemorrhage") then
-      spell = "hemorrhage"
-   elseif cp >= 4 then
-      spell = "eviscerate"
-   else
-      spell = "backstab"
+   if cp < 5 and ((jps.cd("shadow dance")==0 and energy < 85) or (jps.cd("vanish")==0) and energy < 60) then
+	   jps.write("Pooling energy")
+	   return nil
    end
 
-   return spell
+	local spellTable = 
+	{
+		{"shadow dance",	energy > 85 and cp < 5 and not jps.buff("stealth") },
+		{"vanish",			energy > 60 and cp <= 1 and jps.cd("shadowstep")==0 and not jps.buff("shadow dance") and not jps.buff("master of subtlety")},
+		{"shadowstep",		jps.buff("stealth") or jps.buff("shadowdance") },
+		{"premeditation",	cp <= 2 },
+		{"ambush",			cp <= 4 },
+		{"preparation",		jps.cd("vanish") > 60 },
+		{"slice and dice",	jps.buffDuration("Slice and dice") < 3 and cp == 5 },
+		{"rupture",			cp == 5 and not jps.debuff("rupture") },
+		{"recuperate",		cp == 5 and jps.buffDuration("recuperate") < 3 },
+		{"eviscerate",		cp == 5 and jps.debuffDuration("rupture") > 1 },
+		{"hemorrhage",		cp < 4 and energy > 40 and jps.debuffDuration("hemorrhage") < 4 },
+		{"hemorrhage",		cp < 5 and energy > 80 and jps.debuffDuration("hemorrhage") < 4 },
+		{"backstab",		cp < 4 and energy > 40 },
+		{"backstab",		cp < 5 and energy > 80 },
+	}
+
+   return parseSpellTable( spellTable )
 end
--- Rotations/shaman_elemental.lua
+++ Rotations/shaman_elemental.lua
@@ -1,43 +1,20 @@
 function shaman_elemental(self)
 	local spell = nil
+	local lsStacks = jps.buffStacks("lightning shield")
 	local mana = UnitMana("player")/UnitManaMax("player")
-	local speed = GetUnitSpeed("player")	 
-	local lsCount = jps.buffStacks("lightning shield")
-	local fsDuration = jps.debuffDuration("flame shock")
-	local _, fireTotemName, _, _, _ = GetTotemInfo(1) 
-	 
-	if jps.Interrupts and cd("wind shear") == 0 and jps.shouldKick("target") and mana > .4 then 
-		SpellStopCasting()
-		spell = "wind shear"
-	elseif jps.UseCDs and cd("blood fury") == 0 then
-		spell = "blood fury"
-	elseif lsCount < 2 then
-		spell = "lightning shield"
-	elseif fireTotemName == "" and cd("fire elemental totem") == 0 and jps.UseCDs then
-		spell = "fire elemental totem"
-	elseif fireTotemName == "" then
-		spell = "searing totem"
-	elseif lsCount >= 7 and fsDuration > 6 and cd("earth shock") == 0 then
-		spell = "earth shock"
-	elseif mana < 0.6 and cd("thunderstorm") == 0 then
-		spell = "thunderstorm"	 
-	elseif fsDuration < 2 and cd("flame shock") == 0 then
-		spell = "flame shock"
-	elseif jps.Moving and not ub("player","spiritwalker's grace") and cd("spiritwalker's grace") == 0 then
-		spell = "spiritwalker's grace"
-	elseif jps.Moving and not ub("player","spiritwalker's grace") then
-		spell = "lightning bolt"
-	elseif cd("lava burst") == 0 and cd("elemental mastery") == 0 and jps.UseCDs then
-		spell = "elemental mastery"		
-	elseif cd("lava burst") == 0 then
-		spell = "lava burst"
-	elseif cd("chain lightning") == 0 and jps.MultiTarget then
-		spell = "chain lightning"
-	else
-		spell = "lightning bolt"
-	end
 
-	if jps.Casting and spell ~= "spiritwalker's grace" then return nil end
+	local spellTable =
+	{
+		{ "elemental mastery",	"onCD" },
+		{ "flame shock",		jps.debuffDuration("flame shock") < 2 },
+		{ "lava burst",			"onCD" },
+		{ "earth shock",		lsStacks == 9 },
+		{ "earth shock",		lsStacks > 6 and jps.debuffDuration("flame shock") > 5 },
+		{ "spiritwalker's grace",	jps.Moving },
+		{ "chain lightning",	jps.MultiTarget },
+		{ "lightning bolt",		"onCD" },
+		{ "thunderstorm",		mana < .9 },
+	}
 
-	return spell
+	return parseSpellTable( spellTable )
 end
