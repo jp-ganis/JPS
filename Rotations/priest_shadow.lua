d42bbebd8d599a10c7e6f1e41fb4eb50f9c6714b
Jp <james.ganis@gmail.com>
2011-11-15 11:45:04 +0000

    shadow priest fixes

--- Rotations/priest_shadow.lua
+++ Rotations/priest_shadow.lua
@@ -6,17 +6,17 @@ function priest_shadow(self)
 	
 	local spellTable = 
 	{
-		{ "mind sear",			jps.Multi },
+		{ "mind sear",			jps.MultiTarget },
 		{ "mind blast",			"onCD" },
 		{ jps.DPSRacial,		"onCD" },
-		{ "shadow word: pain",	swpDuration < gcd + .5 },
+		{ "shadow word: pain",	swpDuration < gcd + .5 and jps.LastCast ~= "shadow word: pain"},
 		{ "devouring plague",	plagueDuration < gcd + 1.0 },
 		--actions+=/stop_moving,health_percentage<=25,if=cooldown.shadow_word_death.remains>=0.2|dot.vampiric_touch.remains<cast_time+2.5
-		{ "vampiric touch",		vtDuration < gcd + 2.5 },
+		{ "vampiric touch",		vtDuration < gcd + 2.5 and jps.LastCast ~= "vampiric touch"},
 		{ "archangel",			jps.buffStacks("dark evangelism") >= 5 and vtDuration > 5 and plagueDuration > 5 },
 		--actions+=/start_moving,health_percentage<=25,if=cooldown.shadow_word_death.remains<=0.1
 		{ "shadow word: death",	jps.hp("target") <= 0.25 },
-		{ "shadow fiend",		onCD },
+		{ "shadowfiend",		"onCD" },
 		{ "shadow word: death",	jps.mana() < 0.1 },
 		{ "mind flay",			"onCD" },
 		--actions+=/shadow_word_death,moving=1
