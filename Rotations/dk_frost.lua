c1745c80246ec7805398b42eedc023323eaec290
Jp <james.ganis@gmail.com>
2011-11-26 22:35:41 +0000

    SIMCRAFT

--- Rotations/dk_frost.lua
+++ Rotations/dk_frost.lua
@@ -8,12 +8,12 @@ function dk_frost(self)
 	local fr2 = select(3,GetRuneCooldown(4))
 	local ur1 = select(3,GetRuneCooldown(5))
 	local ur2 = select(3,GetRuneCooldown(6))
-	local 1dr = dr1 or dr2
-	local 2dr = dr1 and dr2
-	local 1fr = fr1 or fr2
-	local 2fr = fr1 and fr2
-	local 1ur = ur1 or ur2
-	local 2ur = ur1 and ur2
+	local one_dr = dr1 or dr2
+	local two_dr = dr1 and dr2
+	local one_fr = fr1 or fr2
+	local two_fr = fr1 and fr2
+	local one_ur = ur1 or ur2
+	local two_ur = ur1 and ur2
 	local ff_dur = jps.debuffDuration("frost fever")
 	local bp_dur = jps.debuffDuration("blood plague")
 
@@ -36,16 +36,16 @@ function dk_frost(self)
 		-- Mofes
 		{ "howling blast",		ff_dur <= 2 },
 		{ "plague strike",		bp_dur <= 2 },
-		{ "obliterate",			1dr and 1ur and 1fr },
-		{ "obliterate",			(2dr and 2fr) or (2dr and 2ur) or (2fr and 2ur) },
+		{ "obliterate",			one_dr and one_ur and one_fr },
+		{ "obliterate",			(two_dr and two_fr) or (two_dr and two_ur) or (two_fr and two_ur) },
 		{ "frost strike",		rp > 110 },
 		{ "howling blast",		jps.buff("Freezing Fog") },
-		{ "obliterate",			2dr or 2ur or 2fr },
+		{ "obliterate",			two_dr or two_ur or two_fr },
 		{ "frost strike",		rp > 100 },
 		{ "obliterate",			"onCD" },
 		{ "frost strike",		"onCD" },
 		{ "howling blast",		"onCD" },
-		{ "Empower Rune Weapon",jps.UseCDs and not (1dr or 1fr or 1ur) },
+		{ "Empower Rune Weapon",jps.UseCDs and not (one_dr or one_fr or one_ur) },
 		
 		-- Refresh it while we're here
 		{ "horn of winter",		"onCD" },
