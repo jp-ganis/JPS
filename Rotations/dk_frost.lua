7e0cb186642da20e865a72b00543d05cc8070e8e
Jp <james.ganis@gmail.com>
2011-11-26 12:53:26 +0000

    SIMCRAFT: frostdk

--- Rotations/dk_frost.lua
+++ Rotations/dk_frost.lua
@@ -1,7 +1,20 @@
 --jpganis
-
 function dk_frost(self)
 	local rp = UnitPower("player")
+	local dr1 = select(3,GetRuneCooldown(1))
+	local dr2 = select(3,GetRuneCooldown(2))
+	local fr1 = select(3,GetRuneCooldown(3))
+	local fr2 = select(3,GetRuneCooldown(4))
+	local ur1 = select(3,GetRuneCooldown(5))
+	local ur2 = select(3,GetRuneCooldown(6))
+	local 1dr = dr1 or dr2
+	local 2dr = dr1 and dr2
+	local 1fr = fr1 or fr2
+	local 2fr = fr1 and fr2
+	local 1ur = ur1 or ur2
+	local 2ur = ur1 and ur2
+	local ff_dur = jps.debuffDuration("frost fever")
+	local bp_dur = jps.debuffDuration("blood plague")
 
 	local spellTable = 
 	{
@@ -12,39 +25,36 @@ function dk_frost(self)
 		{ "Strangulate",		jps.shouldKick("focus") and jps.UseCDs and IsSpellInRange("mind freeze","focus")==0 and jps.LastCast ~= "mind freeze" , "focus" },
 
 		-- Cooldowns
-		{ "Pillar of Frost",	jps.UseCDs and jps["Pillar of Frost"] },
-		{ "Empower Rune Weapon",jps.UseCDs and jps["Empower Rune Weapon"] },
+		{ "Pillar of Frost",	jps.UseCDs },
 		{ jps.DPSRacial,		jps.UseCDs and jps["DPS Racial"]},
-
-		-- Defence
+		{ "Blood Tap",			not (dr1 or dr2) },
 		{ "raise dead",			jps.UseCDs and jps["Raise Dead (DPS)"] },
-		{ "raise dead",			jps.hp() < 0.4 and rp >= 50 and jps.UseCDs and jps["Raise Dead (Sacrifice)"] },
-		{ "death pact",			GetTotemTimeLeft(1) > 0 and jps.hp() < 0.4 and jps["Raise Dead (Sacrifice)"] },
-		{ "Icebound Fortitude",	jps.hp() < 0.5 and jps.UseCDs },
-
-		-- Horn of Winter
-		{ "horn of winter",		not jps.buff("horn of winter") },
-
+		{ "outbreak",			ff_dur <= 2 or bp_dur <= 2 },	
 		-- AoE
 		{ "death and decay",	jps.MultiTarget },
-
 		-- Mofes
-		{ "howling blast",		not jps.debuff("frost fever") or jps.buff("freezing fog")},
-		{ "plague strike",		not jps.debuff("blood plague") },
+		{ "howling blast",		ff_dur <= 2 },
+		{ "plague strike",		bp_dur <= 2 },
+		{ "obliterate",			1dr and 1ur and 1fr },
+		{ "obliterate",			(2dr and 2fr) or (2dr and 2ur) or (2fr and 2ur) },
+		{ "frost strike",		rp > 110 },
+		{ "howling blast",		jps.buff("Freezing Fog") },
+		{ "obliterate",			2dr or 2ur or 2fr },
+		{ "frost strike",		rp > 100 },
 		{ "obliterate",			"onCD" },
 		{ "frost strike",		"onCD" },
-		{ "Blood Tap",			"onCD" },
+		{ "howling blast",		"onCD" },
+		{ "Empower Rune Weapon",jps.UseCDs and not (1dr or 1fr or 1ur) },
 		
 		-- Refresh it while we're here
-		{ "horn of winter",		true },
+		{ "horn of winter",		"onCD" },
 	}
 
 	local spell = parseSpellTable( spellTable )
 
 	if spell == "death and decay" then
 		jps.Cast( spell )
-		CameraOrSelectOrMoveStart()
-		CameraOrSelectOrMoveStop()
+		jps.GroundClick()
 		spell = nil
 	end
 
