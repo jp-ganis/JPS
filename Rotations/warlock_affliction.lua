2dd71800089c9ff85b4aee5cab54b4b1f34fa2d9
Jp <james.ganis@gmail.com>
2011-11-29 21:20:13 +0000

    SIMCRAFT

--- Rotations/warlock_affliction.lua
+++ Rotations/warlock_affliction.lua
@@ -16,62 +16,24 @@ function warlock_affliction(self)
 		focus_bane = jps.debuffDuration("bane of agony","focus")
 	end
 
-	if not ud("target","curse of the elements") then
-		spell = "curse of the elements"
-	-- Opening
-	elseif jps.Opening and not jps.Casting then
-		if not ud("target","shadow and flame") and jps.LastCast ~= "shadow bolt" then
-			spell = "shadow bolt"
-		elseif cd("haunt") == 0 then
-			spell = "haunt"
-		elseif cd("demon soul") == 0 then
-			spell = "demon soul"
-		elseif not ud("target","bane of doom") then
-			spell = "bane of doom"
-		elseif not ud("target","corruption") then
-			spell = "corruption"
-		elseif not ud("target","unstable affliction") and jps.LastCast ~= "unstable affliction" then
-			spell = "unstable affliction"
-		else
-			spell = "drain life"
-			jps.Opening = false
-		end
-	elseif not jps.Casting then
-	-- Standard
-		if cd("haunt") == 0 and not jps.Moving then
-			spell = "haunt"
-		elseif cd("demon soul") == 0 then
-			spell = "demon soul"
-		elseif bod_duration < 15 then
-			spell = "bane of doom"
-		elseif not ud("target","shadow and flame") and jps.LastCast ~= "shadow bolt" then
-			spell = "shadow bolt"
-		elseif ub("player","shadow trance") then
-			spell = "shadow bolt"
-		elseif cpn_duration < 1.5 then
-			spell = "corruption"
-		elseif jps.Moving then
-			spell = "fel flame"
-		elseif ua_duration < 1.5 and jps.LastCast ~= "unstable affliction" then
-			spell = "unstable affliction"
-		elseif UnitHealth("target")/UnitHealthMax("target") < 0.25 then
-			spell = "drain soul"
-		elseif focus_dotting and focus_corruption < 1.5 then
-			spell = "corruption"
-			jps.Target = "focus"
-		elseif focus_dotting and focus_bane < 1.5 then
-			spell = "bane of agony"
-			jps.Target = "focus"
-		elseif focus_dotting and focus_ua < 1.5 then
-			spell = "unstable affliction"
-			jps.Target = "focus"
-		elseif cd("Shadowflame") == 0 and IsShiftKeyDown() then
-			spell = "shadowflame"
-		elseif mana < 0.5 then
-			spell = "life tap"
-		else
-			spell = "drain life"
-		end
-	end
-	return spell
-end   
+	local spellTable =
+	{
+		{ "demon soul" },
+		{ "corruption", cpn_duration < 3 },
+		{ "unstable affliction", ua_duration < 4.5 },
+		{ "bane of doom", bod_duration == 0 },
+		{ "haunt" },
+		{ "summon doomguard" },
+		{ "drain soul", jps.hp("target") <= 0.25 },
+		{ "shadowflame", IsShiftKeyDown() },
+		{ "life tap", jps.mana() <= 0.35 },
+		{ "soulburn", not jps.buff("demon soul: felhunter") },
+		{ "soulfire", jps.buff("soulburn") },
+		{ "shadow bolt" },
+		{ "life tap", jps.Moving and jps.mana() < 0.8 and jps.mana() < jps.hp("target") },
+		{ "fel flame", jps.Moving },
+		{ "life tap" },
+	}
+
+	return parseSpellTable(spellTable)
+end
