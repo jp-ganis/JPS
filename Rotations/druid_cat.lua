2f9ea0c17c9032d1b00d4d52f5459466c2adab78
Jp <james.ganis@gmail.com>
2011-12-12 23:24:17 +0000

    ravage fixed

--- Rotations/druid_cat.lua
+++ Rotations/druid_cat.lua
@@ -18,46 +18,47 @@ function druid_cat(self)
 
 	local spellTable =
 	{
-		{nil,					IsSpellInRange("shred","target")==0 },
+		{ nil,					IsSpellInRange("shred","target") == 0 },
 		--
-		{"tiger's fury", 		IsSpellInRange("shred","target") and energy <= 35 and not clearcasting and gcdLocked },
+		{ "tiger's fury", 		IsSpellInRange("shred","target") and energy <= 35 and not clearcasting and gcdLocked },
+		{{"macro","/use 14"},	jps.itemCooldown(77113) == 0 and jps.buff("tiger's fury") and jps.UseCDs },
 		--
-		{"berserk", 			jps.UseCDs and jps.buff("tiger's fury") },
-		{jps.DPSRacial,			jps.LastCast == "berserk" },
+		{ "berserk", 			jps.UseCDs and jps.buff("tiger's fury") },
+		{ jps.DPSRacial,			jps.LastCast == "berserk" },
 		--
-		{"skull bash(cat form)",jps.shouldKick() and jps.Interrupts },
-		{"swipe",				jps.MultiTarget },
+		{ "skull bash(cat form)",jps.shouldKick() and jps.Interrupts },
+		{ "swipe",				jps.MultiTarget },
 		--
-		{nil,					gcdLocked or IsSpellInRange("shred","target") == 0 },
+		{ nil,					gcdLocked },
 		--
-		{"faerie fire (feral)", not jps.debuff("faerie fire") and jps.debuffStacks("sunder armor")~=3 },
+		{ "faerie fire (feral)", not jps.debuff("faerie fire") and jps.debuffStacks("sunder armor")~=3 },
 		--
-		{"mangle(cat form)", 	mangleDuration < 2 and not jps.debuff("trauma") and not jps.debuff("hemorrhage") },
+		{ "mangle(cat form)", 	mangleDuration < 2 and not jps.debuff("trauma") and not jps.debuff("hemorrhage") },
 		--
-		{"ravage", 				jps.buff("stampede") and (jps.buffDuration("stampede") < 4) },
+		{ {"macro","/cast ravage"}, 				jps.buff("stampede") and jps.buffDuration("stampede") < 4 },
 		--
-		{"ferocious bite", 		executePhase and cp == 5 and ripDuration > 0 },
-		{"ferocious bite", 		executePhase and cp > 0 and ripDuration <= 2.1 },
+		{ "ferocious bite", 		executePhase and cp == 5 and ripDuration > 0 },
+		{ "ferocious bite", 		executePhase and cp > 0 and ripDuration <= 2.1 and ripDuration > 0 },
 		--
-		{"rip", 				cp == 5 and ripDuration < 2 and (berserking or ripDuration+2 <= tfCD) },
+		{ "rip", 				cp == 5 and ripDuration < 2 and (berserking or ripDuration+2 <= tfCD) },
 		--
-		{"ferocious bite",		berserking and cp == 5 and ripDuration > 5 and srDuration > 3 },
+		{ "ferocious bite",		berserking and cp == 5 and ripDuration > 5 and srDuration > 3 },
 		--
-		{"rake", 				jps.buff("tiger's fury") and rakeDuration < 9 },
-		{"rake", 				rakeDuration < 3 and (berserking or rakeDuration-0.8 <= tfCD or energy >= 71) },
+		{ "rake", 				jps.buff("tiger's fury") and rakeDuration < 9 },
+		{ "rake", 				rakeDuration < 3 and (berserking or rakeDuration-0.8 <= tfCD or energy >= 71) },
 		--
-		{"shred",				jps.buff("clearcasting") },
+		{ "shred",				jps.buff("clearcasting") },
 		--
-		{"savage roar",			cp > 0 and srDuration < 1 and ripDuration > 6 },
+		{ "savage roar",			cp > 0 and srDuration < 1 and ripDuration > 6 },
 		--
-		{"ferocious bite",		(not berserking or energy < 25) and cp == 5 and ripDuration >= 14 and srDuration >= 10 },
+		{ "ferocious bite",		(not berserking or energy < 25) and cp == 5 and ripDuration >= 14 and srDuration >= 10 },
 		--
-		{"ravage", 				jps.buff("stampede") and not clearcasting and (energy <= 100-energyPerSec) },
+		{ {"macro","/cast ravage"}, 				jps.buff("stampede") and not clearcasting and (energy <= 100-energyPerSec) },
 		--
-		{"shred", 				berserking or tfUp },
-		{"shred",				(cp < 5 and ripDuration <= 3) or (cp == 0 and srDuration <= 2) },
-		{"shred",				tfCD <= 3 },
-		{"shred",				energy >= 100 - energyPerSec },
+		{ "shred", 				berserking or tfUp },
+		{ "shred",				(cp < 5 and ripDuration <= 3) or (cp == 0 and srDuration <= 2) },
+		{ "shred",				tfCD <= 3 },
+		{ "shred",				energy >= 100 - energyPerSec },
 	}
 
 	return parseSpellTable(spellTable)
