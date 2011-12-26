997b990516f7b6300f880c9e92e6c1c50c524806
Jp <james.ganis@gmail.com>
2011-12-26 01:11:22 +0000

    Cooldowns now properly limited

--- Rotations/paladin_ret.lua
+++ Rotations/paladin_ret.lua
@@ -6,9 +6,9 @@ function paladin_ret(self)
 	{
 		{ "rebuke", jps.Interrupts and jps.shouldKick() },
 		{ "judgement", not jps.buff("judgements of the pure") },
-		{ "zealotry" },
-		{ "guardian of ancient kings", (jps.buffDuration("zealotry") < 31 and jps.buff("zealotry")) or jps.cd("zealotry")>60 },
-		{ "avenging wrath", jps.buffDuration("zealotry") < 21 and jps.buff("zealotry") },
+		{ "zealotry", jps.UseCDs },
+		{ "guardian of ancient kings", ((jps.buffDuration("zealotry") < 31 and jps.buff("zealotry")) or jps.cd("zealotry")>60) and jps.UseCDs },
+		{ "avenging wrath", jps.buffDuration("zealotry") < 21 and jps.buff("zealotry") and jps.UseCDs },
 		{ "inquisition", jps.buffDuration("inquisition") < 5 and (hPower == 3 or jps.buff("divine purpose")) },
 		{ "crusader strike", hPower < 3 },
 		{ "templar's verdict", jps.buff("divine purpose") },
