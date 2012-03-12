fc2e660dc2bb2e8d18cec1de5d35222a03a16321
Your Name <tropicdome@hotmail.com>
2012-03-12 07:17:39 +0100

    Resto druid + Holy pals updates

--- Rotations/paladin_holy.lua
+++ Rotations/paladin_holy.lua
@@ -1,4 +1,8 @@
 function paladin_holy(self)
+	-- INFO
+	-- 3 holy power + right mouse button to use Ligh of Dawn
+	-- CDs: Lay on Hands, Divine Shield, Divine Protection, Avenging Wrath, Divine Favor
+	
 	--healer
 	local holyPower = UnitPower("player",9)
 	
@@ -38,9 +42,12 @@ function paladin_holy(self)
       cleanseTarget = jps.FindMeADispelTarget({"Poison"},{"Disease"})
     end
 
-	local defaultTarget = jps.lowestFriendly()
+	--Default to healing lowest partymember
+	local defaultTarget = jps.lowestInRaidStatus()
+--	local defaultTarget = jps.lowestFriendly()
 
-    if UnitExists(tank) and jps.hpInc(tank) < 0.4 then defaultTarget = tank end
+	--Check that the tank isn't going critical, and that I'm not about to die
+    if jps.canHeal(tank) and jps.hpInc(tank) <= 0.5 then defaultTarget = tank end
 	if jps.hpInc(me) < 0.2 then	defaultTarget = me end
 
 	local defaultHP = jps.hpInc(defaultTarget)
@@ -48,24 +55,25 @@ function paladin_holy(self)
 	local spellTable =
 	{
 		{ "lay on hands",		defaultHP < 0.05 and jps.UseCDs, defaultTarget },
-		{ "hand of protection",	defaultHP < 0.15 and jps.UseCDs, defaultTarget },
+--		{ "hand of protection",	defaultHP < 0.15 and jps.UseCDs, defaultTarget }, -- Not sure it is a good to have, since if the tank gets it, he will loose aggro
         { "divine shield",	    jps.hp() < 0.30 and jps.UseCDs and not jps.buff("Forbearance", me), me }, --can be optimized by using on specific boss mechanics
         { "divine protection",	jps.hp() < 0.50 and jps.UseCDs, me },
-        { "seal of insight",	"refresh" },
+        { "seal of insight",	not jps.buff("seal of insight") }, 
         { "beacon of light",	jps.buffDuration("beacon of light",tank) < 30, tank },
-        { "cleanse",			cleanseTarget~=nil, cleanseTarget },
+--        { "cleanse",			cleanseTarget~=nil, cleanseTarget },
         { "rebuke",			    jps.shouldKick("target") and IsSpellInRange("Rebuke", "target"), "target" },
-        { "judgement",			UnitExists(judgeTarget), judgeTarget },
-        { "crusader strike",	UnitExists(crusaderTarget) and holyPower < 3, crusaderTarget },
-        { "divine favor",	    UnitExists(focus) and jps.Combat and jps.hp("focus") < 0.9 and UnitClassification("focustarget")=="worldboss", me },
-        { "avenging wrath",	    UnitExists(focus) and jps.Combat and jps.hp("focus") < 0.9 and UnitClassification("focustarget")=="worldboss", me },
-        { "flash of light",		defaultHP < 0.35, defaultTarget },
-        { "light of dawn",		holyPower == 3 and IsMouseButtonDown(2) },  -- only when right mouse button is pressed
-        { "holy shock",			defaultHP < 0.8 and holyPower < 3, defaultTarget },
+        { "judgement",			not jps.buff("Judgements of the Pure"), judgeTarget }, -- No need to judge all the time, only once per min for hast buff 
+--        { "crusader strike",	UnitExists(crusaderTarget) and holyPower < 3, crusaderTarget }, -- More for PvP use
+        { "light of dawn",		holyPower == 3 and IsMouseButtonDown(2) ~= nil },  -- only when right mouse button is pressed
         { "word of glory",		defaultHP < 0.8 and holyPower == 3, defaultTarget },
-        { "divine light",		defaultHP < 0.5, defaultTarget },
-        { "holy light",			defaultHP < 0.7, defaultTarget },
-        { "holy radiance",		jps.MultiTarget and jps.UseCDs },
+        { "avenging wrath",	    jps.UseCDs },
+        { "holy shock",			defaultHP < 0.94 and holyPower < 3, defaultTarget },
+        { "divine favor",	    jps.UseCDs },
+        { "holy radiance",		defaultHP < 0.95 and jps.MultiTarget, defaultTarget },
+-- { "guardian of ancient kings", 	}, -- TODO
+        { "flash of light",		defaultHP < 0.3, defaultTarget },
+        { "divine light",		defaultHP < 0.75, defaultTarget },
+        { "holy light",			defaultHP < 0.95, defaultTarget },
 	}
 
 	local spell,target = parseSpellTable(spellTable)
