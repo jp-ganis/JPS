ae79b7a030f11381af2b66c012298374d2d59bdf
Jp Ganis <s0789747@sms.ed.ac.uk>
2012-03-18 02:36:12 +0000

    no ooc and innervate

--- jps.lua
+++ jps.lua
@@ -172,7 +172,7 @@ function combatEventHandler(self, event, ...)
  	-- table.wipe(jps.RaidStatus)
  		
         local unit = ...
-        if jps.canHeal(unit) and jps.Enabled then combat() end -- and jps.Combat
+        if jps.canHeal(unit) and jps.Enabled and jps.Combat then combat() end -- and jps.Combat
 		if jps.canHeal(unit) and jps.hpInc(unit,"absolute") < UnitHealthMax(unit) then
 			local unitSubGroup = jps.findSubGroupUnit(unit)
 			hp = jps.hpInc(unit)
