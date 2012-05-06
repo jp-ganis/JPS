22339935d36030e1883e815f1bd94e47c0c3828a
htordeux <h.tordeux@laposte.net>
2012-05-07 00:42:01 +0300

    Update Rotations/warrior_prot.lua

--- Rotations/warrior_prot.lua
+++ Rotations/warrior_prot.lua
@@ -18,6 +18,7 @@ local stackThunder = jps.buffStacks("Thunderstruck")
 		{ "Shield Wall" , UnitHealth('player')<30000 , "target" },
 		{ "Last Stand" , UnitHealth('player')<30000 , "target" },
 		{ "Enraged Regeneration" , playerHealth<0.60 , "target" },
+		{ "Lifeblood" , "onCD" , "player" },
 		{ "Shield Block" , playerHealth<0.60 , "target" },
 		{ "Charge" , jps.UseCDs , "target" },
 		{ "Heroic Throw" , "onCD" , "target" },
@@ -25,52 +26,22 @@ local stackThunder = jps.buffStacks("Thunderstruck")
 		{ "Pummel" , jps.shouldKick("target") , "target" },
 		{ "Spell Reflection" , (UnitCastingInfo("target") or UnitChannelInfo("target")) , "target" },
 		{ "Victory Rush" , ub("player","Victorious") , "target" },
+		{ "Shockwave" , jps.MultiTarget and stackThunder>1 , "target" },
+		{ "Thunder Clap" , jps.MultiTarget , "target" },
 		{ "Devastate" , not ud("target","Sunder Armor") , "target" },
 		{ "Shield Slam" , "onCD" , "target" },
 		{ "Devastate" , stackSunder<3 , "target" },
 		{ "Devastate" , ub("player","Sword and Board") , "target" },
 		{ "Revenge" , "onCD" , "target" },
-		{ "Shockwave" , jps.MultiTarget and stackThunder>1 , "target" },
-		{ "Thunder Clap" , jps.MultiTarget , "target" },
-		{ "Lifeblood" , "onCD" , "player" },
 		{ "Heroic Strike" , ub("player","Incite") , "target" },
 		{ "Heroic Strike" , nPower>70 , "target" },
 		{ "Concussion Blow" , "onCD" , "target" },
 		{ "Devastate" , "onCD" , "target" },
 		{ "Rend" , not UnitDebuff("target","Rend") , "target" },
-		{ {"macro","/startattack"} , nil , "target" },
-
+		{ {"macro","/startattack"}, nil, "target" },
 	}
 
 	local spell,target = parseSpellTable(spellTable)
 	jps.Target = target
 	return spell
 end
-
---[[
-Renvoi de sort - Spell Reflection
-Ivresse de la victoire - Victory Rush
-Victorieux - Victorious
-Provocation - Taunt
-Fracasser armure - Sunder Armor
-Emulation - Incite
-Fracasser armure - Sunder Armor
-Mur protecteur - Shield Wall
-Dernier rempart - Last Stand
-Charge - Charge
-Lancer héroïque - Heroic Throw
-Dévaster - Devastate
-Heurt de bouclier - Shield Slam
-Epée et bouclier - Sword and Board
-Volée de coups - Pummel
-Maîtrise du blocage	 - Shield Block
-Frappe héroïque - Heroic Strike
-Sang-de-vie - Lifeblood
-Régénération enragée - Enraged Regeneration
-Coup traumatisant - Concussion Blow
-Pourfendre - Rend
-Onde de choc - Shockwave
-Foudroyé - Thunderstruck
-Coup de tonnerre - Thunder Clap
-Revanche - Revenge
-]]
