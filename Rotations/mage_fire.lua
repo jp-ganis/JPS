33fe946ee054033317cbf8bb3aeff93a5c22cc29
pcmd git <info@livefast.de>
2012-09-05 22:21:22 +0200

    mage fire rotation: indent, flamestrike, dps racial stuff

--- Rotations/mage_fire.lua
+++ Rotations/mage_fire.lua
@@ -4,30 +4,38 @@ function mage_fire(self)
 	local spellTable = 
 	{
 	   --interrupt
-		{ "Counterspell", jps.Interrupts and jps.shouldKick("target"), "target" },
-		{ "Ice Barrier", (UnitHealth("player") / UnitHealthMax("player") < 0.40)  and not jps.buff("Ice Barrier","player"), "player" },
+		{ "Counterspell",     jps.Interrupts and jps.shouldKick("target"), "target" },
+		{ "Ice Barrier",      (UnitHealth("player") / UnitHealthMax("player") < 0.40)  and not jps.buff("Ice Barrier","player"), "player" },
 		
-		--deff
-		{ "Molten Armor", not jps.buff("Molten Armor","player"), "player" },
+		--buffs
+		{ "Molten Armor",     not jps.buff("Molten Armor","player"), "player" },
+		{ "Arcane Brilliance",     not jps.buff("Arcane Brilliance","player"), "player" },
 		
 		--aoe
-		{ "Dragon's Breath", CheckInteractDistance("target", 3) == 1, "target" }, 
-		--{ "Flamestrike",	jps.MultiTarget }, --need groundClick demonstration
+		{ "Dragon's Breath",  CheckInteractDistance("target", 3) == 1, "target" }, 
+		{ "Flamestrike",      jps.MultiTarget },
 		
 		--dots & opener
-		{ "Combustion", jps.debuffDuration("Ignite") > 0 and jps.debuffDuration("Pyroblast") > 0  and jps.UseCDs, "target" },
-		{ "Mirror Image", jps.UseCDs },
-		--{ "Living Bomb", jps.debuffDuration("Living Bomb") == 0 , "target" },
-		{ "Frost Bomb", jps.debuffDuration("Frost Bomb") == 0, "target" }, --depending on your talent tree
+		{ "Combustion",       jps.debuffDuration("Ignite") > 0 and jps.debuffDuration("Pyroblast") > 0  and jps.UseCDs, "target" },
+		{ "Mirror Image",     jps.UseCDs },
+		{ jps.DPSRacial,    jps.UseCDs and jps["DPS Racial"]},
+		--{ "Living Bomb",    jps.debuffDuration("Living Bomb") == 0 , "target" },
+		{ "Frost Bomb",       jps.debuffDuration("Frost Bomb") == 0, "target" }, --depending on your talent tree
 		
 		--rotation
-		{ "Inferno Blast", jps.buff("Heating Up","player"), "target" },
-		{ "Pyroblast", jps.buff("Pyroblast!","player"), "target" },
-		{ "Scorch", jps.Moving, "target" },
-		{ "Fireball", "onCD", "target" },
+		{ "Inferno Blast",    jps.buff("Heating Up","player"), "target" },
+		{ "Pyroblast",        jps.buff("Pyroblast!","player"), "target" },
+		{ "Scorch",           jps.Moving, "target" },
+		{ "Fireball",         "onCD", "target" },
 		
 		
 	}
+   local spell,target = parseSpellTable(spellTable)
+   if spell == "Flamestrike" then
+       jps.Cast( spell )
+       jps.groundClick()
+   end
 
-   return parseSpellTable(spellTable)
+   jps.Target = target
+   return spell
 end
