c21060663666d7198c45028b9b01b72e9c2ee3c8
jp-ganis <james.ganis@gmail.com>
2012-09-06 11:05:20 +0200

    5.0.4

--- Rotations/warlock_demo.lua
+++ Rotations/warlock_demo.lua
@@ -1,9 +1,8 @@
 function warlock_demo(self)
---latitude
+
 if UnitCanAttack("player","target")~=1 or UnitIsDeadOrGhost("target")==1 then return end
 
    local mana = UnitMana("player")/UnitManaMax("player")
-   local bod_duration = jps.debuffDuration("bane of doom")
    local cpn_duration = jps.debuffDuration("corruption")
    local cur_duration = jps.debuffDuration("curse of the elements")
    local meta_duration = jps.buffDuration("metamorphosis")
@@ -17,8 +16,9 @@ if UnitCanAttack("player","target")~=1 or UnitIsDeadOrGhost("target")==1 then re
    
    { "curse of the elements", cur_duration == 0 },
    { "fel flame", currentSpeed > 0 },
-
-   { {"macro","/cast Dark Soul: Knowledge"}, jps.cooldown("Dark Soul: Knowledge") == 0 and jps.UseCDs },
+   
+   { {"macro","/cast Dark Soul: Knowledge"}, jps.cooldown("Dark Soul: Knowledge") == 0 and jps.UseCDs and jps.Opening },
+   { {"macro","/cast Dark Soul: Knowledge"}, jps.cooldown("Dark Soul: Knowledge") == 0 and jps.UseCDs and dpower > 780 },
    { "hand of gul'dan" },
    { {"macro","/use 10"}, jps.glovesCooldown() == 0 and jps.UseCDs },
    { jps.DPSRacial, jps.UseCDs },
@@ -29,21 +29,21 @@ if UnitCanAttack("player","target")~=1 or UnitIsDeadOrGhost("target")==1 then re
    { {"macro","/cast felstorm"}, jps.cooldown("felstorm") == 0 },
    { {"macro","/cast Grimoire: Felguard"}, jps.cooldown("Grimoire: Felguard") == 0 and not jps.buff("metamorphosis") and jps.UseCDs },
    
-   ----- meta only for opening -----
+   ----- meta pull -----
    { "metamorphosis", jps.Opening },
-
    
    ---meta cycle ----
    { "metamorphosis", dpower > 800 },
-   { "bane of doom", jps.buff("metamorphosis") and bod_duration < 1 },
-   { "touch of chaos", jps.buff("metamorphosis") },
+   
+   { {"macro","/cast doom"}, jps.buff("metamorphosis") and jps.buffDuration("doom") <= 30 },
+   { {"macro","/cast touch of chaos"}, jps.buff("metamorphosis") },
    
    
    -- forme humaine --
    
-   { "soul fire", jps.buff("molten core") and jps.buffDuration("molten core") > 2},
-   { "soul fire", jps.hp("target") <= 0.25 },
-   { "shadow bolt" },
+   { "soul fire", jps.buff("molten core") and jps.buffDuration("molten core") > 2 and not jps.buff("metamorphosis") },
+   { "soul fire", jps.hp("target") <= 0.25 and not jps.buff("metamorphosis") },
+   { "shadow bolt", not jps.buff("metamorphosis") },
    
    
    }
