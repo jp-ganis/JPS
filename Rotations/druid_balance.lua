98cb1c0cab583718960ff2a261c5b53d63065456
jp-ganis <james.ganis@gmail.com>
2012-09-06 10:55:17 +0200

    Update Rotations/druid_balance.lua
    5.0.4 (balance power tracker no longer provides the spell info - but also is no longer needed really in 5.0)

--- Rotations/druid_balance.lua
+++ Rotations/druid_balance.lua
@@ -1,13 +1,7 @@
 -- jpganis
 -- simcrafty
 --TODO: add tab-dotting everything.
--- thanks to balance power tracker for making this SO much better
---function druid_balance(self)
---	if BalancePowerTracker_SharedInfo == nil then return druid_balance_fail()
---	else return druid_balance_bpt() end
---end
 
---bpt
 function druid_balance(self)
 	-- bpt virtual trackers
 	local tEnergy = UnitPower("player",SPELL_POWER_ECLIPSE)
@@ -66,73 +60,5 @@ function druid_balance(self)
 	--	Petattack("target")
 	end
 
-	return spell
-end
-
-function druid_balance_fail(self)
-	local power = UnitPower("player",SPELL_POWER_ECLIPSE)
-	local eclipse = GetEclipseDirection()
-	if eclipse == "none" then eclipse = "sun" end
-
-	local isTick = 1.2
-	local mfTick = 1.2
-
-	local sEclipse = jps.buff("eclipse (solar)")
-	local lEclipse = jps.buff("eclipse (lunar)")
-
-	local mfDuration = jps.debuffDuration("moonfire")
-	local sfDuration = jps.debuffDuration("sunfire")
-	
-	local focusDotting, focusIS, focusMF, focusSF
-	focusDotting = UnitExists("focus")
-
-	if focusDotting then
-		focusMF = jps.debuffDuration("moonfire","focus")
-		focusSF = jps.debuffDuration("sunfire","focus")
-	end
-
-	local spellTable =
-	{
-		{ "moonfire", jps.Opening },
-		-- Moonfire
-		{ "moonfire", sEclipse and sfDuration < mfTick and mfDuration == 0 },
-		{ "moonfire", sEclipse and sfDuration < 7 and power < 15 },
-		{ "moonfire", lEclipse and mfDuration < mfTick and sfDuration == 0 },
-		{ "moonfire", lEclipse and mfDuration < 10 and power >= -20 },
-		--
-		{ "typhoon", jps.Moving },
-		{ "starfall", lEclipse },
-		{ "moonfire", sEclipse and ((sfDuration < mfTick and mfDuration == 0) or (power < 15 and sfDuration < 7)) and jps.LastCast ~= "moonfire" },
-		{ "moonfire", lEclipse and ((mfDuration < mfTick and sfDuration == 0) or (power > -20 and mfDuration < 7)) and jps.LastCast ~= "moonfire" },
-		--moving
-		{ "starsurge", jps.Moving and jps.buff("shooting stars") },
-		{ "moonfire", jps.Moving },
-		{ "sunfire", jps.Moving },
-		--
-		{ "starsurge", eclipse == "moon" and power > -80 },
-		{ "starsurge", lEclipse and jps.buff("shooting stars") },
-		{ "innervate", jps.mana() < 0.5 },
-		{ "force of nature", jps.UseCDs },
-		{ "starfire", eclipse == "sun" and power < 75 },
-		{ "starfire", jps.LastCast == "wrath" and eclipse == "moon" and power < -84 },
-		{ "wrath", eclipse == "moon" and power >= -84 },
-		{ "wrath", jps.LastCast == "starfire" and eclipse == "sun" and power >= 75 },
-		{ "starfire", eclipse == "sun" },
-		{ "wrath", eclipse == "moon" },
-		{ "starfire" },
-	}
-
-	spell = parseSpellTable( spellTable )
-
-
-	if spell == "force of nature" or spell == "wild mushroom" then
-		jps.groundClick()
-	--	Petattack("target")
-	end
-
-	if spell == "starfall" and jps.glovesCooldown() == 0 then
-		RunMacroText("/use 10")
-	end
-	
 	return spell
 end
