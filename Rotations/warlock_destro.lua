5476d0cfdcd6fdfb703d03631e994866eff60f0c
jp-ganis <james.ganis@gmail.com>
2012-09-07 12:31:41 +0200

    5.0.4

--- Rotations/warlock_destro.lua
+++ Rotations/warlock_destro.lua
@@ -1,49 +1,27 @@
 function warlock_destro(self)
-	local mana = UnitMana("player")/UnitManaMax("player");
-	local shards = UnitPower("player",7);
-	local spell = nil;
+   --latitude
+   local burning = UnitPower("player",14);   
+   local spell = nil;   
+   local imo_duration = jps.debuffDuration("immolate");
 
-	local isf_duration = jps.buffDuration("improved soul fire");
-	local bod_duration = jps.debuffDuration("bane of doom");
-	local cpn_duration = jps.debuffDuration("corruption");
-	local isb_duration = jps.debuffDuration("shadow and flame");
-	local imo_duration = jps.debuffDuration("immolate");
+local spellTable =
 
-	if cd("demon soul") == 0 then 
-		spell = "demon soul";
-	elseif cd("lifeblood") == 0 then
-		spell = "lifeblood";
-	elseif not ud("target","curse of the elements") then
-		spell = "curse of the elements";
-	elseif cd("soulburn") == 0 and shards > 0 then
-		spell = "soulburn";
-	elseif ub("player","soulburn") or ub("player","empowered imp") then
-		spell = "soul fire";
-	elseif bod_duration < 2 then
-		spell = "bane of doom";
-	elseif cpn_duration < 2 then
-		spell = "corruption";
-	elseif GetUnitSpeed("player") > 0 then
-		spell = "fel flame";
-	elseif isf_duration < 3 and jps.LastCast ~= "soul fire" then
-		spell = "soul fire";
-	elseif not ud("target","shadow and flame") and jps.LastCast ~= "shadow bolt" then
-		spell = "shadow bolt";
-	elseif IsSpellInRange("shadowflame","target") == 1 and cd("shadowflame") == 0 then
-		spell = "shadowflame";
-	elseif imo_duration < 2 and jps.LastCast ~= "immolate" then
-		spell = "immolate";
-	elseif cd("conflagrate") == 0 and ud("target","immolate") then
-		spell = "conflagrate";
-	elseif cd("chaos bolt") == 0 then
-		spell = "chaos bolt";
-	elseif mana < 0.3 then
-		spell = "life tap";
-	else
-		spell = "incinerate";
-	end
-
-	return spell;
-
-end		
-
+{
+   
+   
+   { "curse of the elements", not jps.debuff("curse of the elements") },
+   { {"macro","/use 13"},      jps.itemCooldown("77114") == 0 and jps.UseCDs },
+   { {"macro","/cast Dark Soul: Instability"}, jps.cooldown("Dark Soul: Instability") == 0 and jps.UseCDs },
+   { {"macro","/use 10"}, jps.glovesCooldown() == 0 and jps.UseCDs },
+   { jps.DPSRacial, jps.UseCDs },
+   { "shadowburn", jps.hp("target") <= 0.20 and burning > 0  },
+   { "chaos bolt", lcChaosBolting },
+   { "immolate", imo_duration < 2 and jps.LastCast ~= "immolate" },
+   { "conflagrate", "onCD" },
+   { "incinerate" },
+}   
+        if burning <= 1 then lcChaosBolting = false end
+         if burning > 2 then lcChaosBolting = true end
+        local spell = parseSpellTable( spellTable )
+      return spell
+end
