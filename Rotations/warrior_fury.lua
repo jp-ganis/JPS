9abefce7ea725272849e47cb86b156fd79ba05d4
The Dude <michael.mcconachie@hotmail.com>
2012-09-07 22:37:31 -0300

    Update Rotations/warrior_fury.lua
    Extended work that others have provided; added rage checks, cleaned up unnecessary checks and abilities, and tested 10 times.  This profile realizes ~28% DPS increase over a 10 minute pass on a heroic dummy.

--- Rotations/warrior_fury.lua
+++ Rotations/warrior_fury.lua
@@ -1,106 +1,37 @@
 function warrior_fury(self)
-	
-	local spell = nil
-	local playerHealth = UnitHealth("player")/UnitHealthMax("player")
-	local targetHealth = UnitHealth("target")/UnitHealthMax("target")
-	local nSang = UnitBuff("player","Bloodsurge")
-	local nEnrage = UnitBuff("player","Enrage")
-	local nRage = UnitBuff("player","Berserker Rage")
-	local nPower = UnitPower("Player",1) -- Rage est PowerType 1
-	local nEmul = UnitBuff("player","Incite")
-	local nTrance = UnitBuff("player","Battle Trance")
-	local nExec= UnitBuff("player","Executioner")
-	local stackExec = jps.buffStacks("Executioner")
 
--- Rugissement de courage - augmentant leur Force et leur Agilité de 549
--- Totem de force de la terre - augmente de 549 la Force et l'Agilité
--- Cor de l'hiver - augmente de 549 le total de Force et d'Agilité
+if UnitCanAttack("player","target")~=1 or UnitIsDeadOrGhost("target")==1 then return end
 
-if UnitCanAttack("player","target") then
+   local spell = nil
+   local targetHealth = UnitHealth("target")/UnitHealthMax("target") *100
+   local nRage = jps.buff("Berserker Rage","player")
+   local nPower = UnitPower("Player",1)
+   local spellTable = {
 
-	if cd("Battle Shout")==0 and IsUsableSpell("Battle Shout") and not ub("player","Battle Shout") and not ub("player","Roar of Courage") and not ub("player","Horn of Winter") and not ub("player","Strength of earth totem") then
-      	spell= "Battle Shout"
-    elseif nPower < 10 and cd("Battle Shout")==0 and IsUsableSpell("Battle Shout") then
-		spell= "Battle Shout"
-	elseif cd("Commanding Shout")==0 and IsUsableSpell("Commanding Shout") and playerHealth < 0.25 then
-		spell= "Commanding Shout"
-	elseif cd("Berserker Rage")==0 and not ub("player","Berserker Rage") and IsUsableSpell("Berserker Rage") then
-		spell= "Berserker Rage"
-	elseif IsMouseButtonDown(3) and cd("Intercept")==0 and IsSpellInRange("Intercept","target")==1 and IsUsableSpell("Intercept") then
-		spell= "Intercept"
-	elseif cd("Enraged Regeneration")==0 and playerHealth<0.60 and IsUsableSpell("Enraged Regeneration") and nEnrage then
-		spell= "Enraged Regeneration"
-	elseif cd("Pummel")==0 and IsSpellInRange("Pummel","target")==1 and IsUsableSpell("Pummel") and jps.shouldKick("target") then
-		spell= "Pummel"
-	elseif ub("player","Victorious") and IsUsableSpell("Victory Rush") then 
-		spell= "Victory Rush"
-	elseif (UnitHealth("target") > 400000) and cd("Lifeblood")==0 and IsUsableSpell("Lifeblood") and UnitBuff("player","Flurry") then 
-		spell= "Lifeblood"
+--Buffs and Shit
+    { "Battle Shout",   "onCD" and nPower <= 70, "player" },
+    { "Berserker Rage",   jps.UseCDs and (not nRage or (jps.buffStacks("Raging Blow") == 2 and targetHealth > 20 )), "player" },
+    { "Recklessness",   jps.UseCDs and (jps.debuffDuration("Colossus Smash") >= 5 or jps.cooldown("Colossus Smash") <= 4 ) and (targetHealth < 20 or targetHealth >= 40) , "target" },
+    { "Deadly Calm",   jps.UseCDs and nPower >= 40, "player" },
+    { "Lifeblood",   "onCD", "player" },  --if I'm an Herbalist.  Otherwise, ignore me!!
 
-	-- IsMouseButtonDown([button]) 1 or LeftButton - 2 or RightButton - 3 or MiddleButton or clickable scroll control
-	elseif IsMouseButtonDown(2) and cd("Inner Rage")==0 and IsUsableSpell("Inner Rage") then 
-		spell= "Inner Rage"
-	elseif IsMouseButtonDown(2) and nPower>30 and cd("Cleave")==0 and IsUsableSpell("Cleave") then 
-		spell= "Cleave"
-	elseif IsMouseButtonDown(3) and nPower>25 and cd("Whirlwind")==0 and IsUsableSpell("Whirlwind") then 
-		spell= "Whirlwind"
-		
-	elseif cd("Death wish")==0 and nPower>50 and IsUsableSpell("Death wish") then 
-		spell= "Death wish"
-	elseif cd("Colossus Smash")==0 and IsUsableSpell("Colossus Smash") then 
-		spell= "Colossus Smash"
-		
-	elseif targetHealth<0.20 and cd("Execute")==0 and IsUsableSpell("Execute") then 
-		if stackExec==nil or stackExec<5 then
-			spell= "Execute"
-		elseif cd("Bloodthirst")~=0 and (nEnrage~=nil or nRage~=nil) and cd("Raging Blow")==0 and IsUsableSpell("Raging Blow") then
-			spell= "Raging Blow"
-		elseif cd("Bloodthirst")~=0 and nSang~=nil and cd("Slam")==0 and IsUsableSpell("Slam") then
-			spell= "Slam"
-		elseif cd("Bloodthirst")==0 and IsUsableSpell("Bloodthirst") then 
-			spell= "Bloodthirst"
-		else
-			spell= "Execute"
-		end
-		
-	elseif ud("target","Colossus Smash") and UnitBuff("player","Death wish") and cd("Bloodthirst")==0 and IsUsableSpell("Bloodthirst") then
-		spell= "Bloodthirst" 
-	elseif ud("target","Colossus Smash") and UnitBuff("player","Death wish") and (nEnrage~=nil or nRage~=nil) and cd("Raging Blow")==0 and IsUsableSpell("Raging Blow") then
-		spell= "Raging Blow"
-	elseif ud("target","Colossus Smash") and UnitBuff("player","Death wish") and nSang~=nil and cd("Slam")==0 and IsUsableSpell("Slam") then
-		spell= "Slam"
-	elseif ud("target","Colossus Smash") and UnitBuff("player","Death wish") and cd("Heroic Strike")==0 and nEmul~=nil and nPower>50 and IsUsableSpell("Heroic Strike") then
-		spell= "Heroic Strike"
-	elseif ud("target","Colossus Smash") and cd("Bloodthirst")==0 and IsUsableSpell("Bloodthirst") then
-		spell= "Bloodthirst"
-	elseif ud("target","Colossus Smash") and cd("Bloodthirst")~=0 and (nEnrage~=nil or nRage~=nil) and cd("Raging Blow")==0 and IsUsableSpell("Raging Blow") then 
-		spell= "Raging Blow"
-	elseif ud("target","Colossus Smash") and cd("Bloodthirst")~=0 and nSang~=nil and cd("Slam")==0 and IsUsableSpell("Slam") then
-		spell= "Slam"
+--Rage Dump w/ various situational implementations
+    { "Heroic Strike",   (((jps.buff("player", "Colossus Smash") and nPower >= 40) or (jps.buff("Deadly Calm") and nPower >= 30)) and targetHealth >= 20 ) or nPower >=75 , "target" },
 
-	elseif cd("Heroic Strike")==0 and nTrance~=nil and IsUsableSpell("Heroic Strike") then
-		spell= "Heroic Strike"
-	elseif cd("Heroic Strike")==0 and targetHealth>0.20 and nPower>70 and IsUsableSpell("Heroic Strike") then
-		spell= "Heroic Strike"
-	elseif cd("Heroic Strike")==0 and nEmul~=nil and nPower>50 and IsUsableSpell("Heroic Strike") then
-		spell= "Heroic Strike"
+--Start Rotation w/ rate checks where appropriate
+    { "Bloodthirst",   "onCD", "target" },
+    { "Colossus Smash",   "onCD", "target" },
+    { "Execute",      "onCD" and targetHealth <= 20, "target" }, --Execute when target <=20% HP
+    { "Raging Blow",   "onCD" and nPower >= 10, "target" },
+    { "Wild Strike",   jps.buff("Bloodsurge") and targetHealth >= 20 and nPower >= 30, "target" },
+    { "Dragon Roar",   "onCD" , "target" },
+    { "Impending Victory",   nPower >= 10, "target" },
+    { "Heroic Throw",   "onCD" , "target" },
 
-	elseif cd("Bloodthirst")~=0 and (nEnrage~=nil or nRage~=nil) and cd("Raging Blow")==0 and IsUsableSpell("Raging Blow") then
-		spell= "Raging Blow"
-	elseif cd("Bloodthirst")~=0 and nSang~=nil and cd("Slam")==0 and IsUsableSpell("Slam") then
-		spell= "Slam"
-	elseif cd("Bloodthirst")==0 and IsUsableSpell("Bloodthirst") then 
-		spell= "Bloodthirst"
-	elseif UnitThreatSituation("player")==0 and cd("Heroic Throw")==0 and IsUsableSpell("Heroic Throw") then 
-		spell= "Heroic Throw"
-	elseif (UnitHealth("target") > 500000) and (jps.debuffStacks("Sunder Armor") < 3) and cd("Sunder Armor")==0 and IsUsableSpell("Sunder Armor") then
-		spell= "Sunder Armor"
-	end
-else return end
-	return spell
+--Catch all for AA / swing timer generation
+    { {"macro","/startattack"}, nil, "target" },
+}
+   local spell,target = parseSpellTable(spellTable)
+   jps.Target = target
+   return spell
 end
-
-
---http://manaflask.com/Ronnie/blog/5964/
-
-
