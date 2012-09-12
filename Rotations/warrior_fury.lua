eb8b4c7b57e26a20b6bfa8c8423ae03ae6f3f34e
The Dude <michael.mcconachie@hotmail.com>
2012-09-12 01:46:52 -0300

    Update Rotations/warrior_fury.lua
    fixed typo (had a trailing "s" on the interrupt line)

--- Rotations/warrior_fury.lua
+++ Rotations/warrior_fury.lua
@@ -8,15 +8,15 @@ if UnitCanAttack("player","target")~=1 or UnitIsDeadOrGhost("target")==1 then re
    local nPower = UnitPower("Player",1)
    local spellTable = {
 
---Buffs and Shit
+--Buffs and Shiz
     { "Battle Shout",   "onCD" and nPower <= 70, "player" },
     { "Berserker Rage",   jps.UseCDs and (not nRage or (jps.buffStacks("Raging Blow") == 2 and targetHealth > 20 )), "player" },
     { "Recklessness",   jps.UseCDs and (jps.debuffDuration("Colossus Smash") >= 5 or jps.cooldown("Colossus Smash") <= 4 ) and (targetHealth < 20 or targetHealth >= 40) , "target" },
     { "Deadly Calm",   jps.UseCDs and nPower >= 40, "player" },
     { "Lifeblood",   "onCD", "player" },  --if I'm an Herbalist.  Otherwise, ignore me!!
 
---Interrupt that shit
-	{ "Pummel",       jps.shouldKick("target") },s
+--Interrupt that bad boy
+	{ "Pummel",       jps.shouldKick("target") },
 
 --Rage Dump w/ various situational implementations
     { "Heroic Strike",   (((jps.buff("player", "Colossus Smash") and nPower >= 40) or (jps.buff("Deadly Calm") and nPower >= 30)) and targetHealth >= 20 ) or nPower >=75 , "target" },
