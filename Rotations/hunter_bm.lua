b81849161c55339ef5b22cd42bf4c7edf39cb5ad
Jp <james.ganis@gmail.com>
2012-09-15 12:48:50 +0100

    bugfix, missing brackets

--- Rotations/hunter_bm.lua
+++ Rotations/hunter_bm.lua
@@ -10,21 +10,21 @@ local pet_attacking = IsPetAttackActive()
 
 local spellTable = 
 {
-{ {"hunter's mark", not jps.debuff("hunter's mark")},
-{ "macro","/petattack"}, not IsPetAttackActive() },
-{ jps.DPSRacial, jps.UseCDs },
-{ "bestial wrath", focus > 60 },
-{ "serpent sting", not jps.debuff("serpent sting") },
-{ "kill shot" },
-{ "rapid fire", not jps.buff("bloodlust") and not jps.buff("the beast within") and not jps.buff("heroism") and not jps.buff("time warp") },
-{ "kill command", },
-{ "cobra shot", focus <= 20},
-{ "dire beast", },
-{ "lynx rush", },
-{ "focus fire", pet_frenzy==5 and not jps.buff("the beast within")},
-{ "arcane shot", focus >= 59 or jps.buff("the beast within") },
-{ {"macro","/cast cobra shot"}, jps.cooldown("cobra shot") == 0 },
-{ "explosive trap", IsShiftKeyDown() and jps.MultiTarget },
+	{ "hunter's mark", not jps.debuff("hunter's mark") },
+	{ {"macro","/petattack"}, not IsPetAttackActive() },
+	{ jps.DPSRacial, jps.UseCDs },
+	{ "bestial wrath", focus > 60 },
+	{ "serpent sting", not jps.debuff("serpent sting") },
+	{ "kill shot" },
+	{ "rapid fire", not jps.buff("bloodlust") and not jps.buff("the beast within") and not jps.buff("heroism") and not jps.buff("time warp") },
+	{ "kill command", },
+	{ "cobra shot", focus <= 20},
+	{ "dire beast", },
+	{ "lynx rush", },
+	{ "focus fire", pet_frenzy==5 and not jps.buff("the beast within")},
+	{ "arcane shot", focus >= 59 or jps.buff("the beast within") },
+	{ {"macro","/cast cobra shot"}, jps.cooldown("cobra shot") == 0 },
+	{ "explosive trap", IsShiftKeyDown() and jps.MultiTarget },
 }
 
 return parseSpellTable(spellTable)
