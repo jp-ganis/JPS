52a9f43fcc96d61693e604262b6e79c05e670505
Jp <james.ganis@gmail.com>
2012-09-15 19:09:22 +0100

    5.0.4

--- Rotations/rogue_sub.lua
+++ Rotations/rogue_sub.lua
@@ -4,27 +4,24 @@ function rogue_sub(self)
    local cp = GetComboPoints("player")
    local energy = UnitMana("player")
    local spell = nil
-   if cp < 5 and ((jps.cd("shadow dance")==0 and energy < 85) or (jps.cd("vanish")==0) and energy < 60) then
-	   write("Pooling energy")
-	   return nil
-   end
 
 	local spellTable = 
 	{
-		{"shadow dance",	energy > 85 and cp < 5 and not jps.buff("stealth") },
-		{"vanish",			energy > 60 and cp <= 1 and jps.cd("shadowstep")==0 and not jps.buff("shadow dance") and not jps.buff("master of subtlety")},
-		{"shadowstep",		jps.buff("stealth") or jps.buff("shadowdance") },
-		{"premeditation",	cp <= 2 },
-		{"ambush",			cp <= 4 },
-		{"preparation",		jps.cd("vanish") > 60 },
-		{"slice and dice",	jps.buffDuration("Slice and dice") < 3 and cp == 5 },
-		{"rupture",			cp == 5 and not jps.myDebuff("rupture") },
-		{"recuperate",		cp == 5 and jps.buffDuration("recuperate") < 3 },
-		{"eviscerate",		cp == 5 and jps.debuffDuration("rupture") > 1 },
-		{"hemorrhage",		cp < 4 and energy > 40 and jps.debuffDuration("hemorrhage") < 4 },
-		{"hemorrhage",		cp < 5 and energy > 80 and jps.debuffDuration("hemorrhage") < 4 },
-		{"backstab",		cp < 4 and energy > 40 },
-		{"backstab",		cp < 5 and energy > 80 },
+		{ "preparation", not jps.buff("vanish") and jps.cd("vanish") > 60 },
+		{ "shadow blades" },
+		{ nil, energy < 75 },
+		{ "shadow dance", energy > 75 and not jps.buff("stealth") and not jps.debuff("find weakness") },
+		{ nil, energy < 30 },
+		{ "vanish", energy >= 45 and energy <= 75 and cp <= 3 and not jps.buff("Shadow Dance") and not jps.buff("Master of Subtlety") and not jps.debuff("find weakness") },
+		{ "premeditation", cp <= 2 },
+		{ "ambush", cp <= 5 },
+		{ "slice and dice", snd_duration < 3 and cp == 5 },
+		{ "rupture", rupture_duration < 5 and cp == 5 },
+		{ "ambush", jps.buffDuration("shadow dance") <= 2 },
+		{ "eviscerate", cp == 5 },
+		{ "hemorrhage", cp < 4 } ,
+		{ "tricks of the trade" },
+		{ "backstab", cp < 5 and energy > 80 and jps.cd("shadow dance") >= 2 },
 	}
 
    return parseSpellTable( spellTable )
