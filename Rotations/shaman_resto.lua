3dcc6657914813172db6b54fc3d133561a0608f0
tropicdome <tropicdome@hotmail.com>
2012-09-23 15:01:43 +0300

    Update Rotations/shaman_resto.lua
    Added healing rain and chain heal for some AoE healing

--- Rotations/shaman_resto.lua
+++ Rotations/shaman_resto.lua
@@ -1,25 +1,25 @@
 function shaman_resto(self)
 	local spell = nil
-   local lsStacks = jps.buffStacks("lightning shield")
-   local focus = "focus"
-   local me = "player"
-   local mh, _, _, oh, _, _, _, _, _ =GetWeaponEnchantInfo()
-   local engineering ="/use 10"
-   local r = RunMacroText
+	local lsStacks = jps.buffStacks("lightning shield")
+	local focus = "focus"
+	local me = "player"
+	local mh, _, _, oh, _, _, _, _, _ =GetWeaponEnchantInfo()
+	local engineering ="/use 10"
+	local r = RunMacroText
 	local tank = nil
 	
 	
 
-   -- Totems
-   local _, fireName, _, _, _ = GetTotemInfo(1)
-   local _, earthName, _, _, _ = GetTotemInfo(2)
-   local _, waterName, _, _, _ = GetTotemInfo(3)
-   local _, airName, _, _, _ = GetTotemInfo(4)
+	-- Totems
+	local _, fireName, _, _, _ = GetTotemInfo(1)
+	local _, earthName, _, _, _ = GetTotemInfo(2)
+	local _, waterName, _, _, _ = GetTotemInfo(3)
+	local _, airName, _, _, _ = GetTotemInfo(4)
 
-   local haveFireTotem = fireName ~= ""
-   local haveEarthTotem = earthName ~= ""
-   local haveWaterTotem = waterName ~= ""
-   local haveAirTotem = airName ~= ""
+	local haveFireTotem = fireName ~= ""
+	local haveEarthTotem = earthName ~= ""
+	local haveWaterTotem = waterName ~= ""
+	local haveAirTotem = airName ~= ""
 
 	
 	
@@ -28,8 +28,8 @@ function shaman_resto(self)
 	
 	
 	-- Check if we should Purify
-    local cleanseTarget = nil
-    cleanseTarget = jps.FindMeADispelTarget({"Curse"},{"Magic"})
+	local cleanseTarget = nil
+	cleanseTarget = jps.FindMeADispelTarget({"Curse"},{"Magic"})
     
 
 	-- lowest friendly
@@ -39,25 +39,32 @@ function shaman_resto(self)
 
 	-- Priority Table
 	local spellTable = {
-			{ "fire elemental totem",   jps.UseCDs },
-			{ "spiritwalker's grace", jps.Moving and defaultHP < 0.75 },
+			{ "fire elemental totem", 	jps.UseCDs },
+			{ "spiritwalker's grace", 	jps.Moving and defaultHP < 0.75 },
 			
-			{ "water shield", not jps.buff("water shield"), me  },
-			{ "Earthliving Weapon",         not mh, me},
+			-- Buffs
+			{ "water shield", 			not jps.buff("water shield"), me  },
+			{ "Earthliving Weapon", 	not mh, me},
 			
+			-- Set focus to put Earth Shield on focus target
 			{ "earth shield",			tank ~= me and not jps.buff("earth shield",tank), tank },
 			
-			
 			-- Heals
-			{ "riptide",				defaultHP < 0.95 and not jps.buff("RipTide"), defaultTarget },
-			{ "greater healing wave",		defaultHP < 0.4, defaultTarget },
-			{ "healing wave",				defaultHP < 0.8, defaultTarget },
+			-- Shift key
+			{ "Healing Rain",			IsShiftKeyDown() ~= nil and GetCurrentKeyBoardFocus() == nil },
+			-- Left Control key
+			{ "chain heal",				IsLeftControlKeyDown() ~= nil and GetCurrentKeyBoardFocus() == nil, defaultTarget },
+			
+			{ "riptide", 				defaultHP < 0.95 and not jps.buff("RipTide"), defaultTarget },
+			{ "greater healing wave", 	defaultHP < 0.4, defaultTarget },
+			{ "healing wave",			defaultHP < 0.8, defaultTarget },
 			
 			{ "Purify spirit",			cleanseTarget~=nil, cleanseTarget },
-			
-			
 
 	}
+	
+	spell = parseSpellTable(spellTable)
+	if spell == "Healing Rain" then jps.groundClick() end
 
-	return parseSpellTable( spellTable )
+	return spell	
 end
