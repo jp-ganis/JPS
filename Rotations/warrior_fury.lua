9b10978aef8d9b53c94fe5d9d137cfe63b741686
jp-ganis <james.ganis@gmail.com>
2012-10-01 13:39:26 +0200

    5.0.4 cleanup

--- Rotations/warrior_fury.lua
+++ Rotations/warrior_fury.lua
@@ -9,26 +9,26 @@ function warrior_fury(self)
 
    local spellTable = 
    {
-      { "Recklessness" ,         jps.UseCDs and (jps.debuffDuration("Colossus Smash") >= 5 or jps.cooldown("Colossus Smash") <= 4 ) and targetHealth < 20 , "target" },   
-      { "Berserker Rage" ,       jps.UseCDs and (not nRage or (jps.buffStacks("Raging Blow") == 2 and targetHealth > 20 )), "player" },
+      { "Recklessness" ,         jps.UseCDs and (jps.debuffDuration("Colossus Smash") >= 5 or jps.cooldown("Colossus Smash") <= 4 ) and targetHealth < 20 },   
+      { "Berserker Rage" ,       jps.UseCDs and (not nRage or (jps.buffStacks("Raging Blow") == 2 and targetHealth > 20 )) },
       { "Deadly Calm" ,         jps.UseCDs and nPower >= 40, "player" },
       { "Lifeblood" ,          "onCD", "player" },
-      { "Heroic Strike" ,       (((jps.buff("player", "Colossus Smash") and nPower >= 40) or (jps.buff("Deadly Calm") and nPower >= 30)) and targetHealth >= 20 ) or nPower >=110 , "target" },
-      { "Bloodthirst" ,          targetHealth > 20 and not jps.debuff("Colossus Smash") and nPower < 30 , "target" },
-      { "Wild Strike" ,          jps.buff("Bloodsurge") and targetHealth >= 20 and jps.cooldown("Bloodthirst") <= 1 , "target" },
-      { "Colossus Smash" ,       "onCD" , "target" },
-      { "Execute" ,            "onCD" , "target" },
-      { "Raging Blow" ,          jps.buff("Raging Blow") , "target" },
-      { "Wild Strike" ,          jps.buff("Bloodsurge") and targetHealth >= 20 , "target" },
-      { "Dragon Roar" ,          "onCD" , "target" },
-      { "Heroic Throw" ,         "onCD" , "target" },
-      { "Battle Shout" ,          nPower <= 70 and not jps.debuff("Colossus Smash") , "player" },
-      { "Wild Strike" ,         jps.debuff("Colossus Smash") and targetHealth >= 20 , "target" },   
-      { "Impending Victory" ,      targetHealth >= 20 , "target" },
-      { "Wild Strike" ,         jps.cooldown("Colossus Smash") >= 1 and nPower >= 60 and targetHealth >= 20 , "target" },
-      { "Battle Shout" ,          nPower <= 70  , "player" },   
+      { "Heroic Strike" ,       (((jps.buff("player", "Colossus Smash") and nPower >= 40) or (jps.buff("Deadly Calm") and nPower >= 30)) and targetHealth >= 20 ) or nPower >=110 },
+      { "Bloodthirst" ,          targetHealth > 20 and not jps.debuff("Colossus Smash") and nPower < 30 },
+      { "Wild Strike" ,          jps.buff("Bloodsurge") and targetHealth >= 20 and jps.cooldown("Bloodthirst") <= 1 },
+      { "Colossus Smash" },
+      { "Execute" },
+      { "Raging Blow" ,          jps.buff("Raging Blow") },
+      { "Wild Strike" ,          jps.buff("Bloodsurge") and targetHealth >= 20 },
+      { "Dragon Roar" },
+      { "Heroic Throw"  },
+      { "Battle Shout" ,          nPower <= 70 and not jps.debuff("Colossus Smash") },
+      { "Wild Strike" ,         jps.debuff("Colossus Smash") and targetHealth >= 20 },   
+      { "Impending Victory" ,      targetHealth >= 20  },
+      { "Wild Strike" ,         jps.cooldown("Colossus Smash") >= 1 and nPower >= 60 and targetHealth >= 20  },
+      { "Battle Shout" ,          nPower <= 70  },   
 
-      { {"macro","/startattack"}, nil, "target" },
+      { {"macro","/startattack"}, nil },
    }
 
    local spell,target = parseSpellTable(spellTable)
