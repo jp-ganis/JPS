0a150380c83999f61afae391fff6eaf7f1701332
Hax McGee <hax@nonexistentemailaddress.com>
2012-12-22 00:39:53 -0500

    tweaked brewmaster rotation some more

--- Rotations/monk_brewmaster.lua
+++ Rotations/monk_brewmaster.lua
@@ -4,16 +4,11 @@ function monk_brewmaster(self)
 	-- Shift to use "Dizzying Haze" at mouse position - AoE threat builder - "Hurl a keg of your finest brew"
 	-- Left control and mouseover target to use "Chi Wave" - can be used on friendlies and enemies (disabled for now)
 
-	local chi = UnitPower("player","12") -- 12 is chi
-	local energy = UnitPower("player","3") -- 3 is energy
-	local targetHealth = UnitHealth("target")
-	local stance = GetShapeshiftForm()
+	local chi = UnitPower("player", "12") -- 12 is chi
+	local energy = UnitPower("player", "3") -- 3 is energy
 	local defensiveCDActive = jps.buff("Fortifying Brew") or jps.buff("Diffuse Magic") or jps.buff("Dampen Harm")
 
 	local possibleSpells = {
-
-		-- Stance check
-		{ nil, stance ~= 2 },
 
 		-- Dizzying Haze when holding down shift.
 		{ "Dizzying Haze", 
@@ -23,102 +18,109 @@ function monk_brewmaster(self)
 		-- Defensive cooldowns 
 		{ "Fortifying Brew", 
       jps.UseCDs 
-      and jps.hp() < .3 
+      and jps.hp() < .4
       and not defensiveCDActive },
 
+    -- Talent based.
     { "Diffuse Magic", 
       jps.UseCDs 
       and jps.hp() < .5 
       and not defensiveCDActive },
 
+    -- Talent based.
     { "Dampen Harm", 
       jps.UseCDs 
       and jps.hp() < .6 
       and not defensiveCDActive },
 
-    { "Chi Wave",
-      jps.UseCDs 
-      and jps.hp() < .6 
-      and chi >= 2 },
-
-
-		-- Insta-kill single target when available
+		-- Insta-kill single target when available.
     { "Touch of Death", 
       jps.UseCDs 
       and jps.buff("Death Note") 
       and chi > 2 
       and not jps.MultiTarget },
 
-    -- On-Use Trinket 1
-    { jps.useTrinket(1), 
-      jps.UseCDs },
-
-    -- On-Use Trinket 2
-    { jps.useTrinket(2), 
-      jps.UseCDs },
-
     -- Purifying Brew to clear stagger when it's moderate or heavy.
 		{ "Purifying Brew",
-			jps.debuff("Heavy Stagger") 
-			or jps.debuff("Moderate Stagger") },
+			jps.debuff("Moderate Stagger") 
+			or jps.debuff("Heavy Stagger") },
 		
 		-- Elusive Brew with 10 or more stacks.
 		{ "Elusive Brew", 
 			jps.buffStacks("Elusive Brew") >= 10 },
 
-		-- Guard when Power Guard buff is available.
+    -- Chi Brew if we have no chi (talent based).
+    { "Chi Brew", 
+      chi == 0 },
+
+    -- Rushing Jade Wind applies shuffle with a heal and multi-target damage. 
+    --  Use it instead of Blackout kick when it's available. (talent based)
+    { "Rushing Jade Wind", 
+      ( not jps.buff("Shuffle")
+        or jps.buffDuration("shuffle") < 3 )
+      and chi >= 2 },
+
+    -- Blackout Kick if shuffle is missing or about to drop.
+    { "Blackout Kick", 
+      ( not jps.buff("Shuffle")
+        or jps.buffDuration("shuffle") < 3 )
+      and chi >= 2 },
+
+		-- Guard when Power Guard buff is available, we're taking some damage.
 		{ "Guard", 
 			jps.buff("Power Guard") 
-			and jps.hp() < 0.85
-			and not defensiveCDActive },
+			and jps.hp() < .9 },
 
-		-- Blackout Kick if we don't have shuffle or if it's about to drop.
-		{ "Blackout Kick", 
-			not jps.buff("Shuffle")
-			or jps.buffDuration("shuffle") < 3 },
+    -- On-Use Trinket 1.
+    { jps.useTrinket(1), 
+      jps.UseCDs },
+
+    -- On-Use Trinket 2.
+    { jps.useTrinket(2), 
+      jps.UseCDs },
 
 		-- Keg Smash to build some chi and threat.
 		{ "Keg Smash", 
 			chi < 3 },
 
-		-- Interrupt
+		-- Interrupt.
     { "Spear Hand Strike", 
       jps.Interrupts 
       and jps.shouldKick() },
 
-		-- Breath of Fire is our strongest AoE
+		-- Breath of Fire is the strongest AoE.
 		{ "Breath of Fire", 
-			jps.MultiTarget },
+			jps.MultiTarget
+      and chi >= 2 },
 
-		-- Expel Harm for building some chi and healing if we're not full health.
-		{ "Expel Harm", 
-			jps.hp() < 0.90 
-			and energy >= 40 
+		-- Expel Harm for building some chi and healing if not at full health.
+		{ "Expel Harm",
+			jps.hp() < .9
+			and energy >= 40
 			and chi < 4 },
 
-		-- Chi Wave for multi-target threat and heal.
-		{ "Chi Wave", 
-			jps.MultiTarget 
-			and chi >= 2 },
+    -- Tiger Palm to keep the Tiger Power buff up. No chi cost due to Brewmaster specialization at level 34.
+    { "Tiger Palm", 
+      not jps.MultiTarget
+      and not jps.buff("Tiger Power")
+      or jps.buffDuration("Tiger Power") <= 1.5 },
 
-		-- Rushing Jade Wind for multi-target threat.
-		{ "Rushing Jade Wind", 
-			jps.MultiTarget 
+    -- Zen Sphere for a small healing boost and some damage (talent based).
+    { "Zen Sphere",
+      not jps.MultiTarget
+      and jps.hp() < .9
+      and chi >= 2 },
+      
+		-- Chi Wave for multi-target threat and heal (talent based).
+		{ "Chi Wave",
+      jps.MultiTarget
+      and jps.hp() < .9
 			and chi >= 2 },
 
 		-- Spinning Crane Kick for multi-target threat.
 		{ "Spinning Crane Kick", 
 			jps.MultiTarget 
 			and energy >= 40 },
-
-		-- Tiger Palm to keep the Tiger Power buff up. No chi cost due to Brewmaster specialization at level 34.
-		{ "Tiger Palm", 
-			not jps.buff("Tiger Power")
-			or jps.buffDuration("Tiger Power") <= 1.5 },
-
-		-- Chi Brew if we have the talent.
-		{ "Chi Brew", 
-			chi == 0 },
 
 		-- Jab is our basic chi builder.
 		{ "Jab", 
