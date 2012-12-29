f75e31e98ecccc2db14e304bcd81e87a1f559bd5
Hax McGee <hax@nonexistentemailaddress.com>
2012-12-29 11:04:59 -0500

    updated druid feral rotation to account for leveling (no doc)

--- Rotations/druid_feral.lua
+++ Rotations/druid_feral.lua
@@ -22,298 +22,279 @@ function druid_feral(self)
 	local thrashDuration = jps.debuffDuration("Thrash")
 	local predatorySwiftnessDuration = jps.buffDuration("Predatory Swiftness")
 
-	local possibleSpells = {}
+	local maxLevel = (UnitLevel("player") == 90)
 
-	if jps.MultiTarget then
-	  possibleSpells = {
-	  	-- Bail if not in cat form.
-		  { nil, 
-		  	not jps.buff("Cat Form") },
+	local possibleSpells = {
+		-- Bail if not in cat form.
+	  { nil, 
+	  	not jps.buff("Cat Form") },
 
-      -- Bail if not in AoE range.
-      { nil, 
-      	IsSpellInRange("Skull Bash", "target") == 0 },
+	  -- Bail if not in melee range.
+    { nil, 
+    	IsSpellInRange("Shred", "target") == 0 },
 
-      -- Savage Roar should be kept up at all times.
-      { "Savage Roar", 
-      	savageRoarDuration == 0 },
+		-- Savage Roar should be kept up at all times.
+    { "Savage Roar", 
+    	savageRoarDuration == 0 },
 
-      -- Tiger's Fury when we're low on energy.
-			{ "Tiger's Fury", 
-				energy <= 35 
-				and not clearcasting 
-				and gcdLocked },
+		-- Healing Touch when we have Predatory Swiftness, less than 2 cenarion stacks, and the combo points to use the damage buff.
+		{ "Healing Touch", 
+			predatorySwiftness
+			and cenarionStacks < 2
+			and cp >= 4
+			and maxLevel },
 
-			-- Berserk when we have Tiger's Fury
-			{ "Berserk", 
-				jps.UseCDs
-				and jps.buff("Tiger's Fury") },
+		-- Healing Touch to use up Predatory Swiftness before it falls off if we have less than 2 cenarion stacks and low combo points and energy.
+		{ "Healing Touch", 
+			predatorySwiftness
+			and predatorySwiftnessDuration <= 1
+			and not clearcasting 
+			and energy < 45 
+			and cenarionStacks < 2 
+			and cp < 4
+			and maxLevel },
 
-			-- Healing Touch when we have Predatory Swiftness, less than 2 cenarion stacks, and the combo points to use the damage buff.
-			{ "Healing Touch", 
-				predatorySwiftness
-				and cenarionStacks < 2
-				and cp >= 4 },
+		-- Healing Touch whenever we have Nature's Swiftness. (talent based)
+		{ "Healing Touch", 
+			jps.buff("Nature's Swiftness")
+			and cenarionStacks < 2
+			and maxLevel },
 
-			-- Healing Touch whenever we have Nature's Swiftness, and either less than 2 cenario stacks or are in need of healing. (talent based)
-			{ "Healing Touch", 
-				jps.buff("Nature's Swiftness")
-				and (cenarionStacks < 2
-					or jps.hp() < .8) },
+		-- Temporary for leveling.
+		{ "Healing Touch", 
+			predatorySwiftness or jps.buff("Nature's Swiftness")
+			and jps.hp() < .6
+			and not maxLevel },
 
-			-- Nature's Swiftness
-			{ "Nature's Swiftness",	
-				jps.UseCDs
-				and cenarionStacks == 0 
-				and not predatorySwiftness 
-				and cp >= 5 
-				and (berserk 
-					or tigersFury) },
+		-- Tiger's Fury when we're low on energy.
+		{ "Tiger's Fury", 
+			energy <= 35 
+			and not clearcasting 
+			and gcdLocked },
 
-      -- Thrash debuff should be kept up at all times.
-		  { "Thrash", 
-		  	thrashDuration < 2 },
+		-- Berserk when we have Tiger's Fury
+		{ "Berserk", 
+			jps.UseCDs
+			and jps.buff("Tiger's Fury") },
 
-		  -- Swipe is the base AoE spell. (Assume there's a good reason to limit at 51+)
-		  { "Swipe", 
-		  	energy > 51 },
-		}
+		-- Nature's Vigil if Berserk buff in on.
+		{ "Nature's Vigil", 
+			jps.UseCDs
+			and berserk },
 
-	else
-		possibleSpells = {
-			-- Bail if not in cat form.
-		  { nil, 
-		  	not jps.buff("Cat Form") },
+		-- Incarnation if Berserk buff in on. (talent specific)
+		{ "Incarnation", 
+			jps.UseCDs
+			and berserk },
 
-		  -- Bail if not in melee range.
-      { nil, 
-      	IsSpellInRange("Shred", "target") == 0 },
+		-- Treants (talent specific)
+		{ "Force of Nature" },
 
-			-- Savage Roar should be kept up at all times.
-      { "Savage Roar", 
-      	savageRoarDuration == 0 },
+		-- On-Use Trinkets if Berserk buff in on.
+    { jps.useSlot(13), 
+      jps.UseCDs
+      and berserk },
+    { jps.useSlot(14), 
+      jps.UseCDs
+      and berserk },
 
-			-- -- Healing Touch when we have Predatory Swiftness, less than 2 cenarion stacks, and the combo points to use the damage buff.
-			-- { "Healing Touch", 
-			-- 	predatorySwiftness
-			-- 	and cenarionStacks < 2
-			-- 	and cp >= 4 },
+		-- DPS Racial if Berserk buff in on.
+		{ jps.DPSRacial, 
+			jps.UseCDs
+			and berserk },
 
-			-- -- Healing Touch to use up Predatory Swiftness before it falls off if we have less than 2 cenarion stacks and low combo points and energy.
-			-- { "Healing Touch", 
-			-- 	predatorySwiftness
-			-- 	and predatorySwiftnessDuration <= 1
-			-- 	and not clearcasting 
-			-- 	and energy < 45 
-			-- 	and cenarionStacks < 2 
-			-- 	and cp < 4 },
+		-- Engineers may have synapse springs on their gloves (slot 10).
+		{ jps.useSlot(10), 
+      jps.UseCDs
+      and berserk },
 
-			-- -- Healing Touch whenever we have Nature's Swiftness. (talent based)
-			-- { "Healing Touch", 
-			-- 	jps.buff("Nature's Swiftness")
-			-- 	and cenarionStacks < 2 },
+		-- Lifeblood if Berserk buff in on. (requires herbalism)
+		{ "Lifeblood",
+			jps.UseCDs
+			and berserk },
 
-			-- Temporary for leveling.
-			{ "Healing Touch", 
-				predatorySwiftness or jps.buff("Nature's Swiftness")
-				and jps.hp() < .6 },
+		-- Interrupt
+		{ "Skull Bash", 
+			jps.shouldKick() 
+			and jps.Interrupts },
 
-			-- Tiger's Fury when we're low on energy.
-			{ "Tiger's Fury", 
-				energy <= 35 
-				and not clearcasting 
-				and gcdLocked },
+		-- Ferocious Bite if we're in execute phase and Rip is about the fall off.
+		{ "Ferocious Bite", 
+			not jps.MultiTarget
+			and executePhase 
+			and cp > 0 
+			and ripDuration <= 2 
+			and ripDuration > 0 },
 
-			-- Berserk when we have Tiger's Fury
-			{ "Berserk", 
-				jps.UseCDs
-				and jps.buff("Tiger's Fury") },
+		-- Multi-target only: Thrash debuff should be kept up at all times.
+	  { "Thrash", 
+	  	jps.MultiTarget
+	  	and thrashDuration < 2 },
 
-			-- Nature's Vigil if Berserk buff in on.
-			{ "Nature's Vigil", 
-				jps.UseCDs
-				and berserk },
+	  -- Multi-target only: Swipe is the base AoE spell. (Assume there's a good reason to limit at 51+?)
+	  { "Swipe", 
+	  	jps.MultiTarget
+	  	and energy > 51 },
 
-			-- Incarnation if Berserk buff in on. (talent specific)
-			{ "Incarnation", 
-				jps.UseCDs
-				and berserk },
+		-- Thrash if we're clearcasting, it's debuff is about to run out, and we have no cenarion stacks.
+		{ "Thrash", 
+			clearcasting 
+			and thrashDuration < 3 
+			and cenarionStacks == 0 },
 
-			-- Treants (talent specific)
-			{ "Force of Nature" },
+		-- Savage Roar
+		{ "Savage Roar", 
+			savageRoarDuration <= 1 
+			or (savageRoarDuration <= 3 
+				and cp > 0) 
+			and executePhase },
 
-			-- On-Use Trinkets if Berserk buff in on.
-	    { jps.useTrinket(1), 
-	      jps.UseCDs
-	      and berserk },
-	    { jps.useTrinket(2), 
-	      jps.UseCDs
-	      and berserk },
+		-- Nature's Swiftness
+		{ "Nature's Swiftness",	
+			cenarionStacks == 0 
+			and not predatorySwiftness 
+			and cp >= 5 
+			and executePhase },
 
-			-- DPS Racial if Berserk buff in on.
-			{ jps.DPSRacial, 
-				jps.UseCDs
-				and berserk },
+		-- Rip
+		{ "Rip", 
+			not jps.MultiTarget
+			and cp >= 5 
+			and cenarionStacks > 0 
+			and executePhase 
+			and not jps.RipBuffed }, -- stronger rip detection
 
-			-- Lifeblood if Berserk buff in on. (requires herbalism)
-			{ "Lifeblood",
-				jps.UseCDs
-				and berserk },
+		-- Ferocious Bite
+		{ "Ferocious Bite", 
+			not jps.MultiTarget
+			and executePhase 
+			and cp == 5 
+			and ripDuration > 0 },
 
-			-- Interrupt
-			{ "Skull Bash", 
-				jps.shouldKick() 
-				and jps.Interrupts },
+		-- Rip
+		{ "Rip", 
+			not jps.MultiTarget
+			and cp >= 5 
+			and ripDuration < 2 
+			and cenarionStacks > 0 },
 
-			-- Ferocious Bite if we're in execute phase and Rip is about the fall off.
-			{ "Ferocious Bite", 
-				executePhase 
-				and cp > 0 
-				and ripDuration <= 2 
-				and ripDuration > 0 },
+		-- Savage Roar
+		{ "Savage Roar", 
+			savageRoarDuration <= 1 
+			or (savageRoarDuration <= 3 
+				and cp > 0) },
 
-			-- Thrash if we're clearcasting, it's debuff is about to run out, and we have no cenarion stacks.
-			{ "Thrash", 
-				clearcasting 
-				and thrashDuration < 3 
-				and cenarionStacks == 0 },
+		-- Nature's Swiftness
+		{ "Nature's Swiftness",	
+			cenarionStacks == 0 
+			and not predatorySwiftness 
+			and cp >= 5 
+			and ripDuration < 3 
+			and (berserk 
+				or ripDuration <= tigersFuryCD) 
+			and not executePhase
+			and maxLevel },
 
-			-- Savage Roar
-			{ "Savage Roar", 
-				savageRoarDuration <= 1 
-				or (savageRoarDuration <= 3 
-					and cp > 0) 
-				and executePhase },
+		-- Temporary for leveling
+		{ "Nature's Swiftness",	
+			not predatorySwiftness
+			and not maxLevel },
 
-			-- Nature's Swiftness
-			{ "Nature's Swiftness",	
-				cenarionStacks == 0 
-				and not predatorySwiftness 
-				and cp >= 5 
-				and executePhase },
+		-- Rip
+		{ "Rip", 
+			not jps.MultiTarget
+			and cp >= 5 
+			and ripDuration < 2 
+			and (berserk 
+				or ripDuration < tigersFuryCD) },
 
-			-- Rip
-			{ "Rip", 
-				cp >= 5 
-				and cenarionStacks > 0 
-				and executePhase 
-				and not jps.RipBuffed }, -- stronger rip detection
+		-- Thrash
+		{ "Thrash", 
+			clearcasting 
+			and thrashDuration < 3 },
 
-			-- Ferocious Bite
-			{ "Ferocious Bite", 
-				executePhase 
-				and cp == 5 
-				and ripDuration > 0 },
+		-- Savage Roar
+		{ "Savage Roar", 
+			savageRoarDuration <= 6 
+			and cp >= 5 
+			and ripDuration > 4 },
 
-			-- Rip
-			{ "Rip", 
-				cp >= 5 
-				and ripDuration < 2 
-				and cenarionStacks > 0 },
+		-- Ferocious Bite
+		{ "Ferocious Bite", 
+			not jps.MultiTarget
+			and cp >= 5 
+			and ripDuration > 4 },
 
-			-- Savage Roar
-			{ "Savage Roar", 
-				savageRoarDuration <= 1 
-				or (savageRoarDuration <= 3 
-					and cp > 0) },
+		-- Rake
+		{ "Rake", 
+			not jps.MultiTarget
+			and cenarionStacks > 0 
+			and not jps.RakeBuffed },
 
-			-- -- Nature's Swiftness
-			-- { "Nature's Swiftness",	
-			-- 	cenarionStacks == 0 
-			-- 	and not predatorySwiftness 
-			-- 	and cp >= 5 
-			-- 	and ripDuration < 3 
-			-- 	and (berserk 
-			-- 		or ripDuration <= tigersFuryCD) 
-			-- 	and not executePhase },
+		-- Rake
+		{ "Rake", 
+			not jps.MultiTarget
+			and rakeDuration < 3 
+			and (berserk 
+				or tigersFuryCD + .8 >= rakeDuration) },
 
-			-- Temporary for leveling
-			{ "Nature's Swiftness",	
-				not predatorySwiftness },
+		-- Shred
+		{ "Shred", 
+			not jps.MultiTarget
+			and clearcasting },
 
-			-- Rip
-			{ "Rip", 
-				cp >= 5 
-				and ripDuration < 2 
-				and (berserk 
-					or ripDuration < tigersFuryCD) },
+		-- Shred
+		{ "Shred", 
+			not jps.MultiTarget
+			and predatorySwiftnessDuration > 1 
+			and not (energy + (energyPerSec * (predatorySwiftnessDuration - 1)) < (4 - cp) * 20) },
 
-			-- Thrash
-			{ "Thrash", 
-				clearcasting 
-				and thrashDuration < 3 },
-
-			-- Savage Roar
-			{ "Savage Roar", 
-				savageRoarDuration <= 6 
-				and cp >= 5 
-				and ripDuration > 4 },
-
-			-- Ferocious Bite
-			{ "Ferocious Bite", 
-				cp >= 5 
-				and ripDuration > 4 },
-
-			-- Rake
-			{ "Rake", 
-				cenarionStacks > 0 
-				and not jps.RakeBuffed },
-
-			-- Rake
-			{ "Rake", 
-				rakeDuration < 3 
-				and (berserk 
-					or tigersFuryCD + .8 >= rakeDuration) },
-
-			-- Shred
-			{ "Shred", 
-				clearcasting },
-
-			-- Shred
-			{ "Shred", 
-				predatorySwiftnessDuration > 1 
-				and not (energy + (energyPerSec * (predatorySwiftnessDuration - 1)) < (4 - cp) * 20) },
-
-			-- Shred
-			{ "Shred", 
+		-- Shred
+		{ "Shred", 
+			not jps.MultiTarget
+			and ( 
 				(cp < 5 
 					and ripDuration < 3) 
 				or (cp == 0 
-					and savageRoarDuration < 2 ) },
+					and savageRoarDuration < 2 ) 
+			) },
 
-			-- Thrash
-			{ "Thrash", 
-				cp >= 5 
-				and thrashDuration < 6 
-				and (tigersFury 
-					or berserk) },
+		-- Thrash
+		{ "Thrash", 
+			cp >= 5 
+			and thrashDuration < 6 
+			and (tigersFury 
+				or berserk) },
 
-			-- Thrash
-			{ "Thrash", 
-				cp >= 5 
-				and thrashDuration < 6 
-				and tigersFuryCD <= 3 },
+		-- Thrash
+		{ "Thrash", 
+			cp >= 5 
+			and thrashDuration < 6 
+			and tigersFuryCD <= 3 },
 
-			-- Thrash
-			{ "Thrash", 
-				cp >= 5 
-				and thrashDuration < 6 
-				and energy >= 100 - energyPerSec },
+		-- Thrash
+		{ "Thrash", 
+			cp >= 5 
+			and thrashDuration < 6 
+			and energy >= 100 - energyPerSec },
 
-			-- Shred
-			{ "Shred", 
-				berserk 
-				or jps.buff("tiger's fury") },
+		-- Shred
+		{ "Shred", 
+			not jps.MultiTarget
+			and ( berserk 
+				or jps.buff("tiger's fury")
+			) },
 
-			-- Shred
-			{ "Shred", 
-				tigersFuryCD <= 3 },
+		-- Shred
+		{ "Shred", 
+			not jps.MultiTarget
+			and tigersFuryCD <= 3 },
 
-			-- Shred
-			{ "Shred", 
-				energy >= 100 - (energyPerSec * 2) },
-		}
-	end
+		-- Shred
+		{ "Shred", 
+			not jps.MultiTarget
+			and energy >= 100 - (energyPerSec * 2) },
+	}
 
 	return parseSpellTable(possibleSpells)
 end
