8464f3ebd177933e57f9de4caa9205b0e0963359
Hax McGee <hax@nonexistentemailaddress.com>
2013-01-10 15:07:52 -0500

    rewrite of mage fire and frost rotations

--- Rotations/mage_fire.lua
+++ Rotations/mage_fire.lua
@@ -1,9 +1,14 @@
 function mage_fire(self)
---pcmd
+
 	if UnitCanAttack("player","target") ~= 1 or UnitIsDeadOrGhost("target") == 1 then return end
 
 	local castingSpell, _, _, _, _, endTime = UnitCastingInfo("player")
-
+  
+  local evocating = (castingSpell == "Evocation")
+  local atActive = jps.buff("Altered Time")
+  local pomActive = jps.buff("Presence of Mind")
+  local pyroActive = jps.buff("Pyroblast!")
+  
 	local possibleSpells = {
 
 		-- Ice Block when you're about to die.
@@ -19,30 +24,26 @@ function mage_fire(self)
 		-- Refresh your Ice block. (talent based)
 		{ "Cold Snap",
 			jps.cooldown("Ice Block") > 0
-			and jps.cooldown("Cold Snap") == 0
-			and not jps.buff("Presence of Mind") },
+			and jps.cooldown("Cold Snap") == 0 },
 		
 		-- Ice Barrier when you're taking a decent amount of damage. (talent based)
 		{ "Ice Barrier",
 			jps.hp() < .7
-			and not jps.buff("Ice Barrier")
-			and not jps.buff("Presence of Mind") },
+			and not jps.buff("Ice Barrier") },
 
 		-- Interrupts.
 		{ "Counterspell", 
 			jps.Interrupts 
-      and jps.shouldKick()
-      and not jps.buff("Presence of Mind") },
+      and jps.shouldKick() },
 
 		-- Molten Armor if you forgot to buff it.
 		{ "Molten Armor", 
 			not jps.buff("Molten Armor")
-			and not jps.buff("Presence of Mind") },
+			and not pomActive },
 
 		-- Arcane Brilliance if you forgot to buff it.
 		{ "Arcane Brilliance", 
-			not jps.buff("Arcane Brilliance")
-			and not jps.buff("Presence of Mind"), "player" },
+			not jps.buff("Arcane Brilliance"), "player" },
 		
 		-- Removed AoE spells since they are very situational.
 		-- { "Dragon's Breath", CheckInteractDistance("target", 3) == 1 }, 
@@ -50,10 +51,10 @@ function mage_fire(self)
 		
 		-- Evocation whenever you're missing the damage buff.
 		-- ** Important ** This assumes you have the Invocation talent. Comment this line our if you don't.
-		{ "Evocation",
-			not jps.buff("Invoker's Energy")
-			and jps.cooldown("Evocation") == 0
-			and not jps.buff("Presence of Mind") },
+		--{ "Evocation",
+		--	not jps.buff("Invoker's Energy")
+		--	and jps.cooldown("Evocation") == 0
+		--	and not pomActive },
 
 		-- Combustion is now based only off ignite. It would be nice if we could judge the size of the ignite though.
 		{ "Combustion", 
@@ -62,51 +63,49 @@ function mage_fire(self)
 		
 		-- Mirror Image is a minor DPS increase.
 		{ "Mirror Image", 
-			jps.UseCDs },
+			jps.UseCDs
+      and not ( evocating or atActive ) },
 
 		-- PoM for insta-pyro. (talent based)
 		{ "Presence of Mind",
-			not jps.buff("Pyroblast!") },
+			not ( evocating or atActive ) },
 
 		-- Engineers may have synapse springs on their gloves (slot 10).
     { jps.useSlot(10), 
       jps.UseCDs
-      and not castingSpell == "Evocation"
-      and ( jps.buff("Heating Up")
-      	or jps.buff("Pyroblast!")
-      	or jps.buff("Presence of Mind")
-      ) },
+      and not ( evocating or atActive )
+      and ( pyroActive or pomActive ) },
 
 		-- On-use Trinkets when we have a damage buff.
     { jps.useSlot(13), 
       jps.UseCDs
-      and not castingSpell == "Evocation"
-      and ( jps.buff("Heating Up")
-      	or jps.buff("Pyroblast!")
-      	or jps.buff("Presence of Mind")
-      ) },
+      and not ( evocating or atActive )
+      and ( pyroActive or pomActive ) },
     { jps.useSlot(14), 
       jps.UseCDs
-      and not castingSpell == "Evocation"
-      and ( jps.buff("Heating Up")
-      	or jps.buff("Pyroblast!")
-      	or jps.buff("Presence of Mind")
-      ) },
+      and not ( evocating or atActive )
+      and ( pyroActive or pomActive ) },
 
     -- Lifeblood on cooldown. (profession based)
     { "Lifeblood",
       jps.UseCDs
-      and not castingSpell == "Evocation" },
+      and not ( evocating or atActive ) },
 
     -- DPS Racial on cooldown.
     { jps.DPSRacial, 
       jps.UseCDs
-      and not castingSpell == "Evocation" },
+      and not ( evocating or atActive ) },
 
+    -- Alter Time whenver we have decent buffs.
+    { "Alter Time",
+      jps.UseCDs
+      and not evocating
+      and ( pyroActive or pomActive ) },
+    
 		-- Pyroblast whenever we proc Pyroblast! or if we're using Presence of Mind.
 		{ "Pyroblast", 
-			jps.buff("Pyroblast!")
-			or jps.buff("Presence of Mind") },
+			pyroActive
+      or pomActive },
 
 		-- Living Bomb. (talent based)
 		{ "Living Bomb", 
@@ -132,12 +131,7 @@ function mage_fire(self)
 		{ "Fireball" },
 		
 	}
-   local spell,target = parseSpellTable(possibleSpells)
-   if spell == "Flamestrike" then
-       jps.Cast( spell )
-       jps.groundClick()
-   end
-
-   jps.Target = target
-   return spell
+  
+  return parseSpellTable(possibleSpells)
+  
 end
--- Rotations/mage_frost.lua
+++ Rotations/mage_frost.lua
@@ -1,40 +1,142 @@
---psycho + stolen code ;-)
-
 function mage_frost(self)
---pcmd
+
 	if UnitCanAttack("player","target")~=1 or UnitIsDeadOrGhost("target")==1 then return end
+  
+	local castingSpell, _, _, _, _, endTime = UnitCastingInfo("player")
+  
+  local evocating = (castingSpell == "Evocation")
+  local atActive = jps.buff("Altered Time")
+  local pomActive = jps.buff("Presence of Mind")
+  local fofActive = jps.buff("Fingers of Frost")
+  local bfActive = jps.buff("Brain Freeze")
+  local ivActive = jps.buff("Icy Veins")
+  
+	local possibleSpells = {
+    
+		-- Ice Block when you're about to die.
+		{ "Ice Block",
+			jps.hp() < .3
+			and not jps.buff("Ice Block")
+			and not jps.debuff("Hypothermia") },
 
-	local spellTable =
-	{
-	--interrupt
-		{ "Counterspell",     jps.Interrupts and jps.shouldKick("target"), "target" },
-		{ "Ice Barrier",      (UnitHealth("player") / UnitHealthMax("player") < 0.40)  and not jps.buff("Ice Barrier","player"), "player" },
+		{ { "macro", "/cancelaura Ice Block" }, 
+			jps.hp() > .8
+			and jps.buff("Ice Block") },
+
+		-- Refresh your Ice block. (talent based)
+		{ "Cold Snap",
+			jps.cooldown("Ice Block") > 0
+			and jps.cooldown("Cold Snap") == 0 },
 		
-		--buffs
-		{ "Molten Armor",     not jps.buff("Molten Armor","player"), "player" },
-		{ "Arcane Brilliance",     not jps.buff("Arcane Brilliance","player"), "player" },
+		-- Ice Barrier when you're taking a decent amount of damage. (talent based)
+		{ "Ice Barrier",
+			jps.hp() < .7
+			and not jps.buff("Ice Barrier") },
+
+		-- Interrupts.
+		{ "Counterspell", 
+			jps.Interrupts 
+      and jps.shouldKick() },
+
+		-- Molten Armor if you forgot to buff it.
+		{ "Molten Armor", 
+			not jps.buff("Molten Armor")
+			and not pomActive },
+
+		-- Arcane Brilliance if you forgot to buff it.
+		{ "Arcane Brilliance", 
+			not jps.buff("Arcane Brilliance"), "player" },
+    
+		-- Evocation whenever you're missing the damage buff.
+		-- ** Important ** This assumes you have the Invocation talent. Comment this line our if you don't.
+		{ "Evocation",
+      jps.UseCDs
+			and not jps.buff("Invoker's Energy")
+			and jps.cooldown("Evocation") == 0
+			and not pomActive },
+    
+		-- Mirror Image is a minor DPS increase.
+		{ "Mirror Image", 
+			jps.UseCDs
+      and not ( evocating or atActive ) },
+
+		-- PoM for insta-frostbolt. (talent based)
+		{ "Presence of Mind",
+      jps.UseCDs
+      and not ( evocating or atActive ) },
+    
+		-- Icy Veins for haste buff.
+		{ "Icy Veins",
+      jps.UseCDs
+      and not ( evocating or atActive ) },
+
+		-- Engineers may have synapse springs on their gloves (slot 10).
+    { jps.useSlot(10), 
+      jps.UseCDs
+      and not ( evocating or atActive )
+      and ( fofActive or pomActive or ivActive ) },
+
+		-- On-use Trinkets when we have a damage buff.
+    { jps.useSlot(13), 
+      jps.UseCDs
+      and not ( evocating or atActive )
+      and ( fofActive or pomActive or ivActive ) },
+    { jps.useSlot(14), 
+      jps.UseCDs
+      and not ( evocating or atActive )
+      and ( fofActive	or pomActive or ivActive ) },
+
+    -- Lifeblood on cooldown. (profession based)
+    { "Lifeblood",
+      jps.UseCDs
+      and not ( evocating or atActive ) },
+
+    -- DPS Racial on cooldown.
+    { jps.DPSRacial, 
+      jps.UseCDs
+      and not evocating },
 		
-		--CDs
-		{ "Mirror Image",     jps.UseCDs },
-		{jps.useTrinket(1),     jps.UseCDs},
-		{jps.useTrinket(2),     jps.UseCDs},
-		
-		--aoe
-		{ "Flamestrike",      jps.MultiTarget and IsShiftKeyDown() ~= nil },
-		
-		--rotation
-		{ "frostfire bolt", jps.buff("brain freeze") },
-		{ "ice lance", jps.buffStacks("fingers of frost") > 1 },
-		{ "frostbolt" },
-		{ "ice lance", jps.Moving },
+    -- Alter Time whenver we have decent buffs.
+    { "Alter Time",
+      jps.UseCDs
+      and not evocating
+      and ( fofActive or bfActive or pomActive or ivActive ) },
+    
+    -- Instant Frostfire Bolt when we have Brain Freeze buff.
+		{ "Frostfire Bolt", 
+      bfActive or pomActive },
+    
+    -- Ice Lance when we have Fingers of Frost buff.
+		{ "Ice Lance", 
+      fofActive },
+    
+		-- Living Bomb. (talent based)
+		{ "Living Bomb", 
+			jps.debuffDuration("Living Bomb") < 1 },
+
+		-- Frost Bomb. (talent based)
+		{ "Frost Bomb", 
+			jps.debuffDuration("Frost Bomb") == 0 },
+
+		-- Nether Tempest. (talent based)
+		{ "Nether Tempest", 
+			jps.debuffDuration("Nether Tempest") < 1 },
+    
+		-- Frozen Orb
+		{ "Frozen Orb", 
+			jps.UseCDs },
+        
+    -- Frostbolt if we're not moving.
+		{ "Frostbolt", not jps.Moving },
+    
+    -- Scorch if we are moving. (talent based)
+		{ "Scorch", jps.Moving },
+    
+    -- Ice Lance if we are moving.
+		{ "Ice Lance", jps.Moving },
+    
 	}
-
- local spell,target = parseSpellTable(spellTable)
-   if spell == "Flamestrike" then
-       jps.Cast( spell )
-       jps.groundClick()
-   end
-
-   jps.Target = target
-   return spell
+  
+  return parseSpellTable(possibleSpells)
+  
 end
