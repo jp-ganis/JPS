14f92d1f81138992b43977a8029abdc5076502aa
nishikazuhiro <nishikazuhiro@me.com>
2013-03-20 16:10:37 -0300

    Missing a ","
    It was missing a "," at line 91.

--- Rotations/paladin_protadin.lua
+++ Rotations/paladin_protadin.lua
@@ -88,7 +88,7 @@ function paladin_protadin(self)
     { "Judgment" },      
     
     -- Damage (Single target)
-    { "Crusader Strike"
+    { "Crusader Strike",
        not jps.MultiTarget },
     
     -- Damage
