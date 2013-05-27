a51685063624a7dd49028797b4da6ee4076bb5eb
PCMDGIT <karol@kutrowski.de>
2013-05-27 10:31:37 +0200

    added jps tips for testing functions

---
+++ jptips.lua
@@ -0,0 +1,15 @@
+--[[
+|cffe5cc80 = beige (artifact)
+|cffff8000 = orange (legendary)
+|cffa335ee = purple (epic)
+|cff0070dd = blue (rare)
+|cff1eff00 = green (uncommon)
+|cffffffff = white (normal)
+|cff9d9d9d = gray (crappy)
+|cFFFFff00 = yellow
+|cFFFF0000 = red
+]]
+
+-----------------------
+-- FUNCTION TEST 
+-----------------------
