3a5709122eb49c1b63ffa5ebd53f1ea116628d9c
PCMDGIT <karol@kutrowski.de>
2013-05-27 10:54:28 +0200

    updated mage frost

--- Rotations/mage_frost.lua
+++ Rotations/mage_frost.lua
@@ -1,214 +1,205 @@
-function mage_frost(self)
+-- SEE BOTTOM FOR USER NOTES
+-- by: SwollNMember WoW v5.2 compliant
+function mage_frost()
 
-	if UnitCanAttack("player","target") ~= 1 or UnitIsDeadOrGhost("target") == 1 then return end
-  
-	local castingSpell, _, _, _, _, endTime = UnitCastingInfo("player")
-  
-  local evocating = (castingSpell == "Evocation")
-  local atActive = jps.buff("Altered Time")
-  local pomActive = jps.buff("Presence of Mind")
-  local fofActive = jps.buff("Fingers of Frost")
-  local bfActive = jps.buff("Brain Freeze")
-  local ivActive = jps.buff("Icy Veins")
-  
-  local targetType = UnitClassification("target")
-  local onBoss = (targetType == 'worldboss')
-  
-	local possibleSpells = {
+-- Player Specific
+	local player = jpsName
+	local mana = UnitPower(player,0)/UnitPowerMax(player,0)
+	local atActive = jps.buff("Altered Time")
+	local fofActive = jps.buff("Fingers of Frost")
+	local bfActive = jps.buff("Brain Freeze")
+	local fbStacks = jps.debuffStacks("frostbolt")
+	local stun = jps.StunEvents()
+	local isFalling = IsFalling()==1
+-- Unit Info
+	local targetName = GetUnitName("target")
+	local targetClass = UnitClass("target")
+	local targetSpec = GetSpecialization("target")
+	local targethealth_pct = jps.hpInc("target")
+	local dmgBlock = jps.buff("ice block",rangedTarget) or jps.buff("devine shield",rangedTarget) or jps.buff("hand of protection",rangedTarget) or jps.buff("deterrence",rangedTarget)
+	local kick = jps.shouldKick(rangedTarget) or jps.IsCastingPoly(rangedTarget)
 
-		-- Flamestrike when holding down shift.
-		{ "Flamestrike", 
-		  IsShiftKeyDown() ~= nil
-      and GetCurrentKeyBoardFocus() == nil
-      and not jps.Moving
-      and not evocating },
-    
-		-- Freeze when holding down control.
-		{ "Freeze", 
-			IsControlKeyDown() ~= nil
-      and GetCurrentKeyBoardFocus() == nil },
-    
-		-- Rune of Power when holding down alt. (talent based)
-		{ "Rune of Power", 
-			IsAltKeyDown() ~= nil
-      and GetCurrentKeyBoardFocus() == nil
-      and not jps.Moving
-      and not evocating },
-      
-		-- Ice Block when you're about to die.
-		{ "Ice Block",
-			jps.hp() < .3
-			and not jps.buff("Ice Block")
-			and not jps.debuff("Hypothermia")
-      and not evocating },
-
-		{ { "macro", "/cancelaura Ice Block" }, 
-			jps.hp() > .8
-			and jps.buff("Ice Block") },
-
-    -- Healthstone if you get low.
-    { "Healthstone",
-      jps.hp() < .5
-      and GetItemCount("Healthstone", 0, 1) > 0 },
-        
-		-- Refresh your Ice block. (talent based)
-		{ "Cold Snap",
-			jps.cooldown("Ice Block") > 0
-			and jps.cooldown("Cold Snap") == 0
-      and not evocating },
-		
-		-- Incanter's Ward when you're taking some damage. (talent based)
-		{ "Incanter's Ward",
-			jps.hp() < .9
-      and not atActive
-      and not evocating },
-    
-		-- Ice Barrier when you're taking some damage. (talent based)
-		{ "Ice Barrier",
-			jps.hp() < .85
-      and not evocating },
-
-		-- Interrupts.
-		{ "Counterspell", 
-			jps.Interrupts 
-      and jps.shouldKick()
-      and not evocating },
-
-		-- Molten Armor if you forgot to buff it.
-		{ "Frost Armor", 
-			not jps.buff("Frost Armor")
-      and not jps.Moving },
-
-		-- Arcane Brilliance if you forgot to buff it.
-		{ "Arcane Brilliance", 
-			not jps.buff("Arcane Brilliance") },
-    
-		-- Evocation whenever you're missing the damage buff.
-		-- ** Important ** This assumes you have the Invocation talent. Comment this line our if you don't.
-    -- If you have the talent Rune of Power and find yourself casting it over and over again, it's because
-    -- it replaces Evocation and the following command will keep casting it because you don't have Invoker's Energy,
-    -- real pain to track down...
-		-- { "Evocation",
-    --  jps.UseCDs
-    --  and not jps.Moving
-		--	and not jps.buff("Invoker's Energy")
-		--	and jps.cooldown("Evocation") == 0
-		--	and not pomActive
-    --  and not atActive },
-    
-		-- Mirror Image is a minor DPS increase.
-		{ "Mirror Image", 
-			jps.UseCDs
-      and not evocating
-      and not atActive },
-
-		-- PoM for insta-frostbolt. (talent based)
-		{ "Presence of Mind",
-      jps.UseCDs
-      and not evocating
-      and not atActive },
-    
-		-- Icy Veins for haste buff.
-		{ "Icy Veins",
-      jps.UseCDs
-      and not evocating
-      and not atActive },
-
-		-- Engineers may have synapse springs on their gloves (slot 10).
-    { jps.useSlot(10), 
-      jps.UseCDs
-      and not evocating
-      and not atActive },
-
-		-- On-use Trinkets.
-    { jps.useSlot(13), 
-      jps.UseCDs
-      and not evocating
-      and not atActive },
-    { jps.useSlot(14), 
-      jps.UseCDs
-      and not evocating
-      and not atActive },
-
-    -- Lifeblood on cooldown. (profession based)
-    { "Lifeblood",
-      jps.UseCDs
-      and not evocating
-      and not atActive },
-
-    -- DPS Racial on cooldown.
-    { jps.DPSRacial, 
-      jps.UseCDs
-      and not evocating },
-		
-    -- Alter Time whenver we have decent buffs.
-    { "Alter Time",
-      jps.UseCDs
-      and not evocating
-      and ( ( fofActive and bfActive ) or ivActive ) },
-    
-    -- Instant Frostfire Bolt when we have Brain Freeze buff.
-		{ "Frostfire Bolt", 
-      not evocating
-      and ( bfActive
-        or pomActive ) },
-    
-    -- Ice Lance when we have Fingers of Frost buff.
-		{ "Ice Lance", 
-      fofActive
-      and not evocating },
-    
-    -- Spread Living Bomb with Fireblast (talent and glyph based).
-    { "Fire Blast", 
-      jps.MultiTarget 
-      and jps.debuff("Living Bomb")
-      and not evocating },
-    
-		-- Living Bomb. (talent based)
-		{ "Living Bomb", 
-			jps.debuffDuration("Living Bomb") < 1
-      and not evocating },
-
-		-- Frost Bomb. (talent based)
-		{ "Frost Bomb", 
-			jps.debuffDuration("Frost Bomb") == 0
-      and not jps.Moving
-      and not evocating },
-
-		-- Nether Tempest. (talent based)
-		{ "Nether Tempest", 
-			jps.debuffDuration("Nether Tempest") < 1
-      and not evocating },
-    
-		-- Frozen Orb
-		{ "Frozen Orb", 
-			jps.UseCDs
-      and not evocating },
-    
-    -- Ice Ward for a big nova on the tank if we're multi target. (talent based)
-    { "Ice Ward",
-      jps.MultiTarget,
-      jps.findMeATank()
-      and not evocating },
-    
-    -- Scorch if we are moving. (talent based)
-		{ "Scorch", jps.Moving },
-    
-    -- Ice Lance if we are moving and don't have scorch.
-		{ "Ice Lance", jps.Moving },
-    
-    -- Frostbolt filler.
-		{ "Frostbolt",
-       not evocating },
+------------------------
+-- LOCAL FUNCTIONS -----
+------------------------
+-- COUNTERS
+-- Death Knight
+	local function parse_vsDK()
+	local table =
+	{
+	  -- { "empty", empty , empty },
 	}
-  
-	local spell, target = parseSpellTable(possibleSpells)
-	jps.Target = target
-  
-  if spell == "Flamestrike" or spell == "Rune of Power" or spell == "Freeze" then
-    jps.groundClick()
-  end
-  
-	return spell
-  
+	return table
+	end
+-- Druid
+	local function parse_vsDruid()
+	local table =
+	{
+	  -- { "empty", empty , empty },
+	}
+	return table
+	end
+-- Hunter
+	local function parse_vsHunter()
+	local table =
+	{
+	  -- { "empty", empty , empty },
+	}	
+	return table
+	end
+-- Mage
+	local function parse_vsMage()
+	local table =
+	{
+	  -- { "empty", empty , empty },
+	}
+	return table
+	end	
+-- Monk
+-- Paladin
+	local function parse_vsPaladin()
+	local table =
+	{
+	  -- { "empty", empty , empty },
+	}
+	return table
+	end
+-- Priest
+	local function parse_vsPriest()
+	local table =
+	{
+	  -- { "empty", empty , empty },
+	}
+	return table
+	end
+-- Rogue
+	local function parse_vsRogue()
+	local table =
+	{
+	  -- { "empty", empty , empty },
+	}
+	return table
+	end
+-- Shaman
+	local function parse_vsShaman()
+	local table =
+	{
+	  -- { "empty", empty , empty },	 
+	}
+	return table
+	end
+-- Warlock
+	local function parse_vsWarlock()
+	local table =
+	{
+	  -- { "empty", empty , empty },
+	}
+	return table
+	end	
+-- Warrior
+	local function parse_vsWarrior()
+	local table =
+	{
+	  -- { "empty", empty , empty },
+	}
+	return table
+	end
+ 
+
+-- Remove Curse
+	local function decurse()
+	  if jps.debuff("curse of the elements",player) then return true end
+	  if jps.debuff("curse of enfeeblement",player) then return true end
+	  if jps.debuff("curse of exhaustion",player) then return true end
+	  if jps.debuff("agony",player) then return true end
+	  if jps.debuff("doom",player) then return true end
+	  if jps.debuff("havok",player) then return true end
+	  if jps.debuff("hex",player) then return true end
+	return false
+	end
+ 
+-- Enemy Tracking
+	local enemycount,targetcount = jps.TableEnemyCount()
+	local EnemyUnit = {}
+	  for name, _ in pairs(jps.RaidTarget) do table.insert(EnemyUnit,name) end
+	local rangedTargetCount = jps.RaidEnemyTargetCount()
+	local rangedTarget = "target"
+	  if jps.canDPS("target") then rangedTarget = "target"
+	  elseif jps.canDPS("focustarget") then rangedTarget = "focustarget"
+	  elseif jps.canDPS("targettarget") then rangedTarget = "targettarget"
+	  elseif jps.canDPS(EnemyUnit[1]) then rangedTarget = EnemyUnit[1]
+	end
+
+	RunMacroText("/target "..rangedTarget)
+	
+------------------------
+-- SPELL TABLE ---------
+------------------------
+local spellTable =
+{
+	{ SetView(1), },
+-- Class Counters
+	{ "nested", targetClass=="Death Knight" , parse_vsDK() },
+	{ "nested", targetClass=="Hunter" , parse_vsHunter() },
+	{ "nested", targetClass=="Mage" or jps.debuff("frost nova",player) or jps.debuff("freeze",player) , parse_vsMage() },
+	{ "nested", targetClass=="Paladin" , parse_vsPaladin() },
+	{ "nested", targetClass=="Rogue" or jps.debuff("cheap shot",player) or jps.debuff("kidney shot",player) , parse_vsRogue() },
+	{ "nested", targetClass=="Shaman" , parse_vsShaman() },
+	{ "nested", targetClass=="Warlock" , parse_vsWarlock() },
+	{ "nested", targetClass=="Warrior" , parse_vsWarrior() },
+	
+-- Fall/Knockback Response
+	{ "slow fall", isFalling , player },
+	
+-- Gap Closers
+-- Remove Snares, Roots, Loss of Control, etc.
+	{ "every man for himself", jps.LoseControl(player,"CC") , player },
+	
+-- Kicks, Crowd Control, etc.
+	{ {"macro","/use High-Powered Bolt Gun"}, kick , rangedTarget },
+	{ "counterspell", kick , rangedTarget },
+	
+-- Survival, Heals, etc.
+	{ {"macro","/use Mana Gem"}, mana < 0.70 and GetItemCount("Mana Gem", 0, 1) > 0 , player }, 
+	{ {"macro","/cast icy veins\n/cast evocation"}, jps.hp() < .41 , player },
+	{ "Healthstone",	  jps.hp() < .7 and GetItemCount("Healthstone", 0, 1) > 0 },
+
+-- Debuffs
+	{ "remove curse", decurse() , player },
+-- Buffs
+	{ "ice barrier", not jps.buff("ice barrier") , player },
+------------------------
+-- ATTACKS -------------
+------------------------
+
+--CDs
+	{ "Mirror Image",	 jps.UseCDs },
+	{ jps.DPSRacial, jps.UseCDs },
+	{ jps.useTrinket(1), jps.UseCDs },
+	{ jps.useTrinket(2), jps.UseCDs },
+	
+	-- Requires engineerins
+	{ jps.useSynapseSprings(), jps.UseCDs },
+	
+	-- Requires herbalism
+	{ "Lifeblood", jps.UseCDs },
+	
+	
+-- Finishers
+	{ {"macro","/cast alter time\n/use 13\n/use 14\n/cast presence of mind\n/cast frostbolt"}, targethealth_pct < 0.30 or jps.debuff("deep freeze") and not dmgBlock , rangedTarget },
+	{ {"macro","/cast alter time\n/use 13\n/use 14\n/cast presence of mind\n/cast frostbolt"}, atActive and not dmgBlock , rangedTarget },
+	
+-- Base Attacks
+	{ "ice lance", jps.debuff("deep freeze") or jps.LastCast == frostbolt or jps.Moving or fofActive , rangedTarget },
+	{ "frostfire bolt", bfActive , rangedTarget },
+	{ "frost bomb", not jps.debuff("frost bomb") , rangedTarget },
+	{ "frost orb", not jps.buff("fingers of frost") or enemycount > 2 , rangedTarget },
+	{ "fire blast", IsSpellInRange("fire blast",rangedTarget) == 1 , rangedTarget },
+	{ "cone of cold", CheckInteractDistance(rangedTarget,3) == 1 , rangedTarget },
+	{ "frostbolt", fbStacks < 3 and not jps.Moving , rangedTarget },
+	{ {"macro","/startattack"}, nil , rangedTarget },
+}
+
+	local spell,target = parseSpellTable(spellTable)
+	return spell,target
 end
