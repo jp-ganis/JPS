fdf930e3a33a82e9c82ddf90306a27d40a0c6582
PCMDGIT <karol@kutrowski.de>
2013-05-27 12:19:33 +0200

    druid guradian update

--- Rotations/druid_guardian.lua
+++ Rotations/druid_guardian.lua
@@ -1,147 +1,61 @@
-function druid_guardian(self)
-  
-  if UnitCanAttack("player","target") ~= 1 or UnitIsDeadOrGhost("target") == 1 then return end
-  
-  -- Rage
+function druid_guardian()
+	-- attempted by jpganis, fixed by Attip, updated by peanutbird
+	
+	local spell = nil
+	local target = nil
+	
+	-- Other stuff
 	local rage = UnitMana("player")
-
-	local possibleSpells = {
-        
-		-- Bear Form
-	  { "Bear Form", 
-	  	not jps.buff("Bear Form") },
-    
-    -- Barkskin
-		{ "Barkskin",
-      jps.UseCDs
-      and jps.hp() < .6 },
-    
-    -- Renewal. (talent based)
-		{ "Renewal",
-      jps.UseCDs
-      and jps.hp() < .6 },
-    
-    -- Nature's Vigil. (talent based)
-		{ "Nature's Vigil",
-      jps.UseCDs
-      and jps.hp() < .55
-      and jps.hp("target") > .4 },
-    
-		-- Healing Touch whenever we have Nature's Swiftness. (talent based)
-		{ "Healing Touch", 
-			jps.buff("Nature's Swiftness") },
+	local lacCount = jps.debuffStacks("lacerate")
+	local lacDuration = jps.debuffDuration("lacerate")
+	local thrashDuration = jps.debuffDuration("thrash")
+	local hp = UnitHealth("player")/UnitHealthMax("player") * 100
+	local onCD = "onCD"
+	
+	local spellTable =
+	{
+	
+		-- Buffs
+		{ "mark of the wild",		 	not jps.buff("mark of the wild") , player },
+		{nil,									IsSpellInRange("lacerate","target") ~= 1 },
+	
+		-- Interrupts
+		{"skull bash",						jps.Interrupts and jps.shouldKick() },
+		{"mighty bash",					jps.Interrupts and jps.shouldKick() },
 		
-		-- Nature's Swiftness. (talent based)
-		{ "Nature's Swiftness",	
-			jps.hp() < .8 },
-        
-    -- Might of Ursoc
-		{ "Might of Ursoc",
-      jps.UseCDs
-      and jps.hp() < .4 },
-    
-    -- Survival Instincts
-		{ "Survival Instincts",
-      jps.UseCDs
-      and jps.hp() < .5 },
-    
-    -- Healthstone if we get low.
-    { "Healthstone",
-      jps.hp() < .6
-      and GetItemCount("Healthstone", 0, 1) > 0 },
-        
-    -- Life Spirit if we get low.
-    { "Life Spirit",
-      jps.hp() < .5
-      and GetItemCount("Life Spirit", 0, 1) > 0 },
-    
-    -- Bail now if we aren't in melee range
-		{ nil,
-      IsSpellInRange("Mangle", "target") ~= 1 },
-    
-		-- Interrupt
-		{ "Skull Bash", 
-			jps.shouldKick() 
-			and jps.Interrupts },
-
-    -- Stun. (talent based)
-		{ "Mighty Bash", 
-			jps.shouldKick() 
-			and jps.Interrupts },
-		    
-		-- Frenzied Regeneration
-		{ "Frenzied Regeneration",
-      jps.hp() < .7
-      and jps.buff("Savage Defense") },
-    
-    -- Savage Defense
-		{ "Savage Defense",
-      jps.hp() < .9 },
-    
-		-- Engineers may have synapse springs on their gloves (slot 10).
-		{ jps.useSynapseSprings(), 
-      jps.UseCDs },
-      
-		-- On-Use Trinkets.
-    { jps.useTrinket(1), 
-      jps.UseCDs },
-    { jps.useTrinket(2), 
-      jps.UseCDs },
-
-		-- DPS Racial.
-		{ jps.DPSRacial, 
-			jps.UseCDs },
-
-		-- Lifeblood. (requires herbalism)
-		{ "Lifeblood",
-			jps.UseCDs },
-
-		-- Treants. (talent specific)
-		{ "Force of Nature",
-       jps.UseCDs },
-    
- 		-- Faerie Fire to keep Weakened Armor up.
- 		{ "Faerie Fire",
-       not jps.MultiTarget
-       and not jps.debuff("Weakened Armor") },
-    
-		-- Berserk if thrash and weakened armor are already up.
-		{ "Berserk",
-      jps.UseCDs
-      and jps.debuff("Thrash")
-      and jps.debuff("Weakened Armor") },
-    
-    -- Mangle on cooldown.
-    { "Mangle" },
-    
-    -- Thrash on cooldown.
-		{ "Thrash" },
-    
-    -- Lacertate on cooldown for single-target.
-		{ "Lacerate",
-      not jps.MultiTarget },
-    
-    -- Maul when Tooth and Claw procs or as a rage dump.
-    { "Maul", 
-      not jps.MultiTarget
-      and (
-        jps.buff("Tooth and Claw")
-        or rage > 80 ) },
-    
-    -- Enrage if we're low on rage.
-		{ "Enrage",
-      rage <= 20 },
-    
-    -- Swipe on cooldown.
-    { "Swipe",
-       IsSpellInRange("Mangle", "target") },
+		-- Healing / Support
+		{"heart of the wild",			IsControlKeyDown() ~= nil},
+		{"rejuvenation",					jps.buff("heart of the wild") and hp < 75 and not jps.buff("rejuvenation")},
+		{"rejuvenation", 				jps.buff("heart of the wild") and IsControlKeyDown() ~= nil and IsSpellInRange("rejuvenation", "mouseover"), "mouseover" },
+		
+		-- Defense
+		{"barkskin",						hp < 75 and jps.UseCDs},
+		{"survival instincts",			hp < 50 and jps.UseCDs},
+		{"might of ursoc",				hp < 25 and jps.UseCDs},
+		{"frenzied regeneration",	hp < 55 and jps.buff("savage defense")},
+		{"savage defense",				hp < 90 and rage >= 60},
+		{"renewal", 						hp < 20 and jps.UseCDs },
+		{"nature’s swiftness", 			hp < 20 and jps.UseCDs },
+		{"healing touch", 				hp < 20 and jps.buff("nature's swiftness") and jps.UseCDs },
+		{"enrage",							rage <= 10 and hp > 95},
+		
+		-- Offense
+		{"berserk",						jps.UseCDs and jps.debuff("thrash") and jps.debuff("faerie fire")},
+	
+		-- Multi-Target
+		{"thrash",			jps.MultiTarget and not jps.debuff("thrash")},
+		{"mangle",			jps.MultiTarget },
+		{"swipe",			jps.MultiTarget },
+		
+		-- Single Target
+		{"mangle",			onCD or jps.buff("berserk") },
+		{"maul",			rage > 90 and hp >= 85 },	
+		{"faerie fire",		not jps.debuff("weakened armor") },
+		{"thrash",			not jps.debuff("thrash") or thrashDuration < 3 },
+		{"lacerate",		lacCount < 3 or lacDuration < 1 },
+		{"faerie fire",		onCD },
 	}
 
-	return parseSpellTable(possibleSpells)
-  
+	spell,target = parseSpellTable(spellTable)
+	return spell,target
 end
-
--- Yellow: Smooth, Fractured
--- Purple: Accurate, Assassin's, Defender's, Etched, Guardian's, Purified, Tense, Veiled
--- Green: Effulgent, Forceful, Jagged, Lightning, Misty, Nimble, Piercing, Puissant, Radiant, Sensei's, Shattered, Turbid, Vivid, Zen
--- Orange: Deft, Fierce, Fine, Inscribed, Keen, Lucent, Polished, Potent, Skillful, Splendid, Stalwart, Tenuous, Wicked
