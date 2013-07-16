62b57a3a6dedd719d66874653cbc14572700478f
PCMDGIT <karol@kutrowski.de>
2013-07-16 12:34:38 +0200

    ele shaman by duplicate

--- Rotations/shaman_elemental.lua
+++ Rotations/shaman_elemental.lua
@@ -1,71 +1,52 @@
-jps.registerStaticTable("SHAMAN","ELEMENTAL",function()
-	-- Updated for MoP
-	-- Tier 1: Astral Shift
-	-- Tier 2: Windwalk Totem
-	-- Tier 3: Call of the Elements
-	-- Tier 4: Echo of the Elements
-	-- Tier 5: Healing Tide Totem
-	-- Major Glyphs: Flame Shock (required), Spiritwalker's Grace (recommended),
-	-- Telluric Currents (recommended)
-	-- Minor Glyphs: Thunderstorm (required)
-	
-	local lsStacks = jps.buffStacks("lightning shield")
-	local focus = "focus"
-	local me = "player"
-	local mh, _, _, oh, _, _, _, _, _ =GetWeaponEnchantInfo()
-	local engineering ="/use 10"
-	local r = jps.Macro
-	
-	-- Totems
-	local _, fireTotem, _, _, _ = GetTotemInfo(1)
-	local _, earthTotem, _, _, _ = GetTotemInfo(2)
-	local _, waterName, _, _, _ = GetTotemInfo(3)
-	local _, airTotem, _, _, _ = GetTotemInfo(4)
-	
-	local fireTotemActive = fireTotem ~= ""
-	local earthTotemActive = earthTotem ~= ""
-	local waterTotemActive = waterName ~= ""
-	local airTotemActive = airTotem ~= ""
-	
-	-- Fear
-	local feared = jps.debuff("Fear") or jps.debuff("Intimidating Shout") or jps.debuff("Howl of Terror") or jps.debuff("Psychic Scream")
-	
-	local spellTable = {
-		{ "Lightning Shield", not jps.buff("Lightning Shield") },
-		{ "Flametongue Weapon", not mh },
-		{ "Astral Shift", jps.hp() < .35 },
-		{ "Healing Tide Totem", jps.hp() < .5 },
-		{ "Healing Surge", jps.hp() < .7 },
-		{ "Fire Elemental Totem", jps.UseCDs },
-		{ "Searing Totem", not fireTotemActive },
-		{ "Earth Elemental Totem", jps.UseCDs and jps.bloodlusting() },
-		{ "Stormlash Totem", jps.UseCDs and jps.bloodlusting() },
+-- Updated for MoP
+-- Tier 1: Astral Shift
+-- Tier 2: Windwalk Totem
+-- Tier 3: Call of the Elements
+-- Tier 4: Echo of the Elements
+-- Tier 5: Healing Tide Totem
+-- by  duplicate
+
+function weaponMainhandEnchant()
+	return select(1, GetWeaponEnchantInfo())
+end
+
+function totemActive(totemId)
+	-- 1 = fire
+	-- 2 = earth
+	-- 3 = water
+	-- 4 = air
+	local _, totemName, _, _, _ = GetTotemInfo(totemId)
+	return totemName ~= ""
+end
+
+jps.registerStaticTable("SHAMAN","ELEMENTAL",
+	{
+		{ "Wind Shear", 'jps.shouldKick("target")',"target"},
+		{ "Wind Shear", 'jps.shouldKick("focus")',"focus"},
+		{ "Lightning Shield",'not jps.buff("Lightning Shield")' },
+		{ "Flametongue Weapon", 'not weaponMainhandEnchant()', "player"  },
+		{ "Astral Shift", 'jps.hp() < 0.35 '},
+		{ "Healing Tide Totem",' jps.hp() < 0.5 and jps.UseCDs'},
+		{ "Healing Surge", 'jps.hp() < 0.70' },
+		{ "Fire Elemental Totem", 'jps.UseCDs '},
+		{ "Searing Totem", 'not totemActive(1)' },
+		{ "Earth Elemental Totem", 'jps.UseCDs and jps.bloodlusting()' },
+		{ "Stormlash Totem",' jps.UseCDs and jps.bloodlusting()' },
 		
-		-- Trinket CDs. 
-		{ jps.useTrinket(0), jps.UseCDs },
-		{ jps.useTrinket(1), jps.UseCDs },
-		
-		-- Synapse Springs CD. (engineering gloves) 
-		{ jps.useSynapseSprings(), jps.UseCDs },
-		
-		-- Lifeblood CD. (herbalists) 
-		{ "Lifeblood", jps.UseCDs },
-		
-		-- DPS Racial CD. 
-		{ jps.DPSRacial, jps.UseCDs },
-		
+		{ jps.getDPSRacial(),'jps.UseCDs '},
+		{ jps.useTrinket(0),'jps.useTrinket(0) ~= nil and jps.UseCDs '},
+		{ jps.useTrinket(1),'jps.useTrinket(1) ~= nil and jps.UseCDs '},
+		{ jps.useSynapseSprings(),'jps.useSynapseSprings() ~= nil and jps.UseCDs '},
+		-- Requires herbalism
+		{"Lifeblood",'jps.UseCDs '},
 		-- Prio-List
-		{ "Wind Shear", jps.shouldKick() },
-		{ "Unleash Elements", jps.debuffDuration("Flame Shock") < 2 },
-		{ "Flame Shock", jps.buff("Unleash Flame") },
-		{ "Lava Burst", jps.debuff("Flame Shock") },
-		{ "Earth Shock", lsStacks > 5 and jps.debuffDuration("Flame Shock") > 5 },
-		{ "Spiritwalker's Grace", jps.Moving },
-		{ "Chain Lightning", jps.MultiTarget },
-		{ "Thunderstorm", jps.mana() < .6 and jps.UseCDs },
-		{ "Lightning Bolt" },
+		{ "Unleash Elements",' jps.myDebuffDuration("Flame Shock") < 2'},
+		{ "Flame Shock",' jps.buff("Unleash Flame")' },
+		{ "Lava Burst", 'jps.mydebuff("Flame Shock")' },
+		{ "Earth Shock", 'jps.buffStacks("lightning shield") > 5 and jps.myDebuffDuration("Flame Shock") > 5 '},
+		{ "Spiritwalker's Grace", 'jps.Moving' },
+		{ "Chain Lightning", 'jps.MultiTarget' },
+		{ "Thunderstorm", 'jps.mana() < 0.6 and jps.UseCDs' },
+		{ "Lightning Bolt",'onCD' },
 	}
-	
-	local spell,target = parseSpellTable(spellTable)
-	return spell,target
-end, "Default")
+,"Elemental Shaman PVE")
