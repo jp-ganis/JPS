be14a52ce286461219a03c56b2e3987bdaeda5ab
htordeux <h.tordeux@laposte.net>
2013-07-17 08:18:18 +0200

    no message

---
+++ Rotations/priest_disc.lua
@@ -0,0 +1,12 @@
+function priest_disc()
+local isArena, isRegistered = IsActiveBattlefieldArena()
+	if jps.PvP then
+		return priest_disc_pvp() 
+	elseif isArena == 1 then
+		return priest_disc_pvp()
+	else
+		return priest_disc_pve()
+	end
+end
+
+
