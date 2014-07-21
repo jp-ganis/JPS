06d0d97c28ef7b4b8d04b4aabd69f478c02bfc6b
pcmd <karol@kutrowski.de>
2014-07-21 15:47:22 +0200

    testings charred remains

--- Rotations/warlock_destro.lua
+++ Rotations/warlock_destro.lua
@@ -80,25 +80,6 @@ local spellTable = {
 }
 
 
---[[[
-@rotation Destruction 5.4
-@class warlock
-@spec destruction
-@talents Vb!112101!ZbS
-@author Kirk24788
-@description
-This is a Raid-Rotation, which will do fine on normal mobs, even while leveling but might not be optimal for PvP.
-[br]
-Modifiers:[br]
-[*] [code]SHIFT[/code]: Cast Rain of Fire @ Mouse - [b]ONLY[/b] if RoF Duration is less than 1 seconds[br]
-[*] [code]CTRL-SHIFT[/code]: Cast Rain of Fire @ Mouse - ignoring the current RoF duration[br]
-[*] [code]ALT-SHIFT[/code]: Cast Shadowfury @ Mouse[br]
-[*] [code]CTRL[/code]: If target is dead or ghost cast Soulstone, else cast Havoc @ Mouse[br]
-[*] [code]ALT[/code]: Stop all casts and only use instants (useful for Dark Animus Interrupting Jolt)[br]
-[*] [code]jps.Interrupts[/code]: Casts from target, focus or mouseover will be interrupted (with FelHunter or Observer only!)[br]
-[*] [code]jps.Defensive[/code]: Create Healthstone if necessary, cast mortal coil and use ember tap[br]
-[*] [code]jps.UseCDs[/code]: Use short CD's - NO Virmen's Bite, NO Doomguard/Terrorguard etc. - those SHOULDN'T be automated![br]
-]]--
 jps.registerRotation("WARLOCK","DESTRUCTION",function()
 	wl.deactivateBurningRushIfNotMoving(1)
 
@@ -115,12 +96,85 @@ jps.registerRotation("WARLOCK","DESTRUCTION",function()
 end,"Destruction 6.0b")
 
 
---[[[
-@rotation Interrupt Only
-@class warlock
-@spec destruction
-@author Kirk24788
-@description
-This is Rotation will only take care of Interrupts. [i]Attention:[/i] [code]jps.Interrupts[/code] still has to be active!
-]]--
-jps.registerStaticTable("WARLOCK","DESTRUCTION",wl.interruptSpellTable,"Interrupt Only")
+
+local spellTableCharred = {
+	-- Interrupts
+	wl.getInterruptSpell("target"),
+	wl.getInterruptSpell("focus"),
+	wl.getInterruptSpell("mouseover"),
+	
+	-- Def CD's
+	{wl.spells.mortalCoil, 'jps.Defensive and jps.hp() <= 0.80' },
+	{wl.spells.createHealthstone, 'jps.Defensive and GetItemCount(5512, false, false) == 0 and jps.LastCast ~= wl.spells.createHealthstone'},
+	{jps.useBagItem(5512), 'jps.hp("player") < 0.65' }, -- Healthstone
+	
+	{wl.spells.emberTap, 'jps.Defensive and jps.hp() <= 0.30 and jps.burningEmbers() > 0' },
+
+	-- Soulstone
+	wl.soulStone("target"),
+
+	-- Rain of Fire
+	{wl.spells.rainOfFire, 'IsShiftKeyDown() and jps.buffDuration(wl.spells.rainOfFire) < 1 and not GetCurrentKeyBoardFocus()'	},
+	{wl.spells.rainOfFire, 'IsShiftKeyDown() and IsControlKeyDown() and not GetCurrentKeyBoardFocus()' },
+
+	{wl.spells.cataclysm, 'IsShiftKeyDown() and IsAltKeyDown() and not GetCurrentKeyBoardFocus()'},
+
+
+	{wl.spells.fireAndBrimstone, 'jps.emberShards() > 15 and not jps.buff(wl.spells.fireAndBrimstone, "player") and jps.MultiTarget and not jps.isRecast(wl.spells.fireAndBrimstone, "target")' },
+	{ {"macro","/cancelaura "..wl.spells.fireAndBrimstone}, 'jps.buff(wl.spells.fireAndBrimstone, "player") and jps.burningEmbers() == 0' },
+	{ {"macro","/cancelaura "..wl.spells.fireAndBrimstone}, 'jps.buff(wl.spells.fireAndBrimstone, "player") and not jps.MultiTarget' },
+
+	-- CD's
+	{"nested", 'jps.canDPS("target") and not jps.Moving', {
+		{ {"macro","/cast " .. wl.spells.darkSoulInstability}, 'jps.cooldown(wl.spells.darkSoulInstability) == 0 and not jps.buff(wl.spells.darkSoulInstability) and jps.UseCDs' },
+		{ jps.getDPSRacial(), 'jps.UseCDs' },
+		{wl.spells.lifeblood, 'jps.UseCDs' },
+		{ {"macro","/use 10"}, 'jps.useSynapseSprings() ~= "" and jps.UseCDs' },
+		{ jps.useTrinket(0),	   'jps.UseCDs' },
+		{ jps.useTrinket(1),	   'jps.UseCDs' },	
+	}},
+	-- Shadowburn mouseover!
+	{wl.spells.shadowburn, 'jps.hp("mouseover") < 0.20 and jps.burningEmbers() > 0 and jps.myDebuffDuration(wl.spells.shadowburn, "mouseover")<=0.5  and unitNotGarroshMCed("target")', "mouseover"  },
+
+	{"nested", 'not jps.MultiTarget and not IsAltKeyDown()', {
+		{wl.spells.havoc, 'not IsShiftKeyDown() and IsControlKeyDown() and not GetCurrentKeyBoardFocus()', "mouseover" },
+		{wl.spells.havoc, 'not jps.Moving and wl.attackFocus()', "focus" },
+		{wl.spells.shadowburn, 'jps.hp("target") <= 0.20 and jps.burningEmbers() > 0  and unitNotGarroshMCed("target")'  },
+		{wl.spells.chaosBolt, 'jps.burningEmbers() > 0 and jps.buffStacks(wl.spells.havoc)>=3'},
+		{"nested", 'not jps.Moving', {
+			jps.dotTracker.castTableStatic("immolate"),
+		}},
+		{wl.spells.conflagrate },
+		{wl.spells.chaosBolt, 'not jps.Moving and jps.buff(wl.spells.darkSoulInstability) and jps.emberShards() >= 10' ,"target" },
+		{wl.spells.chaosBolt, 'not jps.Moving and jps.emberShards() >= 20' ,"target"},
+		{wl.spells.chaosBolt, 'not jps.Moving and wl.hasProc(1) and jps.emberShards() >= 10 and jps.buffStacks(wl.spells.backdraft) < 3' ,"target"},
+		{wl.spells.incinerate },
+	}},
+
+	{"nested", 'not jps.MultiTarget and IsAltKeyDown()', {
+		{wl.spells.shadowburn, 'jps.hp("target") <= 0.20 and jps.burningEmbers() > 0'  },
+		{wl.spells.conflagrate },
+	}},
+	
+	{"nested", 'jps.MultiTarget', {
+		{wl.spells.immolate , 'jps.buff(wl.spells.fireAndBrimstone, "player") and jps.myDebuffDuration(wl.spells.immolate) <= 2.0 and jps.LastCast ~= wl.spells.immolate'},
+		{wl.spells.conflagrate, 'jps.buff(wl.spells.fireAndBrimstone, "player") and jps.emberShards() < 35' },
+		{wl.spells.chaosBolt, 'jps.emberShards() >= 30'}, 
+		{wl.spells.incinerate },
+	}},
+}
+
+jps.registerRotation("WARLOCK","DESTRUCTION",function()
+	wl.deactivateBurningRushIfNotMoving(1)
+
+	if IsAltKeyDown() and jps.CastTimeLeft("player") >= 0 then
+		SpellStopCasting()
+		jps.NextSpell = nil
+	end
+	
+	if jps.IsSpellKnown("Shadowfury") and jps.cooldown("Shadowfury") == 0 and IsAltKeyDown() and not GetCurrentKeyBoardFocus() and wl.btn("altShadowfury") then
+		jps.Cast("Shadowfury")
+	end
+	
+	return parseStaticSpellTable(spellTableCharred)
+end,"Destruction Charred Remais 6.0b")
