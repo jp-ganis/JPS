1a61cad1d88a8e76f19f8c1b4d1a7df8207b92fb
pcmd <karol@kutrowski.de>
2014-10-21 08:06:34 +0200

    Update jpparse.lua

--- jpparse.lua
+++ jpparse.lua
@@ -148,10 +148,12 @@ end
 function jps.canDPS(unit) 
 	if not jps.UnitExists(unit) then return false end
 	if jps.PvP then 
-		local iceblock = tostring(select(1,GetSpellInfo(45438))) -- ice block mage
-		local divineshield = tostring(select(1,GetSpellInfo(642))) -- divine shield paladin
-		if jps.buff(divineshield,unit) then return false end
-		if jps.buff(iceblock,unit) then return false end
+		if jps.buff("Ice Block", unit) then return false end
+		if jps.buff("Dispersion", unit) then return false end
+		if jps.buff("Divine Shield", unit) then return false end
+		if jps.buff("Deterrence", unit) then return false end
+		if jps.buff("Evasion", unit) then return false end
+		if jps.buff("Cloak of Shadows", unit) then return false end
 	end
 	if (GetUnitName(unit) == L["Training Dummy"]) or (GetUnitName(unit) == L["Raider's Training Dummy"]) then return true end	
 	if UnitCanAttack("player", unit)==false then return false end-- UnitCanAttack(attacker, attacked) return 1 if the attacker can attack the attacked, nil otherwise.
