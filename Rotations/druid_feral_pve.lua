cf25359f41ef86b2a4d647cbc28d0bb8e1aef3e0
pcmd <karol@kutrowski.de>
2014-10-27 09:49:40 +0100

    Update druid_feral_pve.lua

--- Rotations/druid_feral_pve.lua
+++ Rotations/druid_feral_pve.lua
@@ -1,195 +1,142 @@
---[[[
-@rotation Default PvE
-@class DRUID
-@spec FERAL
-@author jpganis
-@description 
-Ty to MEW Feral Sim
-]]--
+
+if not druid then druid = {} end
+if not druid.spells then druid.spells = {} end
+druid.spells.arcaneTorrent = "arcane torrent";	      
+druid.spells.berserk = "berserk";	    
+druid.spells.berserking = "Berserking";  
+druid.spells.bloodFury = "blood fury";	      
+	      
+druid.spells.dreamOfCenarius = "Dream of Cenarius";	      
+druid.spells.faerieFire = "faerie fire";	      
+druid.spells.feralRage = "feral rage";	      
+druid.spells.ferociousBite = "ferocious bite";	      
+druid.spells.forNext = "for next";	      
+druid.spells.forceOfNature = "force of nature";	      
+druid.spells.healingTouch = "healing touch";	      
+druid.spells.name = "name";	      
+druid.spells.omenOfClarity = "omen of clarity";	      
+druid.spells.poolResource = "pool resource";	      
+druid.spells.predatorySwiftness = "predatory swiftness";	      
+druid.spells.rake = "rake";	      
+druid.spells.rip = "rip";	      
+druid.spells.runeOfReorigination = "rune of reorigination";	      
+druid.spells.savageRoar = "savage roar";	      
+druid.spells.skullBashCat = "skull bash";	      
+druid.spells.slot = "slot";	      
+druid.spells.stealthed = "prowl";	
+druid.spells.prowl = "prowl";	
+druid.spells.shred = "shred";
+druid.spells.kingOfTheJungle = "King Of the Jungle";    
+druid.spells.swipeCat = "swipe";	      
+druid.spells.thrashCat = "thrash";	      
+druid.spells.tigersFury = "tiger's fury";	      
+druid.spells.vicious = "vicious";	      
+druid.spells.virmensBitePotion = "virmens bite potion";	      
+druid.spells.weakenedArmor = "weakened armor";	    
+druid.spells.markOfTheWild = "Mark of the Wild";
+druid.spells.mangle = "mangle";
+druid.spells.feralFury ="Feral Fury";
+
+druid.spells.energyRegen = function() 
+	return select(1,GetPowerRegen())
+end
+
+druid.spells.cp = function()
+	return GetComboPoints("player")
+end
+druid.spells.timeToMax = function() 
+	return (100- UnitMana("player")) / druid.spells.energyRegen()
+end
+
+druid.spells.energy = function()
+	return UnitMana("player")
+end
+
+if jps.TimetoDie == nil then
+	jps.TimetoDie = function(unit) 
+		if DeathClock_TimeTillDeath ~= nil then
+			return jps.cachedValue(function()
+				return  DeathClock_TimeTillDeath(unit) 
+			end , 1)
+		end
+	end
+end
+
+jps.sub = function(p1,p2)
+	return p1 - p2
+end
+
+jps.add = function(p1,p2)
+	return p1+p2
+end
+
+
+local spellTable = {
+		
+ -- buffs
+	{nil, 'IsControlKeyDown() == true'},
+	{druid.spells.barksin, 'jps.hp("player") < 0.5'},
+	{druid.spells.markOfTheWild, 'not jps.hasStatsBuff("player") and not jps.buff("Cat Form")'},
+	{jps.useBagItem(5512), 'jps.hp("player") < 0.65' }, -- Healthstone
+	{jps.useBagItem(5512), 'jps.hp("player") < 0.90 and jps.debuff("weak ancient barrier")' }, --malk barrier
+	{jps.useBagItem(5512), 'jps.hp("player") < 0.99 and jps.debuff("ancient barrier")' }, --malk barrier
+	{jps.useBagItem(86569), 'not jps.buff("Flask of the spring blossom") and not jps.buff("Crystal of Insanity")'},
 	
-jps.registerRotation("DRUID","FERAL", function()
+	{ "Cat Form", ' not jps.buff("Cat Form")'},
+
+ -- cooldowns
+ 
+ 	{druid.spells.skullBashCat, 'jps.Interrupts and jps.shouldKick("target")' },
+	{druid.spells.arcaneTorrent, 'jps.Interrupts and jps.shouldKick("target") and IsSpellInRange("Shred", "target") == 1' },
+
+	{ "nested",'IsSpellInRange("Shred", "target") == 1 and jps.UseCDs',{	
+		{druid.spells.berserking, 'jps.buff(druid.spells.tigersFury)' },
+		{druid.spells.tigersFury, 'not jps.buff(druid.spells.clearcasting) and jps.energy() >= 60' },
+		{druid.spells.tigersFury, 'jps.energy() <= 40' },
+		{druid.spells.incarnation, 'jps.buff(druid.spells.berserk)' },
+		{druid.spells.berserk, 'jps.buff(druid.spells.tigersFury)' },
+		{"Lifeblood", 'jps.UseCDs' },
+		{ jps.useTrinket(0), 'jps.buff(druid.spells.tigersFury)' },
+		{ jps.useTrinket(1), 'jps.buff(druid.spells.tigersFury)' },
+		{druid.spells.bloodFury, 'jps.buff(druid.spells.tigersFury)' },
+		
+	}},
+			
+	{druid.spells.rake, 'jps.buff(druid.spells.prowl)' },
+	{druid.spells.feralforceOfNature, 'select(1,GetSpellCharges(druid.spells.feralforceOfNature))==3 and not jps.isRecast(druid.spells.feralforceOfNature)' },
+	{druid.spells.feralforceOfNature, 'not jps.isRecast(druid.spells.feralforceOfNature) and jps.TimeToDie("target") < 20' },
+	
+	{druid.spells.ferociousBite, 'jps.myDebuff(druid.spells.rip) and jps.myDebuffDuration(druid.spells.rip) < 3 and jps.hp("target") < 0.25' },
+	{druid.spells.ferociousBite, 'GetComboPoints("player") == 5 and not jps.targetIsRaidBoss() and jps.TimeToDie("target") < 15' },
+	
+	--blood talons is L100 {druid.spells.healingTouch, 'jps.talentInfo(druid.spells.bloodtalons) and jps.buff(druid.spells.predatorySwiftness) and (comboPoints >= 4 or jps.buffDuration(druid.spells.predatorySwiftness) < 1.5)' },
+	{druid.spells.savageRoar, 'jps.buffDuration(druid.spells.savageRoar) < 3' },
+	{druid.spells.thrashCat, 'jps.buff(druid.spells.omenOfClarity) and jps.myDebuffDuration(druid.spells.thrashCat) <= 4 and jps.MultiTarget' },
+	{druid.spells.ferociousBite, 'GetComboPoints("player") == 5 and jps.hp("target") < 0.25 and jps.myDebuff(druid.spells.rip)' },
+	{druid.spells.rip, 'GetComboPoints("player") == 5 and jps.myDebuffDuration(druid.spells.rip) <= 3' },
+	{druid.spells.rip, 'GetComboPoints("player") == 5 and jps.myDebuffDuration(druid.spells.rip) <= 4' },
+	{druid.spells.savageRoar, 'GetComboPoints("player") == 5 and jps.energy() >= 90 and jps.buffDuration(druid.spells.savageRoar) < 12' },
+	{druid.spells.savageRoar, 'GetComboPoints("player") == 5 and jps.buff(druid.spells.berserk) and jps.buffDuration(druid.spells.savageRoar) < 12' },
+	{druid.spells.savageRoar, 'GetComboPoints("player") == 5 and jps.cooldown(druid.spells.tigersFury) < 3 and jps.buffDuration(druid.spells.savageRoar) < 12' },
+	
+	{druid.spells.ferociousBite, 'GetComboPoints("player") == 5 and jps.energy() >= 90' },
+	{druid.spells.ferociousBite, 'GetComboPoints("player") == 5 and jps.cooldown(druid.spells.tigersFury) < 3' },
+	{druid.spells.ferociousBite, 'GetComboPoints("player") == 5 and jps.buff(druid.spells.berserk)' },
+	
+	{druid.spells.rake, 'jps.myDebuffDuration(druid.spells.rake) <= 3 and GetComboPoints("player") < 5' },
+	--{druid.spells.rake, 'jps.myDebuffDuration(druid.spells.rip) <= 7 and GetComboPoints("player") < 5' },
+	{druid.spells.thrashCat, 'jps.myDebuffDuration(druid.spells.thrashCat) <= 4 and jps.MultiTarget' },
+	--wtf?:{druid.spells.moonfire, 'jps.myDebuffDuration(druid.spells.rip) <= 7' },
+	--{druid.spells.rake, 'druid.spells.persistentMultiplier > dot.druid.spells.rake.pmultiplier and comboPoints < 5' },
+	{druid.spells.swipe, 'GetComboPoints("player") < 5 and jps.MultiTarget' },
+	{druid.spells.shred, 'GetComboPoints("player") < 5' },
+}
+
+jps.registerRotation("DRUID","FERAL",function()
 	local spell = nil
 	local target = nil
+	
+	spell,target = parseStaticSpellTable(spellTable)
 
-	local energy = UnitMana("player")
-	local cp = GetComboPoints("player")
-	local executePhase = jps.hp("target") <= .25
-	local energyPerSec = 11.16
-	
-	local tigersFuryCD = jps.cooldown("Tiger's Fury")
-	
-	local clearcasting = jps.buff("Clearcasting")
-	local berserk = jps.buff("Berserk")
-	local tigersFury = jps.buff("Tiger's fury")
-	local predatorySwiftness = jps.buff("Predatory Swiftness")
-	local cenarionStacks = jps.buffStacks(108373) -- jps.buffStacks("Dream of Cenarius") 
-	
-	local ripDuration = jps.myDebuffDuration("Rip")
-	local rakeDuration = jps.myDebuffDuration("Rake")
-	local savageRoarDuration = jps.buffDuration("Savage Roar")
-	local thrashDuration = jps.myDebuffDuration("Thrash")
-	local predatorySwiftnessDuration = jps.buffDuration("Predatory Swiftness")
-	
-	
-	-- Berserk makes every ability cost 50% less energy, so we can't hardcode these values (more future proof this way, anyways).
-	--to do wod ... get spell info no longer returns ability cost!
-	local thrashCost = ({ GetSpellInfo('Thrash') })[4]
-	local swipeCost = ({ GetSpellInfo('Swipe') })[4]
-	local shredCost = ({ GetSpellInfo('Shred') })[4]
-	local ripCost = ({ GetSpellInfo('Rip') })[4]
-	local ravageCost = ({ GetSpellInfo('Ravage') })[4]
-	local rakeCost = ({ GetSpellInfo('Rake') })[4]
-	
-	local maxLevel = (UnitLevel("player") == 90)
-	
-	local spellTable = {
-		
-	-- Cat Form 
-		{ "Cat Form",  not jps.buff("Cat Form") },
-		
-	-- Bail if not in melee range. 
-		{ nil,  IsSpellInRange("Shred", "target") == 0 },
-		
-	-- Survival Instincts 
-		{ "Survival Instincts", jps.hp() < .5 },
-		
-	-- Healthstone if you get low. 
-		{ "Healthstone", jps.hp() < .5 and GetItemCount("Healthstone", 0, 1) > 0 },
-		
-	-- Barkskin 
-		{ "Barkskin", jps.hp() < .6 },
-		
-	-- Interrupts 
-		{ "Skull Bash",  jps.shouldKick()  and jps.Interrupts },
-		
-	-- Talent based stun. 
-		{ "Mighty Bash",  jps.shouldKick()  and jps.Interrupts },
-		
-	-- Savage Roar should be kept up at all times. 
-		{ "Savage Roar",  savageRoarDuration == 0 },
-		
-	-- Healing Touch when we have Predatory Swiftness, less than 2 cenarion stacks, and the combo points to use the damage buff.
-		{ "Healing Touch",  predatorySwiftness and cenarionStacks < 2 and cp >= 4 and maxLevel },
-		
-	-- Healing Touch to use up Predatory Swiftness before it falls off if we have less than 2 cenarion stacks and low combo points and energy. 
-		{ "Healing Touch",  predatorySwiftness and predatorySwiftnessDuration <= 1 and not clearcasting  and energy < 45  and cenarionStacks < 2  and cp < 4 and maxLevel },
-		
-	-- Healing Touch whenever we have Nature's Swiftness. (talent based) 
-		{ "Healing Touch",  jps.buff("Nature's Swiftness") and cenarionStacks < 2 },
-		
-	-- Tiger's Fury when we're low on energy. 
-		{ "Tiger's Fury",  energy <= 35  and not clearcasting },
-		
-	-- Berserk when we have Tiger's Fury 
-		{ "Berserk",  jps.UseCDs and jps.buff("Tiger's Fury") },
-		
-	-- Nature's Vigil if Berserk buff in on. 
-		{ "Nature's Vigil",  jps.UseCDs and berserk },
-		
-	-- Incarnation if Berserk buff in on. (talent specific) 
-		{ "Incarnation",  jps.UseCDs and berserk },
-		
-	-- On-Use Trinkets if Berserk buff in on. 
-		{ jps.useTrinket(0),  jps.UseCDs },
-		{ jps.useTrinket(1),  jps.UseCDs },
-		
-	-- DPS Racial if Berserk buff in on. 
-		{ jps.DPSRacial,  jps.UseCDs },
-		
-	-- Lifeblood if Berserk buff in on. (requires herbalism) 
-		{ "Lifeblood", jps.UseCDs },
-		
-	-- Treants (talent specific) 
-		{ "Force of Nature" },
-		
-	-- Faerie Fire single-target when we know it's going to be a longer fight. 
-		{ "Faerie Fire",  not jps.MultiTarget and energy <= 60 and not jps.debuff("Weakened Armor") and UnitHealth("target") > (UnitHealth("player") * .8) },
-		
-	-- Ferocious Bite if we're in execute phase and Rip is about the fall off. 
-		{ "Ferocious Bite",  not jps.MultiTarget and executePhase  and cp > 0  and ripDuration <= 2  and ripDuration > 0 },
-		
-	-- Multi-target only: Thrash debuff should be kept up at all times. 
-		{ "Thrash",  jps.MultiTarget and energy >= thrashCost and thrashDuration < 2 },
-		
-	-- Multi-target only: Swipe is the base AoE spell. (Assume there's a good reason to limit at 51+?) 
-		{ "Swipe",  jps.MultiTarget and energy >= swipeCost },
-		
-	-- Thrash if we're clearcasting, it's debuff is about to run out, and we have no cenarion stacks. 
-		{ "Thrash",  clearcasting  and thrashDuration < 3  and cenarionStacks == 0 },
-		
-	-- Savage Roar 
-		{ "Savage Roar",  savageRoarDuration <= 1  or (savageRoarDuration <= 3  and cp > 0)  and executePhase },
-		
-	-- Nature's Swiftness 
-		{ "Nature's Swiftness", cenarionStacks == 0  and not predatorySwiftness  and cp >= 5  and executePhase },
-		
-	-- Rip 
-		{ "Rip",  not jps.MultiTarget and (energy >= ripCost or clearcasting) and cp >= 5  and cenarionStacks > 0  and executePhase  and ripDuration < 4 },
-		
-	-- stronger rip detection	  -- Ferocious Bite 
-		{ "Ferocious Bite",  not jps.MultiTarget and executePhase  and cp == 5  and ripDuration > 0 },
-		
-	-- Rip 
-		{ "Rip",  not jps.MultiTarget and (energy >= ripCost or clearcasting) and cp >= 5  and ripDuration < 2  and cenarionStacks > 0 },
-		
-	-- Savage Roar 
-		{ "Savage Roar",  savageRoarDuration <= 1  or (savageRoarDuration <= 3  and cp > 0) },
-		
-	-- Nature's Swiftness 
-		{ "Nature's Swiftness", cenarionStacks == 0  and not predatorySwiftness  and cp >= 5  and ripDuration < 3  and (berserk  or ripDuration <= tigersFuryCD)  and not executePhase and maxLevel },
-		
-	-- Temporary for leveling 
-		{ "Nature's Swiftness", not predatorySwiftness and not maxLevel },
-		
-	-- Rip 
-		{ "Rip",  not jps.MultiTarget and (energy >= ripCost or clearcasting) and cp >= 5  and ripDuration < 2  and (berserk  or ripDuration < tigersFuryCD) },
-		
-	-- Thrash 
-		{ "Thrash",  clearcasting  and thrashDuration < 3 },
-		
-	-- Savage Roar 
-		{ "Savage Roar",  savageRoarDuration <= 6  and cp >= 5  and ripDuration > 4 },
-		
-	-- Ferocious Bite 
-		{ "Ferocious Bite",  not jps.MultiTarget and cp >= 5  and ripDuration > 4 },
-		
-	-- Rake 
-		{ "Rake",  not jps.MultiTarget and (energy >= rakeCost or clearcasting) and cenarionStacks > 0  and rakeDuration < 3 },
-		
-	-- Rake 
-		{ "Rake",  not jps.MultiTarget and (energy >= rakeCost or clearcasting) and rakeDuration < 3  and (berserk  or tigersFuryCD + .8 >= rakeDuration) },
-		
-	-- Shred 
-		{ "Shred",  not jps.MultiTarget and clearcasting },
-		
-	-- Shred 
-		{ "Shred",  not jps.MultiTarget and predatorySwiftnessDuration > 1  and not (energy + (energyPerSec * (predatorySwiftnessDuration - 1)) < (4 - cp) * 20) },
-		
-	-- Shred 
-		{ "Shred",  not jps.MultiTarget and energy >= shredCost and (  (cp < 5  and ripDuration < 3)  or (cp == 0  and savageRoarDuration < 2 )  )  },
-		
-	-- Thrash 
-		{ "Thrash",  cp >= 5  and energy >= thrashCost and thrashDuration < 6  and (tigersFury  or berserk) },
-		
-	-- Thrash 
-		{ "Thrash",  cp >= 5  and energy >= thrashCost and thrashDuration < 6  and tigersFuryCD <= 3 },
-		
-	-- Thrash 
-		{ "Thrash",  cp >= 5  and energy >= thrashCost and thrashDuration < 6  and energy >= 100 - energyPerSec },
-		
-	-- Shred 
-		{ "Shred",  not jps.MultiTarget and energy >= shredCost and (tigersFury  or berserk) },
-		
-	-- Shred 
-		{ "Shred",  not jps.MultiTarget and energy >= shredCost and tigersFuryCD <= 3  },
-		
-	-- Shred 
-		{ "Shred",  not jps.MultiTarget and energy >= 100 - (energyPerSec * 2) }, 
-	-- Mangle if not behind
-		{ "Mangle",  not jps.MultiTarget and jps.isNotBehind }  
-	}
-
-	spell,target = parseSpellTable(spellTable)
 	return spell,target
-
-end	,"Default PvE",true,false)
+end, "Simcraft druid-FERAL")
