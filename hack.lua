fda5122f483563bb47b19ff7bc9afbb77d4b6435
pcmd <karol@kutrowski.de>
2014-10-28 08:52:40 +0100

    Update hack.lua

--- hack.lua
+++ hack.lua
@@ -58,3 +58,33 @@ function fh.groundClick(spell,unit)
 		end
 	end
 end
+
+-- this is from the PE Addon , thx to phelps
+function fh.Distance(a, b)
+	if UnitExists(a) and UnitIsVisible(a) and UnitExists(b) and UnitIsVisible(b) then
+		local ax, ay, az = ObjectPosition(a)
+		local bx, by, bz = ObjectPosition(b)
+		return math.sqrt(((bx-ax)^2) + ((by-ay)^2) + ((bz-az)^2)) - ((UnitCombatReach(a)) + (UnitCombatReach(b)))
+	end
+	return 0
+end
+
+function fh.UnitsAroundUnit(unit, distance, checkCombat)
+	local total = 0
+	if UnitExists(unit) then
+		local totalObjects = ObjectCount()
+		for i = 1, totalObjects do
+			local object = ObjectWithIndex(i)
+			if bit.band(ObjectType(object), ObjectTypes.Unit) > 0 then
+				local reaction = UnitReaction("player", object)
+				local combat = UnitAffectingCombat(object)
+				if reaction and reaction <= 4 and combat then
+					if fh.Distance(object, unit) <= distance then
+						total = total + 1
+					end
+				end
+			end
+		end
+	end
+	return total
+end
