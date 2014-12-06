9805fafc21af968f7683e156df34d05973ac1873
pcmd <karol@kutrowski.de>
2014-12-06 18:14:13 +0100

    6.0 druid balance

--- Rotations/druid_balance.lua
+++ Rotations/druid_balance.lua
@@ -1,60 +1,131 @@
---[[[
-@rotation Default
-@class DRUID
-@spec BALANCE
-@author jpganis
-@description 
-SimCraft[br]
-[br]
-TODO: Add tab-dotting everything
-]]--
-jps.registerRotation("DRUID","BALANCE", function()
+druidBalance = {};
+druidBalance.activeEnemies = "active enemies";	      
+druidBalance.arcaneTorrent = "arcane torrent";	      
+druidBalance.berserking = "berserking";	      
+druidBalance.bloodFury = "blood fury";	      
+druidBalance.celestialAlignment = "celestial alignment";	      
+druidBalance.forceOfNature = "force of nature";	      
+druidBalance.incarnation = "Incarnation: Chosen of Elune";	      
+druidBalance.lunarEmpowerment = "lunar empowerment";	      
+druidBalance.lunarMax = "lunar max";	      
+druidBalance.lunarPeak = "lunar peak";	      
+druidBalance.moonfire = "moonfire";	      
+druidBalance.rechargeTime = "recharge time";	      
+druidBalance.singleTarget = "single target";	      
+druidBalance.solarEmpowerment = "solar empowerment";	      
+druidBalance.solarPeak = "solar peak";	      
+druidBalance.starfall = "starfall";	      
+druidBalance.starfire = "starfire";	      
+druidBalance.starsurge = "starsurge";	      
+druidBalance.stellarFlare = "stellar flare";	      
+druidBalance.sunfire = "sunfire";	      
+druidBalance.wrath = "wrath";	 
+druidBalance.solarBeam ="Solar Beam";
+druidBalance.moonkinForm ="Moonkin Form";
+druidBalance.starsurgeRecharge = function()
+	currentCharges, maxCharges, cooldownStart, cooldownDuration = GetSpellCharges(druidBalance.starsurge)
+	return (GetTime() - cooldownStart) or 0
+end
+
+druidBalance.eclipseChange = function() 
+	local cycleTime = 30/2
+	if jps.talentInfo("Euphoria") then
+		cycleTime = cycleTime/2
+	end
+	if jps.buff(druidBalance.celestialAlignment) then
+		cycleTime = cycleTime/10
+	end
+	local power = jps.eclipsePower() 
+	if power == 0 then power =  1 end
+	if power < 0 then power = power *-1 end
 	
+	return cycleTime * ((100 - power)/100)
+end
+
+
+druidBalance.lunarPower = function()
+	local power = jps.eclipsePower()
+	if power > 0 then return 0 end 
+	if power < 0 then power = power * -1 end
+	return power
+end
+spellTableBalance = {
+		
+ -- buffs
+ -- [buffs]
+ -- cooldowns
+	{druidBalance.solarBeam, 'jps.shouldKick()'},
+	{"nested", 'jps.canDPS("target") and not jps.Moving', {
+		{druidBalance.bloodFury, 'jps.buff(druidBalance.celestialAlignment) and jps.UseCDs' },
+		{druidBalance.berserking, 'jps.buff(druidBalance.celestialAlignment) and jps.UseCDs' },
+		{druidBalance.arcaneTorrent, 'jps.buff(druidBalance.celestialAlignment) and jps.UseCDs' },
+		{druidBalance.forceOfNature, 'jps.UseCDs' },
+		{ jps.useTrinket(0), 'jps.UseCDs and jps.useTrinketBool(0) ' },
+		{ jps.useTrinket(1), 'jps.UseCDs and jps.useTrinketBool(1) ' },	
+		{druidBalance.celestialAlignment, 'jps.UseCDs' },
+		{druidBalance.incarnation, 'jps.UseCDs' },
+	}},
+	-- multitarget druidBalance.target, remove if empty
+
+	{druidBalance.moonkinForm, 'GetShapeshiftForm() == 0'},
+	{'nested' , 'jps.MultiTarget', 
+		
+   {
+		
+	
+	{druidBalance.starfall, 'not jps.buff(druidBalance.es)' },
+	
+	{druidBalance.moonfire, 'druidBalance.lunarPower() > 0 and jps.buffDuration(druidBalance.celestialAlignment) < 4'},
+	
+	{druidBalance.moonfire, 'jps.myDebuffDuration(druidBalance.sunfire) < 4 and jps.eclipsePower() > 0'},
+	{druidBalance.moonfire, 'jps.myDebuffDuration(druidBalance.moonfire) < 4 and druidBalance.lunarPower() > 0' },	
+	{druidBalance.moonfire, 'jps.myDebuffDuration(druidBalance.sunfire,"mouseover") < 4 and jps.eclipsePower() > 0 and jps.canDPS("mouseover")',"mouseover"},
+	{druidBalance.moonfire, 'jps.myDebuffDuration(druidBalance.moonfire,"mouseover") < 4 and druidBalance.lunarPower() > 0 and jps.canDPS("mouseover")',"mouseover" },	
+	{druidBalance.moonfire, 'jps.eclipsePower() > 0 and jps.eclipsePower() < 30 and jps.myDebuffDuration(druidBalance.sunfire) < 15'},
+	{druidBalance.moonfire, 'druidBalance.lunarPower() < 30 and druidBalance.lunarPower() > 0 and jps.myDebuffDuration(druidBalance.moonfire) < 15' },	
+	
+	
+	{druidBalance.starsurge, 'GetSpellCharges(druidBalance.starsurge)==2 and druidBalance.starsurgeRecharge() < 6' },
+	{druidBalance.starsurge, 'GetSpellCharges(druidBalance.starsurge)==3 ' },
+	{druidBalance.wrath, 'druidBalance.lunarPower() > 0' },
+	{druidBalance.starfire, 'jps.eclipsePower() >= 0' },
+	
+    }
+		
+  },
+		
+
+ -- single druidBalance.target, remove if empty
+		
+  {'nested' , 'not jps.MultiTarget', 
+		
+   {
+	
+	{druidBalance.moonfire, 'jps.myDebuffDuration(druidBalance.sunfire,"mouseover") < 4 and jps.eclipsePower() > 0 and jps.canDPS("mouseover")',"mouseover"},
+	{druidBalance.moonfire, 'jps.myDebuffDuration(druidBalance.moonfire,"mouseover") < 4 and druidBalance.lunarPower() > 0 and jps.canDPS("mouseover")',"mouseover" },	
+	{druidBalance.moonfire, 'jps.myDebuffDuration(druidBalance.sunfire) < 4 and jps.eclipsePower() > 0'},
+	{druidBalance.moonfire, 'jps.myDebuffDuration(druidBalance.moonfire) < 4 and druidBalance.lunarPower() > 0' },	
+	{druidBalance.moonfire, 'jps.eclipsePower() > 0 and jps.eclipsePower() < 30 and jps.myDebuffDuration(druidBalance.sunfire) < 15'},
+	{druidBalance.moonfire, 'druidBalance.lunarPower() < 30 and druidBalance.lunarPower() > 0 and jps.myDebuffDuration(druidBalance.moonfire) < 15' },		
+    {druidBalance.starsurge, 'not jps.buff(druidBalance.lunarEmpowerment) and jps.eclipsePower() > 20' },
+	{druidBalance.starsurge, 'not jps.buff(druidBalance.solarEmpowerment) and druidBalance.lunarPower() > 40' },
+	{druidBalance.starsurge, 'GetSpellCharges(druidBalance.starsurge)==2 and druidBalance.starsurgeRecharge() < 6' },
+	{druidBalance.starsurge, 'GetSpellCharges(druidBalance.starsurge)==3 ' },
+
+	
+	
+	{druidBalance.wrath, 'druidBalance.lunarPower() > 0' },
+	{druidBalance.starfire, 'jps.eclipsePower() >= 0' },
+
+	
+}
+		
+  },
+}
+
+jps.registerRotation("DRUID","BALANCE",function()
 	local spell = nil
 	local target = nil
-	
-	-- bpt virtual trackers
-	local Energy = jps.eclipsePower()
-	local Direction = GetEclipseDirection()
-	
-	if Direction == "none" then Direction = "sun" end
-	
-	-- Eclipse Buffs
-	local sEclipse = jps.buff("eclipse (solar)")
-	local lEclipse = jps.buff("eclipse (lunar)")
-	
-	-- Dot Durations
-	local mfDuration = jps.debuffDuration("moonfire") - jps.CastTimeLeft()
-	local sfDuration = jps.debuffDuration("sunfire") - jps.CastTimeLeft()
-	local datEclipse = sEclipse or lEclipse
-
-	
-	local spellTable =
-	{
-		-- rebirth Ctrl-key + mouseover
-		{ "rebirth", IsControlKeyDown() == true and UnitIsDeadOrGhost("mouseover") == true and IsSpellInRange("rebirth", "mouseover"), "mouseover" },
-		-- Buffs
-		{ "mark of the wild", not jps.buff("mark of the wild") , "player" },
-		-- Rotation
-		{ "starfall" },
-		{ "force of nature", jps.buff("nature's grace") or jps.Moving },
-		{ "moonfire", jps.Moving and lEclipse },
-		{ "sunfire", jps.Moving },
-		{ "starsurge", jps.Moving and jps.buff("shooting stars") },
-		{ "incarnation", sEclipse or lEclipse },
-		{ "celestial alignment", jps.UseCDs and ((Direction=="moon" and Energy <= 0) or (Direction=="sun" and Energy >= 0)) and (not select(5,GetTalentInfo(11,"player")) or jps.buff("Incarnation: Chosen of Elune")) },
-		{ "wrath", Energy <= -70 and Direction == "moon" },
-		{ "starfire", Energy >= 60 and Direction == "sun" },
-		{ "moonfire", mfDuration <= 1.5 },
-		{ "sunfire", sfDuration <= 1.5 },
-		{ "starsurge", (Energy < 80 and Energy > -85) or datEclipse},
-		{ "starfire", jps.buff("celestial alignment") },
-		{ "starfire", Direction == "sun" },
-		{ "wrath", Direction == "moon" },
-		{ "moonfire", jps.Moving and sfDuration == 0 },
-		{ "sunfire" , jps.Moving and mfDuration == 0 },
-	}
-
-	spell,target = parseSpellTable(spellTable)
+	spell,target = parseStaticSpellTable(spellTableBalance)
 	return spell,target
-end, "Default")
+end, "Simcraft druid-BALANCE")
