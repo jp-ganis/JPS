d08529b8d403d475fcbdec6d9a6618c56fa97840
pcmd <karol@kutrowski.de>
2015-01-11 14:21:17 +0100

    Update hunter_sv.lua

--- Rotations/hunter_sv.lua
+++ Rotations/hunter_sv.lua
@@ -1,254 +1,129 @@
 --[[[
-@rotation Default
+@rotation MM Hunter PVE 6.0.2
 @class HUNTER
-@spec SURVIVAL
-@author tropic
-@talents Yb!001100!dfgkmM
+@spec MARKSMANSHIP
+@author tropic, updated by peanutbird
 @description
-Features:[br]
-[*] auto misdirect to pet if soloing, misdirect to "focus" e.g. in party/raid[br]
-[*] Auto use "Healthstone" at 50% hp
-[*] mend pet when hp is less than 90%[br]
-[*] Use CDs: Blows all cooldowns: trinkets, eng. gloves pots (if boss) etc.
+Talents/gear:[br]
+[*] Weapon DPS >>> Crit > Haste > Mastery > Multistrike > Agility[br]
+[*] Thrill of the Hunt[br]
+[*] Stampede[br]
+[*] Barrage > Glaive Toss[br]
 [br]
-[br]
-Trap Keys:[br]
-[*][code]SHIFT:[/code] Explosive Trap[br]
-[*][code]ALT:[/code] Freezing Trap[br]
-[*][code]CONTROL:[/code] Snake Trap[br]
-[*][code]SHIFT-CONTROL:[/code] Ice Trap[br]
-]]--
-
-
-jps.registerRotation("HUNTER","SURVIVAL", function()
--- by tropic
-jps.Tooltip = "::Survival Hunter (PvE)::\n- Shift-key: \"Explosive Trap\"\n- Alt-key: \"Freezing Trap\"\n- Control-key: \"Snake Trap\"\n- Shift+Control-key: \"Ice Trap\"\nMisdirect to Pet when soloing or misdirect to \"focus\" in party/raid.\nUse CDs: Blows all cooldowns: trinkets, eng. gloves, \n  talents, pots (if boss) etc.  \nAuto use \"Healthstone\" at 50% hp, \"Mend Pet\" at 90% hp.\nInterrupt spell cast with \"Silencing Shot\".\nCheck file for talents and glyphs..."
---------------
---- TALENTS --
---------------
--- http://www.wowhead.com/talent#hTE
-
----------------
----- GLYPHS ---
----------------
--- Major: (1) Glyph of Deterrence (2) Glyph of Misdirection (3) Glyph of Animal Bond
--- Minor: (1) Glyph of Revive Pet (2) Glyph of Aspect of the Cheetah (3) Glyph of Aspect of the Pack
-
----------------
----- INFO -----
----------------
--- will auto misdirect to pet if soloing
--- misdirect to "focus" e.g. in party/raid
--- mend pet when hp is less than 90%
--- interrupt spellcasting with Silencing Shot
--- use CDs incl. Lifeblood (herbalism)
--- hot-keys for traps
-
----------------
----- SETUP ----
----------------
-local spell = nil
-local target = nil
-local focus = UnitMana("player")
-
----------------------
---- TRAP HOT-KEYS ---
----------------------
--- Reset keys to zero
-local shiftKEY_binary = 0
-local altKEY_binary = 0
-local controlKEY_binary = 0
-
--- Register key downs
-if IsShiftKeyDown() == true    	and GetCurrentKeyBoardFocus() == nil then shiftKEY_binary = 1 	end
-if IsAltKeyDown() == true 	and GetCurrentKeyBoardFocus() == nil then altKEY_binary = 2 	end
-if IsControlKeyDown() == true 	and GetCurrentKeyBoardFocus() == nil then controlKEY_binary = 4 end
-
--- Binary calculation
-trapKEY_combo = shiftKEY_binary + altKEY_binary + controlKEY_binary
--- print("Trap key combo pressed: ",trapKEY_combo) -- FOR TESTING PURPOSE
-
--- Binary key combinations
--- 1 = Shift
--- 2 = Alt
--- 3 = Shift + Alt
--- 4 = Control
--- 5 = Shift + Control
--- 6 = Alt + Control
--- 7 = Shift + Alt + Control
-if trapKEY_combo == 1 then ExplosiveTrap_KEY 	= true 	else ExplosiveTrap_KEY 	= false end -- Launch Explosiv trap
-if trapKEY_combo == 2 then FreezingTrap_KEY 	= true  else FreezingTrap_KEY 	= false end -- Launch Freezing trap (ice block trap)
-if trapKEY_combo == 4 then SnakeTrap_KEY 	= true  else SnakeTrap_KEY 	= false end -- Launch Snake trap
-if trapKEY_combo == 5 then IceTrap_KEY 		= true  else IceTrap_KEY 	= false end -- Launch Ice trap (ice on ground)
-if trapKEY_combo == 6 then allInOneTraps_KEY 	= true 	else allInOneTraps_KEY 	= false end -- Launch all traps after each other
-
---------------------
--- Auto use setup --
---------------------
--- Pots
-local autoUseVirminsBite = true -- Increases your Agility by 4000 for 25 sec. (1 Min Cooldown)
-
----------------------------------------------------------
--- Pet target check - force pet to attack playertarget --
----------------------------------------------------------
--- IMPORTANT!! pet must be set to "Passive" on the pet actionbar, this is done automatically below
-local petTargetID = UnitGUID("pettarget") 		-- get unique ID on pettarget
-local playerTargetID = UnitGUID("target") 		-- get unique ID on playertarget
-local petShouldAttackMyTarget = false 			-- default to false
-if playerTargetID ~= nil				-- 1) check if player has target,
-and playerTargetID ~= petTargetID			-- 2) check petarget is equal to playertarget,
-and UnitCanAttack("player", "target") ~= nil then	-- 3) check that player can attack current target
-petShouldAttackMyTarget = true				-- set variable to true = pet should attack playertarget
-end
-
--------------------------------------------------------
--- Pet passive mode check - force pet to PassiveMode --
--------------------------------------------------------
--- Check pet is passive, returns 1/nil
-local _, _, _, _, petIsPassive, _, _ = GetPetActionInfo(10) -- Slot 10 is PassiveMode on the pet actionbar
-
------------------
--- Spell Table --
------------------
-
-	local spellTable =  {
-		-- Preparation (flasks)
-		{ jps.useBagItem("Alchemist's Flask") , not jps.buff("Enhanced Agility") and not jps.buff("Flask of Spring Blossoms") and jps.UseCDs},
-		-- Revive pet
-		{ "Heart of the Phoenix",		UnitIsDead("pet") ~= nil and HasPetUI() ~= nil }, -- Instant revive pet (only some pets, Ferocity)
-		{ "Revive Pet",				((UnitIsDead("pet") ~= nil and HasPetUI() ~= nil) or HasPetUI() == nil) and not jps.Moving },
-		-- Heal pet
-		{ "Mend Pet", 				jps.hp("pet") < 0.90 and not jps.buff("Mend Pet","pet") },
-		-- Set pet to passive (IMPORTANT!)
-		{ {"macro","/script PetPassiveMode()"},	petIsPassive == nil }, -- Set pet to passive
-		-- Misc
-		{ {"macro","/petattack"}, 		petShouldAttackMyTarget },
-		-- Misdirect to pet if no "focus" -- for farming, best with Glyph of Misdirection
-		{ "Misdirection", 			not jps.buff("Misdirection") and UnitExists("focus") == nil and not IsInGroup() and UnitExists("pet") ~= nil, "pet" }, -- IsInGroup() returns true/false. Works for any party/raid
-		{ "Misdirection", 			not jps.buff("Misdirection") and UnitExists("focus") ~= nil, "focus" },
-		-- Healthstone
-		{ jps.useBagItem("Healthstone") , 	jps.hp("player") < 0.50 }, -- restores 20% of total health
-		--
-		-- Trinkets and Engineering Gloves
-		{ jps.useTrinket(0), jps.UseCDs },
-		{ jps.useTrinket(1), jps.UseCDs },
-
-		-- Requires herbalism
-		{ "Lifeblood",			jps.UseCDs },
-		-- Use pot
-		{ jps.useBagItem("Virmen's Bite"), 	autoUseVirminsBite and jps.UseCDs and jps.bloodlusting() },
-		{ "A Murder of Crows", 			jps.UseCDs and not jps.myDebuff("A Murder of Crows")}, -- Tier 5 talent
-		{ "Dire Beast", 			"onCD" }, -- Tier 4 talents
-
-		{ "Stampede", 				jps.UseCDs },
-		-- Traps
-		{ "Trap Launcher", 			not jps.buff("Trap Launcher") },
-		{ "Explosive Trap",			(ExplosiveTrap_KEY 	or allInOneTraps_KEY) and jps.buff("Trap Launcher") },
-		{ "Ice Trap",				(IceTrap_KEY 		or allInOneTraps_KEY) and jps.buff("Trap Launcher") },
-		{ "Snake Trap",				(SnakeTrap_KEY 		or allInOneTraps_KEY) and jps.buff("Trap Launcher") },
-		{ "Freezing Trap",			FreezingTrap_KEY 	and jps.buff("Trap Launcher") },
-		-- Rotation
-		{ "explosive shot", 			jps.buff("lock and load") },
-		{ "Glaive Toss", 			"onCD"}, -- Tier 6 talent
-		-- AoE
-		{ "Multi-Shot", 			jps.MultiTarget },
-		{ "steady shot", 			jps.MultiTarget and not IsSpellKnown(77767) }, -- Cobra Shot (77767), in MoP learned at level 81, so we use Steady Shot until then
-		{ "cobra shot", 			jps.MultiTarget },
-		-- Single target
-		{ "kill shot", 				"onCD" }, -- Target below 20%
-		{ "serpent sting", 			jps.myDebuffDuration("serpent sting") < .3 },
-		{ "explosive shot", 			"onCD" },
-		{ "black arrow", 			not jps.myDebuff("black arrow") and not jps.MultiTarget },
-		{ "arcane shot", 			focus >= 70 and not jps.buff("lock and load") and not jps.MultiTarget },
-		{ "steady shot", 			"onCD" and not IsSpellKnown(77767) }, -- Cobra Shot (77767), in MoP learned at level 81, so we use Steady Shot until then
-		{ "cobra shot", 			"onCD"},
-	}
-
-	jps.petIsDead = false
-
-	spell,target = parseSpellTable(spellTable)
-	return spell,target
-
-end, "SV Hunter PVE 5.3")
-
-
---[[[
-@rotation Default
-@class HUNTER
-@spec SURVIVAL
-@author tropic
-@talents Yb!001100!dfgkmM
-@description
 Features:[br]
-[*] auto misdirect to pet if soloing, misdirect to "focus" e.g. in party/raid[br]
-[*] Auto use "Healthstone" at 50% hp
+[*] "Defensive" button toggles misdirect to pet if soloing, misdirect to "focus" e.g. in party/raid[br]
+[*] Auto use "Healthstone" at 30% hp[br]
 [*] mend pet when hp is less than 90%[br]
 [*] interrupt spellcasting with Counter Shot[br]
-[*] Use CDs: Blows all cooldowns: trinkets, eng. gloves pots (if boss) etc.
-[br]
+[*] Use CDs: Blows all cooldowns: trinkets, eng. gloves pots (if boss) etc.[br]
 [br]
 Trap Keys:[br]
 [*][code]SHIFT:[/code] Explosive Trap[br]
 [*][code]ALT:[/code] Freezing Trap[br]
-[*][code]CONTROL:[/code] Snake Trap[br]
+[*][code]CONTROL:[/code] Binding Shot[br]
 [*][code]SHIFT-CONTROL:[/code] Ice Trap[br]
 ]]--
 
-jps.registerStaticTable("HUNTER", "SURVIVAL", {
+-- cast_regen always returns zero even though it should be working (?)
+-- the simcraft rotation uses it a lot to prevent focus capping
+-- GetPowerRegen() stays constant despite haste changes (buffs, gear) - bug in the game (?)
+
+-- _, focusPerSec = GetPowerRegen()
+-- cast_regen = jps.CastTimeLeft("player") * GetPowerRegen()
+
+
+-- Pool focus for "Rapid Fire" so we can spam "Aimed shot" with "Careful Aim" buff
+function hunter.rapidfirepooling()
+    if UnitMana("player") < 85 and jps.cooldown(3045) < 8 and jps.cooldown(3045) ~= 0 then
+        return true
+    end
+    return false
+end
+
+function hunter.isBoss()
+	if UnitExists("target") then
+		return UnitLevel("target") == -1
+	end
+	return false
+end
+spellTableSV =  {
+
     -- Revive pet
-    { hunter.spells.heartOfThePhoenix, 'UnitIsDead("pet") ~= nil and HasPetUI() ~= nil' }, -- Instant revive pet (only some pets, Ferocity)
-    { hunter.spells.revivePet, '((UnitIsDead("pet") ~= nil and HasPetUI() ~= nil) or HasPetUI() == nil) and not jps.Moving' },
+    --{ hunter.spells.heartOfThePhoenix, 'UnitIsDead("pet") == true and HasPetUI() == true' }, -- Instant revive pet (only some pets, Ferocity)
+    --{ hunter.spells.revivePet, '((UnitIsDead("pet") == true and HasPetUI() == true) or HasPetUI() == false) and not jps.Moving' },
 
     -- Heal pet
-    { hunter.spells.mendPet, 'jps.hp("pet") < 0.90 and not jps.buff(hunter.spells.mendPet, "pet")' },
-
-    -- Set pet to passive (IMPORTANT!)
-    { {"macro", "/script PetPassiveMode()"},    'hunter.petIsPassive() == nil' },
-    { {"macro", "/petattack"}, 'hunter.petShouldAttackMyTarget()' },
-
-    -- Aspects
-    { hunter.spells.aspectOfTheIronHawk, 'not jps.buff(hunter.spells.aspectOfTheHawk) and not jps.buff(hunter.spells.aspectOfTheIronHawk)' },
+    { hunter.spells.mendPet, 'jps.hp("pet") < 0.90 and not jps.buff(hunter.spells.mendPet, "pet") and UnitExists("pet") == true and not UnitIsDead("pet")' },
 
     -- Misdirection
-    { hunter.spells.misdirection, 'not jps.buff(hunter.spells.misdirection) and UnitExists("focus") == nil and not IsInGroup() and UnitExists("pet") ~= nil', 'pet' }, -- IsInGroup() returns true/false. Works for any party/raid
-    { hunter.spells.misdirection, 'not jps.buff(hunter.spells.misdirection) and UnitExists("focus") ~= nil', 'focus' },
+    { hunter.spells.misdirection, 'jps.Defensive and not jps.buff(hunter.spells.misdirection) and UnitExists("focus") == false and not IsInGroup() and UnitExists("pet") == true', 'pet' }, -- IsInGroup() returns true/false. Works for any party/raid
+    { hunter.spells.misdirection, 'jps.combatTime() < 24 and  jps.Defensive and not jps.buff(hunter.spells.misdirection) and UnitExists("focus") == true', 'focus' },
 
     -- Interrupt
-    { hunter.spells.counterShot, 'jps.shouldKick() and jps.CastTimeLeft("target") < 1.4' },
+    { hunter.spells.counterShot, 'jps.shouldKick()' },
+    --{ hunter.spells.counterShot, 'jps.shouldKick() and jps.CastTimeLeft("target") < 1.4' },
 
     -- Healthstone
-    { jps.useBagItem(5512), 'jps.hp("player") < 0.50' },
+    { jps.useBagItem(5512), 'jps.hp("player") < 0.30' },
 
     -- Trinkets and stuff
-    { jps.useTrinket(0), 'jps.UseCDs' },
-    { jps.useTrinket(1), 'jps.UseCDs' },
-    { jps.DPSRacial, 'jps.UseCDs' },
-    { hunter.spells.lifeblood, 'jps.UseCDs' },
+		{ {"macro","/use 13"}, 'jps.useEquipSlot(13) and jps.UseCDs'},
+		{ {"macro","/use 14"}, 'jps.useEquipSlot(14) and jps.UseCDs'},
+    { "Berserking", 'jps.UseCDs' },
+	{ "Blood Fury", 'jps.UseCDs' },
+	{ hunter.spells.arcaneShot, 'jps.myDebuffDuration("Serpent Sting") <= 2'},
+	{ hunter.spells.arcaneShot, 'jps.myDebuffDuration("Serpent Sting","mouseover") <= 2 and jps.canDPS("mouseover")',"mouseover"},
 
-    -- CDs
-    { hunter.spells.aMurderOfCrows, 'jps.UseCDs and not jps.myDebuff(hunter.spells.aMurderOfCrows)' },
-    { hunter.spells.direBeast },
-    { hunter.spells.rabid, 'jps.UseCDs' },
-    { hunter.spells.rapidFire, 'jps.UseCDs and not jps.buff(hunter.spells.rapidFire) and not jps.bloodlusting()' },
-    { hunter.spells.stampede, 'jps.UseCDs' },
+	{ hunter.spells.aMurderOfCrows, 'jps.UseCDs' },
 
     -- Traps
     { hunter.spells.trapLauncher, 'not jps.buff(hunter.spells.trapLauncher)' },
     { hunter.spells.explosiveTrap, '(hunter.trapKey() == 1 or hunter.trapKey() == 6) and jps.buff(hunter.spells.trapLauncher)' },
     { hunter.spells.freezingTrap, '(hunter.trapKey() == 2 or hunter.trapKey() == 6) and jps.buff(hunter.spells.trapLauncher)' },
-    { hunter.spells.snakeTrap, '(hunter.trapKey() == 4 or hunter.trapKey() == 6) and jps.buff(hunter.spells.trapLauncher)' },
-    { hunter.spells.iceTrap, '(hunter.trapKey() == 5 or hunter.trapKey() == 6) and jps.buff(hunter.spells.trapLauncher)' },
+    { hunter.spells.iceTrap, '(hunter.trapKey() == 3 or hunter.trapKey() == 6) and jps.buff(hunter.spells.trapLauncher)' },
 
     -- Rotation
-    { hunter.spells.explosiveShot, 'jps.buff(hunter.buffs.lockAndLoad)' },
-    { hunter.spells.glaiveToss },
-    { hunter.spells.multiShot, 'jps.MultiTarget' },
-    { hunter.spells.cobraShot, 'jps.MultiTarget' },
+    { hunter.spells.chimaeraShot },
     { hunter.spells.killShot },
-    { hunter.spells.serpentSting, 'jps.myDebuffDuration(hunter.spells.serpentSting, "target") < 0.30' },
-    { hunter.spells.explosiveShot },
-    { hunter.spells.blackArrow, 'not jps.myDebuff(hunter.spells.blackArrow) and not jps.MultiTarget' },
-    { hunter.spells.cobraShot, 'jps.myDebuffDuration(hunter.spells.serpentSting) < 6' },
-    { hunter.spells.arcaneShot, 'jps.focus() >= 70 and not jps.buff(hunter.buffs.lockAndLoad) and not jps.MultiTarget' },
-    { hunter.spells.cobraShot },
-}, "SV Hunter 5.4")
+    { hunter.spells.rapidFire, 'jps.UseCDs and jps.hp("target") < 0.80 and not jps.bloodlusting()' },
+	
+    { hunter.spells.stampede, 'jps.UseCDs and jps.TimeToDie("target") > 20' },
+	{ hunter.spells.glaiveToss, 'jps.MultiTarget and jps.UseCDs' },
+	{ hunter.spells.barrage, 'jps.MultiTarget and jps.UseCDs' },
+	{ hunter.spells.multiShot,'jps.MultiTarget and jps.buff("Bombardment") '},
+	{ hunter.spells.multiShot,'jps.MultiTarget and jps.cooldown("barrage") > 5'},
+
+    {"nested", jps.buff(hunter.buffs.carefulAim), {
+        { hunter.spells.aimedShot },
+        { hunter.spells.arcaneShot },
+    }},
+    { hunter.spells.direBeast, 'jps.focus() < 90 and UnitExists("pet")== true' },
+    { hunter.spells.glaiveToss },
+    { hunter.spells.powershot },
+
+	{ hunter.spells.blackArrow},
+	{ hunter.spells.explosiveShot },
+    { hunter.spells.arcaneShot },
+	{ hunter.spells.cobraShot	},
+	{ hunter.spells.focusingShot,'jps.focus() < 60'	},
+}
+
+jps.registerRotation("HUNTER","SURVIVAL",function()
+
+	if jps.IsSpellKnown(hunter.spells.bindingShot) and jps.cooldown(hunter.spells.bindingShot) == 0 and IsControlKeyDown() == true and not GetCurrentKeyBoardFocus() and  IsShiftKeyDown() == false and IsAltKeyDown() then
+		jps.Cast(hunter.spells.bindingShot)
+	end --spells out of spelltable are currently necessary when they come from talents :(
+
+	return parseStaticSpellTable(spellTableSV)
+end,"MM Hunter 6.0.2")
+
+
+
+
+spellTableOOCMM = {
+	{"Aspect of the Cheetah" ,'IsShiftKeyDown() and jps.glyphInfo(119462) and not jps.buff("Aspect of the Cheetah") and not jps.buff("Aspect of the Fox") and not jps.buff("Aspect of the Pack") ',"player"},
+}
+jps.registerRotation("HUNTER","SURVIVAL",function()
+	
+	return parseStaticSpellTable(spellTableOOCMM)
+end,"Out of Combat",false,false,nil, true)
